06cbf99566b44fa19bea89be02e756547a099eaa
Mario Lorenzo <mariolorenzo213@gmail.com>
2020-03-01 15:27:57 +0800

    Attribute.java: Add Attribute class

--- src/main/java/seedu/address/model/hirelah/Attribute.java
+++ src/main/java/seedu/address/model/hirelah/Attribute.java
@@ -1,8 +1,42 @@
 package seedu.address.model.hirelah;
 
-/**
- * TODO: Javadoc
+/*
+ * Attribute
+ *
+ * CS2103 AY19/20 Semester 2
+ * Team Project
+ * HireLah!
+ *
+ * 01 Mar 2020
+ *
  */
+
+/**
+ * Attribute class represents the parameters that are
+ * taken into account to assess the interviewee.
+ * @author AY1920S2-W15-2
+ */
+
 public class Attribute {
-    // TODO: Just the name of the attribute and a toString method
+    private String name;
+
+    /**
+     * Constructs an Attribute instance.
+     * @param name The name of the attribute.
+     */
+
+    public Attribute(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Overrides the Object's toString method so that
+     * it displays the name of the attribute.
+     * @return The name of the attribute.
+     */
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }
