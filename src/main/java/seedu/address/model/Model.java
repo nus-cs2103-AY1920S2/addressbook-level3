6a2a371777dddb384e149a30ef6185628fc7da24
WANG-Yuchen-Alice <wangalice2000331@icloud.com>
2020-03-02 21:07:15 +0800

    Configure Entry class in logic manager

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -60,16 +60,33 @@ public interface Model {
     boolean hasPerson(Person person);
 
     /**
+     * Returns true if an Entry with the same identity as {@code entry} exists in the address book.
+     */
+    boolean hasEntry(Entry entry);
+
+    /**
      * Deletes the given person.
      * The person must exist in the address book.
      */
     void deletePerson(Person target);
 
     /**
+     * Deletes the given entry.
+     * The entry must exist in the log book.
+     */
+    void deleteEntry(Entry target);
+
+    /**
      * Adds the given person.
      * {@code person} must not already exist in the address book.
      */
     void addPerson(Person person);
+
+    /**
+     * Adds the given Entry.
+     * {@code entry} must not already exist in the log book.
+     */
+    void addEntry(Entry entry);
 
     /**
      * Adds the given person.
@@ -84,8 +101,18 @@ public interface Model {
      */
     void setPerson(Person target, Person editedPerson);
 
+    /**
+     * Replaces the given entry {@code target} with {@code editedEntry}.
+     * {@code target} must exist in the log book.
+     * The entry identity of {@code editedEntry} must not be the same as another existing entry in the log book.
+     */
+    void setEntry(Entry target, Entry editedEntry);
+
     /** Returns an unmodifiable view of the filtered person list */
     ObservableList<Person> getFilteredPersonList();
+
+    /** Returns an unmodifiable view of the filtered entry list */
+    ObservableList<Entry> getFilteredEntryList();
 
     /**
      * Updates the filter of the filtered person list to filter by the given {@code predicate}.
@@ -93,5 +120,11 @@ public interface Model {
      */
     void updateFilteredPersonList(Predicate<Person> predicate);
 
-    void addEntry(Entry toAdd);
+
+    /**
+     * Updates the filter of the filtered entry list to filter by the given {@code predicate}.
+     * @throws NullPointerException if {@code predicate} is null.
+     */
+    void updateFilteredEntryList(Predicate<Entry> predicate);
 }
+
