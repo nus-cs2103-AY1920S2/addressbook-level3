8a0b769520e61f6fc3ac1dcedf2fc20586a528ef
Daythehangedman <59592689+Daythehangedman@users.noreply.github.com>
2020-03-05 19:08:29 +0800

    Remove unused static and non-static variables for Typical Goods.

--- src/test/java/seedu/address/testutil/TypicalGoods.java
+++ src/test/java/seedu/address/testutil/TypicalGoods.java
@@ -1,12 +1,5 @@
 package seedu.address.testutil;
 
-import static seedu.address.logic.commands.CommandTestUtil.VALID_GOOD_NAME_AVOCADO;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_GOOD_NAME_BLUEBERRY;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import seedu.address.model.good.Good;
 
 /**
