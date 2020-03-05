719d4dcf599734594e147f74032a8330c137d973
Daythehangedman <59592689+Daythehangedman@users.noreply.github.com>
2020-03-05 18:50:01 +0800

    Remove unused good variable in typical goods.

--- src/test/java/seedu/address/testutil/TypicalGoods.java
+++ src/test/java/seedu/address/testutil/TypicalGoods.java
@@ -14,24 +14,6 @@ import seedu.address.model.good.Good;
  */
 public class TypicalGoods {
 
-
     public static final Good APPLE = new GoodBuilder().withGoodName("Fuji apple").build();
-    public static final Good BANANA = new GoodBuilder().withGoodName("Cavendish banana").build();
-    public static final Good CITRUS = new GoodBuilder().withGoodName("Brazil citrus").build();
-    public static final Good DURIAN = new GoodBuilder().withGoodName("Malaysia durian").build();
-    public static final Good ENTAWAK = new GoodBuilder().withGoodName("Indonesia entawak").build();
-
-    // Manually added
-    public static final Good FIG = new GoodBuilder().withGoodName("India fig").build();
-    public static final Good GRAPE = new GoodBuilder().withGoodName("Spain grape").build();
-
-
-    // Manually added - Good's details found in {@code CommandTestUtil}
-    public static final Good AVOCADO = new GoodBuilder().withGoodName(VALID_GOOD_NAME_AVOCADO).build();
-    public static final Good BLUEBERRY = new GoodBuilder().withGoodName(VALID_GOOD_NAME_BLUEBERRY).build();
-
-    public static List<Good> getTypicalGoods() {
-        return new ArrayList<>(Arrays.asList(APPLE, BANANA, CITRUS, DURIAN, ENTAWAK));
-    }
 
 }
