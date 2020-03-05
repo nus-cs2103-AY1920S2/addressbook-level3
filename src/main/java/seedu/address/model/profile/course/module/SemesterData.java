cfa92f5d4e9cf5d3375266c3530ed9daac00dbeb
gyant <e0325592@u.nus.edu>
2020-03-06 02:04:00 +0800

    Add SemesterData class

---
+++ src/main/java/seedu/address/model/profile/course/module/SemesterData.java
@@ -0,0 +1,38 @@
+package seedu.address.model.profile.course.module;
+
+/**
+ * Represents a Module's semester data in the address book.
+ * Guarantees:
+ */
+public class SemesterData {
+
+    public final int semNumber;
+
+    /**
+     * Constructs a {@code PrereqList}.
+     *
+     * @param semNumber A valid semester number
+     */
+    public SemesterData(String semNumber) {
+        // requireAllNonNull() // to be implemented
+        // checkArgument() // to be implemented
+        int num = Integer.parseInt(semNumber);
+        this.semNumber = num;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(semNumber);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof SemesterData // instanceof handles nulls
+                && semNumber == (((SemesterData) other).semNumber)); // state check
+    }
+
+    // methods to be implemented
+    // isValidCode()
+    // hashCode()
+}
