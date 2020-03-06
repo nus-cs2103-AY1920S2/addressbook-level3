423faa2da2cefb7f7c6267053919da2936dda6d0
tharshita <>
2020-03-06 12:03:29 +0800

    morphed all person to recipe

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -9,9 +9,9 @@ import java.util.Set;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Phone;
+import seedu.address.model.recipe.Email;
+import seedu.address.model.recipe.Name;
+import seedu.address.model.recipe.Phone;
 import seedu.address.model.tag.Tag;
 
 /**
