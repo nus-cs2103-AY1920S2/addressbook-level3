7c477654cff5409eb8703147a3d9ed635d28ea9f
Hardy Shein <>
2020-03-08 14:44:34 +0800

    Refactors AddCommand to only enforce a NAME field.

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -22,41 +22,53 @@ import seedu.address.model.tag.Tag;
 public class AddCommandParser implements Parser<AddCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the AddCommand and returns an
-     * AddCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the AddCommand
+     * and returns an AddCommand object for execution.
      *
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(
-                        args, PREFIX_NAME, PREFIX_PRIORITY, PREFIX_EMAIL, PREFIX_DESCRIPTION, PREFIX_TAG);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PRIORITY, PREFIX_EMAIL,
+                PREFIX_DESCRIPTION, PREFIX_TAG);
 
-        if (!arePrefixesPresent(
-                        argMultimap, PREFIX_NAME, PREFIX_DESCRIPTION, PREFIX_PRIORITY, PREFIX_EMAIL)
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME)
                 || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
         }
-
+        // if (!arePrefixesPresent(
+        // argMultimap, PREFIX_NAME, PREFIX_DESCRIPTION, PREFIX_PRIORITY, PREFIX_EMAIL)
+        // || !argMultimap.getPreamble().isEmpty()) {
+        // throw new ParseException(
+        // String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        // }
+        
+        System.out.println("Attempting parse.");
         Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
-        Priority priority = ParserUtil.parsePriority(argMultimap.getValue(PREFIX_PRIORITY).get());
-        Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
-        Description address = ParserUtil.parseDescription(argMultimap.getValue(PREFIX_DESCRIPTION).get());
+        
+        Priority priority = (argMultimap.getValue(PREFIX_PRIORITY).isEmpty()) 
+            ? ParserUtil.parsePriority("1")
+            : ParserUtil.parsePriority(argMultimap.getValue(PREFIX_PRIORITY).get());
+        
+        Email email = (argMultimap.getValue(PREFIX_EMAIL).isEmpty())
+            ? ParserUtil.parseEmail("berniceyu@example.com")
+            : ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
+        
+        Description description = (argMultimap.getValue(PREFIX_DESCRIPTION).isEmpty())
+            ? ParserUtil.parseDescription("")
+            : ParserUtil.parseDescription(argMultimap.getValue(PREFIX_DESCRIPTION).get());
+        
         Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
 
-        Person person = new Person(name, priority, email, address, tagList);
+        Person person = new Person(name, priority, email, description, tagList);
 
         return new AddCommand(person);
     }
 
     /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
+     * Returns true if none of the prefixes contains empty {@code Optional} values
+     * in the given {@code ArgumentMultimap}.
      */
-    private static boolean arePrefixesPresent(
-            ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes)
-                .allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
