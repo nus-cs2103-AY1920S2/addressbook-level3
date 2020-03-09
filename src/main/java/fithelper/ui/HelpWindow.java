36fa78df3b20e368b650da5a68fa15dbb9c0535f
WANG-Yuchen-Alice <wangalice2000331@icloud.com>
2020-03-09 20:41:28 +0800

    Build GUI Structure - Main Page.

--- src/main/java/fithelper/ui/HelpWindow.java
+++ src/main/java/fithelper/ui/HelpWindow.java
@@ -2,21 +2,21 @@ package fithelper.ui;
 
 import java.util.logging.Logger;
 
-import fithelper.commons.core.LogsCenter;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.stage.Stage;
+import fithelper.commons.core.LogsCenter;
 
 /**
  * Controller for a help page
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
+    public static final String USERGUIDE_URL =
+            "https://github.com/AY1920S2-CS2103-T09-4/main/blob/master/docs/UserGuide.adoc";
     public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
 
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
