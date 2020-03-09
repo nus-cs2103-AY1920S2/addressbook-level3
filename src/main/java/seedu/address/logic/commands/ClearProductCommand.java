a68d57a7521c35bef189e118dd0282cf367b1b10
aliciatxl <example@mail.com>
2020-03-09 22:21:14 +0800

    Implement clear all products functionality

---
+++ src/main/java/seedu/address/logic/commands/ClearProductCommand.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.AddressBook;
+import seedu.address.model.Model;
+
+/**
+ * Clears the address book.
+ */
+public class ClearProductCommand extends Command {
+
+    public static final String COMMAND_WORD = "clearp";
+    public static final String MESSAGE_SUCCESS = "Product list has been cleared!";
+
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.setAddressBook(new AddressBook());
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}
+
--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -9,6 +9,7 @@ import java.util.regex.Pattern;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddProductCommand;
 import seedu.address.logic.commands.ClearCommand;
+import seedu.address.logic.commands.ClearProductCommand;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
@@ -75,6 +76,9 @@ public class AddressBookParser {
 
         case ListProductCommand.COMMAND_WORD:
             return new ListProductCommand();
+
+        case ClearProductCommand.COMMAND_WORD:
+            return new ClearProductCommand();
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
