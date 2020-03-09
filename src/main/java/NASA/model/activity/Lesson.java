f6d8777b20fcee9cbdaf03f43e60bbdca586fd8a
don-tay <dontay0209@gmail.com>
2020-03-10 01:06:04 +0800

    Add helper methods for lesson to regenerate after the lesson is over

--- src/main/java/NASA/model/activity/Lesson.java
+++ src/main/java/NASA/model/activity/Lesson.java
@@ -1,7 +1,9 @@
 package NASA.model.activity;
 
+import java.time.LocalDateTime;
+
 /**
- * Represents Lesson method in Nasa book.
+ * Represents Lesson method in Nasa book. New lesson re-instantiates itself after the current lesson's status is DONE.
  * Guarantees: details are present and not null, field values are validated, immutable.
  */
 public class Lesson extends Activity {
@@ -20,6 +22,17 @@ public class Lesson extends Activity {
      */
     public Lesson(Name name, Date date, Note note) {
         super(name, date, note);
+    }
+
+    /**
+     * Initialise new lesson with the new date.
+     * @param numOfDaysTillNextLesson number of days from the current lesson till the next lesson
+     * @return new instance of lesson at the new date
+     */
+    public Lesson regenerateNewLesson(int numOfDaysTillNextLesson) {
+        Lesson newLesson = new Lesson(this.getName(), this.getDate().addDaysToCurrDate(numOfDaysTillNextLesson),
+                this.getNote());
+        return newLesson;
     }
 
 }
