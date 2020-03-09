17bbf36f77e42711b2aaa50dbbd619511a053942
WANG-Yuchen-Alice <wangalice2000331@icloud.com>
2020-03-09 21:20:22 +0800

    Checkstyle.

--- src/main/java/fithelper/ui/MainWindow.java
+++ src/main/java/fithelper/ui/MainWindow.java
@@ -106,11 +106,10 @@ public class MainWindow extends UiPart<Stage> {
         });
     }
 
-
     /**
      * Fills up all the placeholders of this window.
      */
-    void fillInnerParts() {
+    private void fillInnerParts() {
         foodListPanel = new FoodListPanel(logic.getFilteredFoodEntryList());
         foodListPanelPlaceholder.getChildren().add(foodListPanel.getRoot());
 
