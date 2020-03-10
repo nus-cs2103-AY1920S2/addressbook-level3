ed6079dc9ebfd838be69aaf940e98a39c8eaaf80
WANG-Yuchen-Alice <wangalice2000331@icloud.com>
2020-03-10 18:44:05 +0800

    Checkstyle.

--- src/main/java/fithelper/ui/UiPart.java
+++ src/main/java/fithelper/ui/UiPart.java
@@ -1,12 +1,12 @@
 package fithelper.ui;
 
-import fithelper.MainApp;
-import javafx.fxml.FXMLLoader;
+import static java.util.Objects.requireNonNull;
 
 import java.io.IOException;
 import java.net.URL;
 
-import static java.util.Objects.requireNonNull;
+import fithelper.MainApp;
+import javafx.fxml.FXMLLoader;
 
 /**
  * Represents a distinct part of the UI. e.g. Windows, dialogs, panels, status bars, etc.
