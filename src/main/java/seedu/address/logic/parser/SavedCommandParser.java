98dd7b3220cbce155279cafa1515ecb9b66a4bf4
Alden <49450743+alcen@users.noreply.github.com>
2020-03-10 23:29:54 +0800

    Usable "saved" command without functionality

---
+++ src/main/java/seedu/address/logic/parser/SavedCommandParser.java
@@ -0,0 +1,28 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.SavedCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * This class contains logic for parsing a
+ * given String into a SavedCommand that
+ * can be executed by Coupon Stash
+ */
+public class SavedCommandParser implements Parser<SavedCommand> {
+    /**
+     * Parses a String (which will be taken from the
+     * application when a user types in a command) into
+     * the corresponding SavedCommand that it represents
+     * @param userInput The String representing raw command
+     *                  from the user.
+     * @return Returns the corresponding SavedCommand,
+     *     if parsing is successful.
+     * @throws ParseException If the user's input does
+     *     not match the exact format for the "saved"
+     *     command (e.g. wrong prefix for the date)
+     */
+    @Override
+    public SavedCommand parse(String userInput) throws ParseException {
+        return new SavedCommand();
+    }
+}
