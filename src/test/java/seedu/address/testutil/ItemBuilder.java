5dc6d5a8ad8770eefc9be8d652cb593131f11e3c
WillySeahh <willy.seah@hotmail.com>
2020-03-10 16:00:16 +0800

    created ItemBuilder class, a necessary class before being able to run test in ItemTest

--- src/test/java/seedu/address/testutil/ItemBuilder.java
+++ src/test/java/seedu/address/testutil/ItemBuilder.java
@@ -16,5 +16,41 @@ public class ItemBuilder {
     public static final String DEFAULT_NAME = "Chicken Rice";
     public static final double DEFAULT_PRICE = 3.50;
 
+    private ItemName name;
+    private ItemPrice price;
+
+    public ItemBuilder() {
+        name = new ItemName(DEFAULT_NAME);
+        price = new ItemPrice(DEFAULT_PRICE);
+    }
+
+    /**
+     * Initializes the ItemBuilder with the data of {@code ItemToCopy}.
+     */
+    public ItemBuilder(Item itemToCopy) {
+        name = itemToCopy.getName();
+        price = itemToCopy.getPrice();
+    }
+
+    /**
+     * Sets the {@code ItemName} of the {@code Item} that we are building.
+     */
+    public ItemBuilder withName(String name) {
+        this.name = new ItemName(name);
+        return this;
+    }
+
+    /**
+     * Sets the {@code ItemPrice} of the {@code Item} that we are building.
+     */
+    public ItemBuilder withAddress(double price) {
+        this.price = new ItemPrice(price);
+        return this;
+    }
+
+    public Item build() {
+        return new Item(name, price);
+    }
+
 
 }
