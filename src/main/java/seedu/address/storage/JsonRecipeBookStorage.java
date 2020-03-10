02ca3c00b6876588965e0bfe5679fff324a46b08
kevinswk94 <kevinswk94@gmail.com>
2020-03-10 22:50:44 +0800

    refactor more references to addressbook

--- src/main/java/seedu/address/storage/JsonRecipeBookStorage.java
+++ src/main/java/seedu/address/storage/JsonRecipeBookStorage.java
@@ -17,42 +17,42 @@ import seedu.address.model.ReadOnlyRecipeBook;
 /**
  * A class to access RecipeBook data stored as a json file on the hard disk.
  */
-public class JsonAddressBookStorage implements AddressBookStorage {
+public class JsonRecipeBookStorage implements RecipeBookStorage {
 
-    private static final Logger logger = LogsCenter.getLogger(JsonAddressBookStorage.class);
+    private static final Logger logger = LogsCenter.getLogger(JsonRecipeBookStorage.class);
 
     private Path filePath;
 
-    public JsonAddressBookStorage(Path filePath) {
+    public JsonRecipeBookStorage(Path filePath) {
         this.filePath = filePath;
     }
 
-    public Path getAddressBookFilePath() {
+    public Path getRecipeBookFilePath() {
         return filePath;
     }
 
     @Override
-    public Optional<ReadOnlyRecipeBook> readAddressBook() throws DataConversionException {
-        return readAddressBook(filePath);
+    public Optional<ReadOnlyRecipeBook> readRecipeBook() throws DataConversionException {
+        return readRecipeBook(filePath);
     }
 
     /**
-     * Similar to {@link #readAddressBook()}.
+     * Similar to {@link #readRecipeBook()}.
      *
      * @param filePath location of the data. Cannot be null.
      * @throws DataConversionException if the file is not in the correct format.
      */
-    public Optional<ReadOnlyRecipeBook> readAddressBook(Path filePath) throws DataConversionException {
+    public Optional<ReadOnlyRecipeBook> readRecipeBook(Path filePath) throws DataConversionException {
         requireNonNull(filePath);
 
-        Optional<JsonSerializableAddressBook> jsonAddressBook = JsonUtil.readJsonFile(
-                filePath, JsonSerializableAddressBook.class);
-        if (!jsonAddressBook.isPresent()) {
+        Optional<JsonSerializableRecipeBook> jsonRecipeBook = JsonUtil.readJsonFile(
+                filePath, JsonSerializableRecipeBook.class);
+        if (!jsonRecipeBook.isPresent()) {
             return Optional.empty();
         }
 
         try {
-            return Optional.of(jsonAddressBook.get().toModelType());
+            return Optional.of(jsonRecipeBook.get().toModelType());
         } catch (IllegalValueException ive) {
             logger.info("Illegal values found in " + filePath + ": " + ive.getMessage());
             throw new DataConversionException(ive);
@@ -60,21 +60,21 @@ public class JsonAddressBookStorage implements AddressBookStorage {
     }
 
     @Override
-    public void saveAddressBook(ReadOnlyRecipeBook addressBook) throws IOException {
-        saveAddressBook(addressBook, filePath);
+    public void saveRecipeBook(ReadOnlyRecipeBook recipeBook) throws IOException {
+        saveRecipeBook(recipeBook, filePath);
     }
 
     /**
-     * Similar to {@link #saveAddressBook(ReadOnlyRecipeBook)}.
+     * Similar to {@link #saveRecipeBook(ReadOnlyRecipeBook)}.
      *
      * @param filePath location of the data. Cannot be null.
      */
-    public void saveAddressBook(ReadOnlyRecipeBook addressBook, Path filePath) throws IOException {
-        requireNonNull(addressBook);
+    public void saveRecipeBook(ReadOnlyRecipeBook recipeBook, Path filePath) throws IOException {
+        requireNonNull(recipeBook);
         requireNonNull(filePath);
 
         FileUtil.createIfMissing(filePath);
-        JsonUtil.saveJsonFile(new JsonSerializableAddressBook(addressBook), filePath);
+        JsonUtil.saveJsonFile(new JsonSerializableRecipeBook(recipeBook), filePath);
     }
 
 }
