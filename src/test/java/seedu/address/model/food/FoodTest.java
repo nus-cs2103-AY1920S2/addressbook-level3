bbf1110903b7400421e05c41a682b6c30745edaa
Akhil Vuputuri <akhilvuputuri@gmail.com>
2020-03-11 02:27:35 +0800

    Add test cases for Food class. Remove optional attributes for Food class.

--- src/test/java/seedu/address/model/food/FoodTest.java
+++ src/test/java/seedu/address/model/food/FoodTest.java
@@ -1,4 +1,74 @@
 package seedu.address.model.food;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalFood.PASTA;
+import static seedu.address.testutil.TypicalFood.BURGER;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.testutil.FoodBuilder;
+import seedu.address.testutil.PersonBuilder;
+
 public class FoodTest {
+
+    @Test
+    public void asObservableList_modifyList_throwsUnsupportedOperationException() {
+        Food food = new FoodBuilder().build();
+        assertThrows(UnsupportedOperationException.class, () -> food.getTags().remove(0));
+    }
+
+    @Test
+    public void isSameFood() {
+        // same object -> returns true
+        assertTrue(PASTA.isSameFood(PASTA));
+
+        // null -> returns false
+        assertFalse(PASTA.isSameFood(null));
+
+        // different name -> returns false
+        Food editedAlice = new FoodBuilder(PASTA).withName(VALID_NAME_BOB).build();
+        assertFalse(PASTA.isSameFood(editedAlice));
+
+        // same name, same phone, different attributes -> returns true
+        editedAlice = new FoodBuilder(PASTA)
+                .withTags(VALID_TAG_HUSBAND).build();
+        assertTrue(PASTA.isSameFood(editedAlice));
+
+        // same name, same phone, same email, different attributes -> returns true
+        editedAlice = new FoodBuilder(PASTA).withTags(VALID_TAG_HUSBAND).build();
+        assertTrue(PASTA.isSameFood(editedAlice));
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        Food aliceCopy = new FoodBuilder(PASTA).build();
+        assertTrue(PASTA.equals(aliceCopy));
+
+        // same object -> returns true
+        assertTrue(PASTA.equals(PASTA));
+
+        // null -> returns false
+        assertFalse(PASTA.equals(null));
+
+        // different type -> returns false
+        assertFalse(PASTA.equals(5));
+
+        // different person -> returns false
+        assertFalse(PASTA.equals(BURGER));
+
+        // IMPLEMENT AFTER ADDING COMMANDS
+//        // different name -> returns false
+//        Food editedAlice = new FoodBuilder(PASTA).withName(VALID_NAME_BURGER).build();
+//        assertFalse(PASTA.equals(editedAlice));
+//
+//        // different tags -> returns false
+//        editedAlice = new FoodBuilder(ALICE).withTags(VALID_TAG_HUSBAND).build();
+//        assertFalse(ALICE.equals(editedAlice));
+    }
 }
---
+++ src/test/java/seedu/address/testutil/FoodBuilder.java
@@ -0,0 +1,62 @@
+package seedu.address.testutil;
+
+import seedu.address.model.food.*;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.util.SampleDataUtil;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A utility class to help with building Food objects.
+ */
+public class FoodBuilder {
+
+    public static final String DEFAULT_NAME = "French Fries";
+    public static final long DEFAULT_CALORIES = 300;
+
+    private Name name;
+    private Calories calories;
+    private Set<Tag> tags;
+
+    public FoodBuilder() {
+        name = new Name(DEFAULT_NAME);
+        calories = new Calories(DEFAULT_CALORIES);
+        tags = new HashSet<>();
+    }
+
+    /**
+     * Initializes the FoodBuilder with the data of {@code foodToCopy}.
+     */
+    public FoodBuilder(Food foodToCopy) {
+        name = foodToCopy.getName();
+        tags = new HashSet<>(foodToCopy.getTags());
+    }
+
+    /**
+     * Sets the {@code Name} of the {@code Person} that we are building.
+     */
+    public FoodBuilder withName(String name) {
+        this.name = new Name(name);
+        return this;
+    }
+
+    /**
+     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
+     */
+    public FoodBuilder withTags(String ... tags) {
+        this.tags = SampleDataUtil.getTagSet(tags);
+        return this;
+    }
+
+    /**
+     * Builds a person.
+     *
+     * @return a Person.
+     */
+
+    public Food build() {
+        return new Food(name, calories, tags);
+    }
+
+}
