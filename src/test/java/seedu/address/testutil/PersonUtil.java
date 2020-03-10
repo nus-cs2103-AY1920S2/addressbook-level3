24c3655f89e600a5f635c0c1d37cc9a6628d237d
fatin99 <fatinnbs@gmail.com>
2020-03-10 23:02:57 +0800

    Edit validation regex for matric

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -34,7 +34,7 @@ public class PersonUtil {
         sb.append(PREFIX_NAME + student.getName().fullName + " ");
         sb.append(PREFIX_PHONE + student.getPhone().value + " ");
         sb.append(PREFIX_EMAIL + student.getEmail().value + " ");
-        sb.append(PREFIX_MATRIC + student.getMatric().matricNumber + " ");
+        sb.append(PREFIX_MATRIC + student.getMatric().value + " ");
         sb.append(PREFIX_ADDRESS + student.getAddress().value + " ");
         student.getTags().stream().forEach(
             s -> sb.append(PREFIX_TAG + s.tagName + " ")
@@ -50,7 +50,7 @@ public class PersonUtil {
         descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullName).append(" "));
         descriptor.getPhone().ifPresent(phone -> sb.append(PREFIX_PHONE).append(phone.value).append(" "));
         descriptor.getEmail().ifPresent(email -> sb.append(PREFIX_EMAIL).append(email.value).append(" "));
-        descriptor.getMatric().ifPresent(matric -> sb.append(PREFIX_MATRIC).append(matric.matricNumber).append(" "));
+        descriptor.getMatric().ifPresent(matric -> sb.append(PREFIX_MATRIC).append(matric.value).append(" "));
         descriptor.getAddress().ifPresent(address -> sb.append(PREFIX_ADDRESS).append(address.value).append(" "));
         if (descriptor.getTags().isPresent()) {
             Set<Tag> tags = descriptor.getTags().get();
