a2c49f980d346a92b01c23d551d88fb6c63dcc8d
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-03-11 17:33:42 +0800

    Revert "Merge remote-tracking branch 'upstream/master' into addressbook-to-recipebook"
    This reverts commit 84fd10249a8f5d465f9e8ec12c4da668bc2a70a1, reversing
    changes made to 436419d5e2942425d7ad67051995134abf92b4af.

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -10,10 +10,5 @@ public class CliSyntax {
     public static final Prefix PREFIX_TIME = new Prefix("t/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_GOAL = new Prefix("g/");
-    public static final Prefix PREFIX_INGREDIENT_GRAIN = new Prefix("ig/");
-    public static final Prefix PREFIX_INGREDIENT_VEGE = new Prefix("iv/");
-    public static final Prefix PREFIX_INGREDIENT_PROTEIN = new Prefix("ip/");
-    public static final Prefix PREFIX_INGREDIENT_OTHER = new Prefix("io/");
-    public static final Prefix PREFIX_NEXTSTEP = new Prefix("s/");
 
 }
