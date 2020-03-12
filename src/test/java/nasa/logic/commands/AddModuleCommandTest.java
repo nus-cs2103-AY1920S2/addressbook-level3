67b5dd54cb410c701ae5a6a3c48a2e49c3dfdf3b
kester-ng <kester.ng@visenze.com>
2020-03-12 15:54:03 +0800

    Updated AddModuleCommandTest

--- src/test/java/nasa/logic/commands/AddModuleCommandTest.java
+++ src/test/java/nasa/logic/commands/AddModuleCommandTest.java
@@ -7,15 +7,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static nasa.testutil.Assert.assertThrows;
 
 import nasa.model.ModelManager;
 import nasa.model.UserPrefs;
 import nasa.model.Model;
 import nasa.model.NasaBook;
+import nasa.logic.commands.exceptions.CommandException;
 
 public class AddModuleCommandTest {
 
     private Model model;
+    private final String MODULE_CODE = "CS2103";
+    private final String MODULE_NAME = "Software Engineering";
 
     @BeforeEach
     public void setModel() {
@@ -30,5 +34,14 @@ public class AddModuleCommandTest {
         AddModuleCommand command = new AddModuleCommand(module);
         command.execute(model);
         assertEquals(expectedModel, model);
+    }
+
+    @Test
+    public void execute_duplicateModule_fail() throws Exception {
+        Module module = new Module(new ModuleCode(MODULE_CODE), new ModuleName(MODULE_NAME));
+        AddModuleCommand command = new AddModuleCommand(module);
+        command.execute(model); //add one time
+        // cannot add the same module again
+        assertThrows(CommandException.class, () -> command.execute(model));
     }
 }
