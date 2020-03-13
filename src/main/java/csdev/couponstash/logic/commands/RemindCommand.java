efd1460b7b90cd52f73ce82a5d80b7a547c9064c
kevvvinn <42349196+kevvvinn@users.noreply.github.com>
2020-03-13 18:18:04 +0800

    Update RemindCommand.java
    slave to checkstyle

--- src/main/java/csdev/couponstash/logic/commands/RemindCommand.java
+++ src/main/java/csdev/couponstash/logic/commands/RemindCommand.java
@@ -1,39 +1,39 @@
 package csdev.couponstash.logic.commands;
 
 import static csdev.couponstash.commons.util.CollectionUtil.requireAllNonNull;
-import static java.util.Objects.requireNonNull;
 import static csdev.couponstash.logic.parser.CliSyntax.PREFIX_REMIND;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-
 import csdev.couponstash.commons.core.Messages;
 import csdev.couponstash.commons.core.index.Index;
 import csdev.couponstash.logic.commands.exceptions.CommandException;
 import csdev.couponstash.model.Model;
 import csdev.couponstash.model.coupon.Coupon;
-import java.time.*;
-import java.time.temporal.ChronoUnit;
+import java.time.LocalDate;
+import java.util.List;
 
+/**
+ * This class represents the remind command in Coupon Stash
+ * The remind command allows the user to set date to be reminded
+ * for a coupon. Upon the date of the reminder, Coupon Stash will
+ * notify the user in a form of a pop when Coupon Stash is launched.
+ */
 
-public class RemindCommand extends Command{
+public class RemindCommand extends Command {
 
     public static final String COMMAND_WORD = "remind";
-    public static  String MESSAGE_SET_REMINDER_SUCCESS = "";
+    private static String messageSuccess = "";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": set a reminder of a coupon, identified by the index number "
+    public static final String MESSAGE_USAGE = COMMAND_WORD 
+            + ": set a reminder of a coupon, identified by the index number "
             + "used in coupon listing. "
             + "Existing reminder will be overwritten by the input.\n"
             + "Parameters: INDEX (must be a positive integer) "
             + PREFIX_REMIND + " [Date] or [String]\n"
             + "Example: " + COMMAND_WORD + " 1 "
             + PREFIX_REMIND + " 25-12-2020"
-            + "\n"   + "Example: " + COMMAND_WORD + " 2 "
+            + "\n" + "Example: " + COMMAND_WORD + " 2 "
             + PREFIX_REMIND + " 2 days";
 
-    public static String MESSAGE_ARGUMENTS = "Reminder has been set on %2$s for Coupon %1$s";
+    private final static String MESSAGE_ARGUMENTS = "Reminder has been set on %2$s for Coupon %1$s";
 
     private final Index index;
     private LocalDate remindDate;
@@ -43,7 +43,7 @@ public class RemindCommand extends Command{
      * @param index of the coupon in the coupon lists to edit
      * @param input details to remind the coupon on
      */
-
+    
     public RemindCommand(Index index, String input) {
         requireAllNonNull(index, input);
 
@@ -51,6 +51,18 @@ public class RemindCommand extends Command{
         this.input = input;
         this.remindDate = LocalDate.now();
     }
+    
+    /**
+     * Executes the RemindCommand with a given Model representing
+     * the current state of the Coupon Stash application
+     * @param model {@code Model} which the command should operate on.
+     * @return Returns the CommandResult that encompasses the
+     *     message that should be shown to the user, as well as
+     *     any other external actions that should occur.
+     * @throws CommandException CommandException is thrown either when
+     *     the remind date is set after the coupon's expiry date or the coupon 
+     *      index is out of range. (Coupon does not exist)
+     */
     @Override
     public CommandResult execute(Model model) throws CommandException {
 
@@ -64,36 +76,34 @@ public class RemindCommand extends Command{
         Coupon couponToEdit = lastShownList.get(index.getZeroBased());
 
         // if "days before scenario", straightaway calculate the remindDate;
-        if(input.contains("days before")){
+        if (input.contains("days before")) {
 
             int daysBefore = Integer.parseInt(input.replaceAll("[^0-9]", ""));
 
             remindDate = (couponToEdit.getExpiryDate().date).minusDays(daysBefore);
             couponToEdit.getRemind().setRemind(remindDate);
 
-            MESSAGE_SET_REMINDER_SUCCESS = "Reminder has been set to remind on "
+            messageSuccess = "Reminder has been set to remind on "
                     + couponToEdit.getRemind().getDate().toString()
                     + " (" + daysBefore + " days before coupon's expiry: "
                     + couponToEdit.getExpiryDate().value + ")";
-        }
-        else{
+        } else {
             LocalDate tempDate = LocalDate.parse(input);
 
             //check if input's date is not after the coupon's expiry date
-            if(tempDate.isAfter(couponToEdit.getExpiryDate().date)){
+            if (tempDate.isAfter(couponToEdit.getExpiryDate().date)){
                 throw new CommandException(Messages.MESSAGE_REMIND_DATE_EXCEED_EXPIRY_DATE);
-            }
-            else{
+            } else {
                 remindDate = tempDate;
                 couponToEdit.getRemind().setRemind(remindDate);
-                MESSAGE_SET_REMINDER_SUCCESS = "Reminder has been set to remind on "
+                messageSuccess = "Reminder has been set to remind on "
                         + couponToEdit.getRemind().getDate().toString()
                         + " (Coupon's expiry : "
                         + couponToEdit.getExpiryDate().value + ")";
             }
         }
-        if(couponToEdit.getRemind(). getRemindFlag()){
-            return new CommandResult(MESSAGE_SET_REMINDER_SUCCESS);
+        if (couponToEdit.getRemind().getRemindFlag()) {
+            return new CommandResult(messageSuccess);
         }
         throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), input));
     }
