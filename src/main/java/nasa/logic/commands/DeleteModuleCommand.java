793c6054a146bb18a68d4e2d5593b98491875739
kester-ng <kester.ng@visenze.com>
2020-03-13 19:56:03 +0800

    Modified delete, for now can only delete one module

--- src/main/java/nasa/logic/commands/DeleteModuleCommand.java
+++ src/main/java/nasa/logic/commands/DeleteModuleCommand.java
@@ -19,39 +19,34 @@ public class DeleteModuleCommand extends Command {
             + "Parameters:"
             + PREFIX_MODULE + "....." + "\n"
             + "Example: " + COMMAND_WORD + " "
-            + "CS2030 CS1231 CS1010S";
+            + "CS2030";
 
-    public static final String MESSAGE_SUCCESS = " are deleted successfully!";
+    public static final String MESSAGE_SUCCESS = " is deleted successfully!";
 
-    public static final String MESSAGE_FAILURE = "Modules indicated all does not exist!";
+    public static final String MESSAGE_FAILURE = "Module indicated all does not exist!";
 
-    private final ArrayList<Module> modulesToDelete;
+    private final Module moduleToDelete;
 
-    public DeleteModuleCommand(ArrayList<Module> modules) {
-        requireNonNull(modules);
-        modulesToDelete = modules;
+    public DeleteModuleCommand(Module module) {
+        requireNonNull(module);
+        moduleToDelete = module;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        StringBuilder msgToUser = new StringBuilder();
-        for (Module module : modulesToDelete) {
-            if (model.hasModule(module)) {
-                msgToUser.append(module.getModuleCode().toString() + " ");
-                model.deleteModule(module);
-            }
-        }
-        if (msgToUser.length() == 0) {
+        if (model.hasModule(moduleToDelete)) {
+            model.deleteModule(moduleToDelete);
+            return new CommandResult(moduleToDelete.getModuleCode().toString() + MESSAGE_SUCCESS);
+        } else {
             throw new CommandException(MESSAGE_FAILURE);
         }
-        return new CommandResult(msgToUser.toString().trim() + MESSAGE_SUCCESS);
     }
 
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof DeleteModuleCommand // instanceof handles nulls
-                && modulesToDelete.equals(((DeleteModuleCommand) other).modulesToDelete));
+                && moduleToDelete.equals(((DeleteModuleCommand) other).moduleToDelete));
     }
 }
---
+++ src/test/java/nasa/logic/parser/DeleteModuleCommandParserTest.java
@@ -0,0 +1,13 @@
+package nasa.logic.parser;
+
+import static nasa.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static nasa.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static nasa.logic.parser.CommandParserTestUtil.assertParseFailure;
+
+
+import org.junit.jupiter.api.Test;
+
+import nasa.logic.commands.DeleteModuleCommand;
+
+public class DeleteModuleCommandParserTest {
+}
