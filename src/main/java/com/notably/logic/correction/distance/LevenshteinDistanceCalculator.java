6c6c4603bb50646d7c0eecb2035abaca33cba18c
Kevin Putera <kevintiosanna@gmail.com>
2020-03-14 17:40:58 +0800

    Create skeleton for LevenshteinDistanceCalculator class

---
+++ src/main/java/com/notably/logic/correction/distance/LevenshteinDistanceCalculator.java
@@ -0,0 +1,9 @@
+package com.notably.logic.correction.distance;
+
+public class LevenshteinDistanceCalculator implements EditDistanceCalculator {
+    @Override
+    public int calculateDistance(String first, String second) {
+        return 0;
+    }
+}
+
