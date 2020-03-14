97f54a6d9cd3a03bc98412883c0ccb515fcca5ce
johannagwan <johannagwan@gmail.com>
2020-03-14 17:04:25 +0800

    Add SuggestionEngine

---
+++ src/main/java/com/notably/logic/suggestion/SuggestionEngine.java
@@ -0,0 +1,14 @@
+package com.notably.logic.suggestion;
+
+/**
+ * Represents the Suggestion Engine instance.
+ * This is the class that we interact with when the user is typing an input.
+ */
+interface SuggestionEngine {
+    /**
+     * Generate suggestions.
+     * When input cannot get corrected, the "error" message will be shown in commandTextProperty.
+     * @param input User's input.
+     */
+    void suggest(String input);
+}
