c6b8e565f2a8fe86ee96f17ee807104b56d96a01
don-tay <dontay0209@gmail.com>
2020-03-15 01:05:13 +0800

    Add EditActivityCommandParser implementation, not 100% working, pending to-do

--- src/main/java/nasa/logic/parser/EditActivityCommandParser.java
+++ src/main/java/nasa/logic/parser/EditActivityCommandParser.java
@@ -2,12 +2,12 @@ package nasa.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static nasa.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static nasa.logic.parser.CliSyntax.PREFIX_MODULE;
+import static nasa.logic.parser.CliSyntax.*;
 
 import nasa.commons.core.index.Index;
 import nasa.logic.commands.EditActivityCommand;
 import nasa.logic.parser.exceptions.ParseException;
-import nasa.model.module.ModuleCode;
+import nasa.model.module.Module;
 
 /**
  * Parses input arguments and creates a new EditActivityCommand object
@@ -23,17 +23,41 @@ public class EditActivityCommandParser implements Parser<EditActivityCommand> {
     public EditActivityCommand parse(String args) throws ParseException {
         requireNonNull(args);
         ArgumentMultimap argMultimap =
-                ArgumentTokenizer.tokenize(args, PREFIX_MODULE); // stub
+                ArgumentTokenizer.tokenize(args, PREFIX_MODULE, PREFIX_DATE, PREFIX_NOTE, PREFIX_PRIORITY, PREFIX_ACTIVITY_NAME);
 
         Index index;
         try {
-            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+            // TODO: de-conflict input format for edit command before changing how its parsed
+             index = ParserUtil.parseIndex(argMultimap.getPreamble());
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditActivityCommand.MESSAGE_USAGE),
                     pe);
         }
 
-        ModuleCode moduleCode = ParserUtil.parseModuleCode(argMultimap.getValue(PREFIX_MODULE).get()); // stub
-        return new EditActivityCommand(index, moduleCode); // stub
+        EditActivityCommand.EditActivityDescriptor editActivityDescriptor = new EditActivityCommand.EditActivityDescriptor();
+        if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
+            editActivityDescriptor.setDate(nasa.logic.parser.ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get()));
+        }
+        if (argMultimap.getValue(PREFIX_ACTIVITY_NAME).isPresent()) {
+            editActivityDescriptor.setName(nasa.logic.parser.ParserUtil.parseActivityName(argMultimap.getValue(PREFIX_ACTIVITY_NAME).get()));
+        }
+        if (argMultimap.getValue(PREFIX_NOTE).isPresent()) {
+            editActivityDescriptor.setNote(nasa.logic.parser.ParserUtil.parseNote(argMultimap.getValue(PREFIX_NOTE).get()));
+        }
+        if (argMultimap.getValue(PREFIX_PRIORITY).isPresent()) {
+            editActivityDescriptor.setPriority(nasa.logic.parser.ParserUtil.parsePriority(argMultimap.getValue(PREFIX_PRIORITY).get()));
+        }
+
+        if (!editActivityDescriptor.isAnyFieldEdited()) {
+            throw new nasa.logic.parser.exceptions.ParseException(EditActivityCommand.MESSAGE_NOT_EDITED);
+        }
+
+        // TODO: get module by moduleCode
+        Module module = new Module(
+                nasa.logic.parser.ParserUtil.parseModuleCode(argMultimap.getValue(PREFIX_MODULE).get()),
+                    nasa.logic.parser.ParserUtil.parseModuleName(argMultimap.getValue(PREFIX_MODULE_NAME).get()));
+        // stub
+        // Module module = ParserUtil.parseModule(argMultimap.getValue(PREFIX_MODULE).get()); // stub
+        return new EditActivityCommand(index, module, editActivityDescriptor); // stub
     }
 }
