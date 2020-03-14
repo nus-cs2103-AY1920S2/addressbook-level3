edb959279395fbbdd4024d7454e917a7dcb4fa4b
johannagwan <johannagwan@gmail.com>
2020-03-14 17:18:50 +0800

    Add SuggestionModel interface

---
+++ src/main/java/com/notably/model/suggestion/SuggestionModel.java
@@ -0,0 +1,47 @@
+package com.notably.model.suggestion;
+
+import java.util.Optional;
+
+import javafx.beans.property.Property;
+
+/**
+ * Represents the Suggestion Model instance.
+ * The UI will directly interact with this model to display the suggestions result.
+ */
+interface SuggestionModel {
+    /**
+     * Gets the list of suggestions saved in the model.
+     * @return The Observable List of suggested items.
+     */
+    ObservableList<SuggestionItem> getSuggestions();
+
+    /**
+     * Saves the list of suggestions in the model.
+     * @param suggestions The list of paths or notes that match the user input.
+     */
+    void setSuggestions(List<SuggestionItem> suggestions);
+
+    /**
+     * Shows user the meaning of their input, e.g. "Create a new note".
+     * When input cannot get corrected, the "error" message will be shown in commandTextProperty.
+     * TODO: add logo.
+     * @return The meaning of the user input.
+     */
+    Property<Optional<String>> commandTextProperty();
+
+    /**
+     * Sets the command text.
+     * @param commandText The "command" that the user inputs, e.g. open, search, etc.
+     */
+    void setCommandText(String commandText);
+
+    /**
+     * Resets the command text when the user clears the input text field.
+     */
+    void clearCommandText();
+
+    /**
+     * Resets the list of suggestions.
+     */
+    void clearSuggestions();
+}
