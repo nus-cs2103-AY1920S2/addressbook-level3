219bd3931b7cb1937200b1351b4afd57ebf4d9e9
Mario Lorenzo <43873384+mario7lorenzo@users.noreply.github.com>
2020-03-14 18:33:41 +0800

    Create NormalParser and InterviewParser skeleton. (#50)
    * Add CommandSupplier and NormalParser class
    
    * Update more prefixes
    
    * Add InterviewParser class
    
    * Add comments
    
    * Fix checkstyle

---
+++ src/main/java/seedu/address/logic/parser/NormalParser.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/*
+ * NormalParser
+ *
+ * CS2103 AY19/20 Semester 2
+ * Team Project
+ * HireLah!
+ *
+ * 13 Mar 2020
+ *
+ */
+
+/**
+ * NormalParser parses the input entered by the client
+ * when HireLah! is not in interviewing mode.
+ */
+
+
+public class NormalParser {
+    /**
+     * Used for initial separation of command word and args.
+     */
+    private static final Pattern BASIC_COMMAND_FORMAT = Pattern.compile("(?<commandWord>\\S+)(?<arguments>.*)");
+    private static final HashMap<String, CommandSupplier> suppliers = new HashMap<>() {
+        {
+            // fill your commands here! e.g. put("add", (x) -> new AddCommand(x[0]));
+        }
+    };
+
+    /**
+     * Parses user input into command for execution.
+     *
+     * @param userInput full user input string
+     * @return the command based on the user input
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public Command parseCommand(String userInput) throws ParseException {
+        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
+        if (!matcher.matches()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+        }
+
+        final String commandWord = matcher.group("commandWord");
+        final String arguments = matcher.group("arguments");
+        if (suppliers.containsKey(commandWord)) {
+            return suppliers.get(commandWord).getCommand(arguments);
+        } else {
+            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
+        }
+    }
+}
