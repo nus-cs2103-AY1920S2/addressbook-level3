acc5297724690b60b0535702c53ee95fdaa05ac2
ZhangYiping126 <e0357438@u.nus.edu>
2020-03-15 02:54:39 +0800

    fix code quality

--- src/main/java/fithelper/ui/calendar/CalendarPanel.java
+++ src/main/java/fithelper/ui/calendar/CalendarPanel.java
@@ -17,8 +17,6 @@ import javafx.scene.layout.StackPane;
 public class CalendarPanel extends UiPart<AnchorPane> {
     private static final String FXML = "CalendarPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(CalendarPanel.class);
-    private CalendarPage calendarPage;
-    private FullCalendar fullCalendar;
 
     @FXML
     private StackPane calendarPagePlaceholder;
@@ -32,11 +30,7 @@ public class CalendarPanel extends UiPart<AnchorPane> {
     public CalendarPanel(ObservableList<Entry> foodList, ObservableList<Entry> sportList) {
         super(FXML);
         logger.info("Initializing Calendar Page");
-        this.calendarPage = new CalendarPage(foodList, sportList);
-        this.fullCalendar = new FullCalendar(foodList, sportList);
-        calendarPagePlaceholder.getChildren().add(this.calendarPage.getRoot());
-        fullCalendarPlaceholder.getChildren().add(this.fullCalendar.getView());
+        calendarPagePlaceholder.getChildren().add(new CalendarPage(foodList, sportList).getRoot());
+        fullCalendarPlaceholder.getChildren().add(new FullCalendar(foodList, sportList).getView());
     }
-
-
 }
