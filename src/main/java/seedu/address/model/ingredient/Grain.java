fd9ff9eb2985170d93753458f731a718e236126e
beatricetay <beatricetayyt@gmail.com>
2020-03-14 14:43:30 +0800

    created ingredient classes

---
+++ src/main/java/seedu/address/model/ingredient/Grain.java
@@ -0,0 +1,13 @@
+
+package seedu.address.model.ingredient;
+
+/**
+ * Represents a Grain Ingredient in the recipe book.
+ */
+public class Grain extends Ingredient {
+
+    public Grain(double quantity, String name) {
+        super(quantity, name);
+        super.unit = Unit.SOLID;
+    }
+}
