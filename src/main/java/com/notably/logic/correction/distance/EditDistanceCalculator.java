385225c527960c0c6bfa775fbc0cb64d444c68e9
Kevin Putera <kevintiosanna@gmail.com>
2020-03-14 17:37:27 +0800

    Update javadoc description of EditDistanceCalculator

--- src/main/java/com/notably/logic/correction/distance/EditDistanceCalculator.java
+++ src/main/java/com/notably/logic/correction/distance/EditDistanceCalculator.java
@@ -1,7 +1,7 @@
 package com.notably.logic.correction.distance;
 
 /**
- * Wrapper for algorithms that calculate distances between two {@link String}s.
+ * Wrapper for an algorithm that calculates the distance between two {@link String}s.
  */
 public interface EditDistanceCalculator {
   int calculateDistance(String first, String second);
