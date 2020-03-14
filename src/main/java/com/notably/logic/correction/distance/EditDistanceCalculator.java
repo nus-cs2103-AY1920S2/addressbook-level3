fcffaabc2711e9c0381f72c0063500a0bd974993
Kevin Putera <kevintiosanna@gmail.com>
2020-03-14 17:44:45 +0800

    Adjust code indentation of EditDistanceCalculator from 2 to 4 spaces

--- src/main/java/com/notably/logic/correction/distance/EditDistanceCalculator.java
+++ src/main/java/com/notably/logic/correction/distance/EditDistanceCalculator.java
@@ -4,6 +4,6 @@ package com.notably.logic.correction.distance;
  * Wrapper for an algorithm that calculates the distance between two {@link String}s.
  */
 public interface EditDistanceCalculator {
-  int calculateDistance(String first, String second);
+    int calculateDistance(String first, String second);
 }
 
