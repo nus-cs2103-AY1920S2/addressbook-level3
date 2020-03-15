9226c3c6820c59c4cfe90051e893306b08d783c6
Kevin Putera <kevintiosanna@gmail.com>
2020-03-15 20:43:29 +0800

    Update Javadoc for CorrectionEngine

--- src/main/java/com/notably/logic/correction/CorrectionEngine.java
+++ src/main/java/com/notably/logic/correction/CorrectionEngine.java
@@ -2,7 +2,6 @@ package com.notably.logic.correction;
 
 /**
  * Represents a correction engine.
- * This is the class to interact with when doing corrections.
  */
 interface CorrectionEngine<T> {
     /**
