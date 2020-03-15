d801808e17721f402e3dde5c729622cf84656cd9
Kevin Putera <kevintiosanna@gmail.com>
2020-03-15 17:53:38 +0800

    Add Javadoc to CorrectionResult

--- src/main/java/com/notably/logic/correction/CorrectionResult.java
+++ src/main/java/com/notably/logic/correction/CorrectionResult.java
@@ -6,6 +6,17 @@ import java.util.Optional;
  * Represents the result of a correction.
  */
 interface CorrectionResult<T> {
+    /**
+     * Gets the status of the correction.
+     *
+     * @return The status of the correction
+     */
     CorrectionStatus getCorrectionStatus();
+    /**
+     * Gets the corrected item.
+     * If correction cannot be done, {@code Optional.empty()} will be returned.
+     * 
+     * @return The corrected item, or {@code Optional.empty()} if correction fails
+     */
     Optional<T> getCorrectedItem();
 }
