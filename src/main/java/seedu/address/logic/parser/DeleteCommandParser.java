93ffbdecaaa8a404c15c365a8b5d21ca2aae3e33
agnesnatasya <e0313565@u.nus.edu>
2020-03-15 13:41:45 +0800

    Add DeleteCommandParser class

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,28 +1,49 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.DeleteAttributeCommand;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.DeleteIntervieweeCommand;
+import seedu.address.logic.commands.DeleteQuestionCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
  */
 public class DeleteCommandParser implements Parser<DeleteCommand> {
 
+    private static final Pattern BASIC_DELETE_COMMAND_FORMAT = Pattern.compile("(?<deleteCommandWord>\\S+)(?<deleteArguments>.+)");
+
     /**
      * Parses the given {@code String} of arguments in the context of the DeleteCommand
      * and returns a DeleteCommand object for execution.
      * @throws ParseException if the user input does not conform the expected format
      */
-    public DeleteCommand parse(String args) throws ParseException {
-        try {
-            Index index = ParserUtil.parseIndex(args);
-            return new DeleteCommand(index);
-        } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+
+    public DeleteCommand parse(String arguments) throws ParseException {
+        Matcher matcher = BASIC_DELETE_COMMAND_FORMAT.matcher(arguments.trim());
+        final String deleteCommandWord = matcher.group("deleteCommandWord");
+        final String deleteArguments = matcher.group("deleteArguments");
+
+        if (!matcher.matches()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE));
+        }
+
+        switch (deleteCommandWord) {
+        case DeleteAttributeCommand.COMMAND_WORD:
+            return new DeleteAttributeCommandParser().parse(deleteArguments);
+        case DeleteIntervieweeCommand.COMMAND_WORD:
+            return new DeleteIntervieweeCommandParser().parse(deleteArguments);
+        case DeleteQuestionCommand.COMMAND_WORD:
+            return new DeleteQuestionCommandParser().parse(deleteArguments);
+        default:
+            throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
         }
     }
 
