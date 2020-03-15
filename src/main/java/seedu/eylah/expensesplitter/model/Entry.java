e1095751e57b3250cd687eb0d6998b6377a8a488
ailanthustng <ailanthustng@gmail.com>
2020-03-16 03:29:10 +0800

    Added Entry, Receipt, ModelManager Classes. Shifted Person class into ExpenseSplitter. Updated Model class.

---
+++ src/main/java/seedu/eylah/expensesplitter/model/Entry.java
@@ -0,0 +1,103 @@
+package seedu.eylah.expensesplitter.model;
+
+import seedu.eylah.expensesplitter.model.item.Item;
+import seedu.eylah.expensesplitter.model.person.Name;
+import seedu.eylah.expensesplitter.model.person.Person;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static seedu.eylah.commons.util.CollectionUtil.requireAllNonNull;
+
+public class Entry {
+    private Item item;
+    private String[] persons;
+    private ArrayList<Person> personsList;
+
+    /**
+     * Every field must be present.
+     * Item and array of String must not be null.
+     *
+     * @param item,persons Represents the compulsory fields of an Entry object.
+     */
+    Entry(Item item, String ... persons) {
+        requireAllNonNull(item, persons);
+        this.item = item;
+        this.persons = persons;
+        this.personsList = createPersonsList(persons);
+    }
+
+    /**
+     * Creates a Person object.
+     *
+     * @param name Name of the person.
+     * @return a Person object initialised with the name;
+     */
+    private Person createPerson(String name) {
+        return new Person(new Name(name), null);
+    }
+
+    /**
+     * This method essentially converts a String array of person names
+     * to an ArrayList of Person objects.
+     *
+     * @param persons String array of person names.
+     * @return An ArrayList of Person objects associated with the Item.
+     */
+    private ArrayList<Person> createPersonsList(String ... persons) {
+        ArrayList<Person> personsList = new ArrayList<>();
+        for (String name : persons) {
+            Person person = createPerson(name);
+            personsList.add(person);
+        }
+        return personsList;
+    }
+
+    /**
+     * Returns the item in the Entry.
+     *
+     * @return the item.
+     */
+    private Item getItem() {
+        return this.item;
+    }
+
+    /**
+     * Returns the ArrayList of Persons associated with the Item.
+     *
+     * @return the ArrayList of Persons associated with the Item.
+     */
+    private ArrayList<Person> getPersonsList() {
+        return this.personsList;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Entry)) {
+            return false;
+        }
+
+        Entry otherItem = (Entry) other;
+        return otherItem.getItem().equals(getItem())
+                && otherItem.getPersonsList().equals(getPersonsList());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(item, persons);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder personsFormatted = new StringBuilder();
+        for (Person person : personsList) {
+            personsFormatted.append(person);
+        }
+        return item + "   " + personsFormatted.toString();
+    }
+}
---
+++ src/main/java/seedu/eylah/expensesplitter/model/Receipt.java
@@ -0,0 +1,76 @@
+package seedu.eylah.expensesplitter.model;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Receipt {
+
+    private ArrayList<Entry> receipt;
+
+    /**
+     * Initialises a Receipt object with an ArrayList to contain Entries.
+     */
+    public Receipt() {
+        this.receipt = new ArrayList<>();
+    }
+
+    /**
+     * Adds an Entry to the receipt.
+     *
+     * @param entry Entry to be added.
+     */
+    void addEntry(Entry entry) {
+        this.receipt.add(entry);
+    }
+
+    /**
+     * Deletes an Entry from the receipt via its index.
+     *
+     * @param index Index of Entry to be deleted.
+     */
+    void deleteEntry(int index) {
+        receipt.remove(index);
+    }
+
+    /**
+     * Returns the ArrayList of Entries.
+     *
+     * @return the ArrayList of Entries.
+     */
+    public ArrayList<Entry> getReceipt() {
+        return this.receipt;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Receipt)) {
+            return false;
+        }
+
+        Receipt otherItem = (Receipt) other;
+        return otherItem.getReceipt().equals(getReceipt());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(receipt);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        for (Entry entry : receipt) {
+            int index = receipt.indexOf(entry);
+            builder.append(index)
+                    .append(". ")
+                    .append(entry);
+        }
+        return builder.toString();
+    }
+
+}
