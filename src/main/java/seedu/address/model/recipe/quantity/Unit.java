b9281734bd8bddede6219e67b1558d4af11fbf70
Sharadh Rajaraman <r.sharadh@yahoo.com.sg>
2020-03-16 00:39:54 +0800

    Add stub Quantity and Unit classes

---
+++ src/main/java/seedu/address/model/recipe/quantity/Unit.java
@@ -0,0 +1,14 @@
+package seedu.address.model.recipe.quantity;
+
+/**
+ * A unit class to model units for each {@code Quantity}.
+ */
+
+public class Unit {
+
+    /**
+     * UnitSymbol
+     */
+    public enum UnitSymbol {
+    }
+}
