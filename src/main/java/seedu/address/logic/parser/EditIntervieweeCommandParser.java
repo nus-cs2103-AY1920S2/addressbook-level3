3b822ea52ea165b20200f0757a21a73e7826a01f
agnesnatasya <e0313565@u.nus.edu>
2020-03-15 13:43:30 +0800

    Add EditIntervieweeCommandParser class

---
+++ src/main/java/seedu/address/logic/parser/EditIntervieweeCommandParser.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.EditAttributeCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EditIntervieweeCommandParser {
+
+    public EditAttributeCommand parse(String arguments) throws ParseException {
+        String oldName = ParserUtil.parseArgumentsBeforePrefix(arguments);
+        String newName = ParserUtil.parseArgumentsBeforePrefix(arguments);
+        return new EditAttributeCommand(oldName.trim(), newName.trim());
+    }
+}
