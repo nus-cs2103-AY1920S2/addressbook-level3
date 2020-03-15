40a6d2e9ec88437d06893e4c9a0bebfa396021cf
Firzan Armani <mrfirzanarmani@gmail.com>
2020-03-15 17:55:47 +0800

    Rename BlockModel interface to Block

--- src/main/java/com/notably/model/block/Block.java
+++ src/main/java/com/notably/model/block/Block.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * API of the BlockModel component.
+ * API of the Block component.
  */
-public interface BlockModel {
+public interface Block {
     /**
      * Gets the title of a block.
      */
@@ -21,38 +21,38 @@ public interface BlockModel {
      * Gets the parent block of a block, if possible.
      * Returns an {@code Optional.empty()} if the block is the root block.
      */
-    Optional<BlockModel> getParent();
+    Optional<Block> getParent();
 
     /**
      * Gets a list of children blocks of a block.
      */
-    List<BlockModel> getChildren();
+    List<Block> getChildren();
 
     /**
      * Replaces all the children of the block with a new list of children
      */
-    void setChildren(List<BlockModel> newChildren);
+    void setChildren(List<Block> newChildren);
 
     /**
      * Finds a child block of a block, with the given input.
      */
-    Optional<BlockModel> getChild(Title title);
+    Optional<Block> getChild(Title title);
 
     /**
      * Replaces a child block of a block, that matches the title, with a new child block.
      */
-    void setChild(Title title, BlockModel newBlock);
+    void setChild(Title title, Block newBlock);
 
     /**
      * Adds a single new child to a block.
      */
-    void addChild(BlockModel newBlock);
+    void addChild(Block newBlock);
 
     /**
      * Removes a specified child block from a block.
      * @param toRemove
      */
-    void removeChild(BlockModel toRemove);
+    void removeChild(Block toRemove);
 
     /**
      * Checks if a block is a root block.
