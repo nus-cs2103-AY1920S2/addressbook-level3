79123549e0e958e24fb40074300080727de9ba6b
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-03-15 16:44:40 +0800

    Add CommandInputModel interface to model/commandInput package

---
+++ src/main/java/com/notably/model/commandInput/CommandInputModel.java
@@ -0,0 +1,30 @@
+package com.notably.model.commandInput;
+
+import javafx.beans.property.StringProperty;
+
+/**
+ * API for the CommandInputModel component.
+ *
+ * The CommandInputModel represents the data that the user sees as text within the text field of the CommandInputView
+ * as well as some supporting operations.
+ */
+public interface CommandInputModel {
+
+    /**
+     * Represents the text data that the user currently sees within the text field of the CommandInputView.
+     * @return A javafx StringProperty object.
+     */
+    StringProperty inputProperty();
+
+    /**
+     * Gets the text data saved in the CommandInputModel.
+     * @return A string corresponding to the text data saved in the CommandInputModel.
+     */
+    String getInput();
+
+    /**
+     * Sets the data for the text in the CommandInputModel.
+     * @param input A String corresponding to the new text data in the CommandInputModel.
+     */
+    void setInput(String input);
+}
