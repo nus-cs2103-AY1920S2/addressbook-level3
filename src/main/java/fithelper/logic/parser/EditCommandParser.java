a0b1f278dd892341b73a73d273c4216eae408d9a
WANG-Yuchen-Alice <wangalice2000331@icloud.com>
2020-03-16 20:47:46 +0800

    Checkstyle.

--- src/main/java/fithelper/logic/parser/EditCommandParser.java
+++ src/main/java/fithelper/logic/parser/EditCommandParser.java
@@ -40,9 +40,7 @@ public class EditCommandParser implements Parser<EditCommand> {
         if (!arePrefixesPresent(argMultimap, PREFIX_TYPE)) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE));
         }
-
-
-
+        
         try {
             Index index = ParserUtil.parseIndex(argMultimap.getValue(PREFIX_INDEX).get());
         } catch (ParseException pe) {
