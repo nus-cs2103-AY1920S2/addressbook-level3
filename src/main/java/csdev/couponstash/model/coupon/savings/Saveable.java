194d281c99ad701d9f392c6ac4378c2ef68f573d
Alden <49450743+alcen@users.noreply.github.com>
2020-03-17 02:02:38 +0800

    Add count field to Saveable

--- src/main/java/csdev/couponstash/model/coupon/savings/Saveable.java
+++ src/main/java/csdev/couponstash/model/coupon/savings/Saveable.java
@@ -13,24 +13,41 @@ public class Saveable implements Comparable<Saveable> {
             + "or null.";
 
     private final String savedItem;
+    private final int count;
 
     /**
      * Constructor for a Saveable, given a String
-     * that represents the saved item.
+     * that represents the saved item. The count of
+     * the Saveable will be set to 1, to represent
+     * the existence of a singular item.
      * @param s String representing saved item.
      */
     public Saveable(String s) {
         this.savedItem = s;
+        this.count = 1;
     }
 
     /**
-     * Checks whether this String is a valid
-     * Saveable value. Used by JsonAdaptedSaveable
+     * Constructor for a Saveable, given a String
+     * that represents the saved item as well as
+     * the number of such items
+     * @param s String representing saved item.
+     * @param count Int representing number of the
+     *              saved item.
+     */
+    public Saveable(String s, int count) {
+        this.savedItem = s;
+        this.count = count;
+    }
+
+    /**
+     * Checks whether this String and this int forms
+     * a valid Saveable value. Used by JsonAdaptedSaveable
      * and Savings classes.
      * @param s The String to be checked.
      */
-    public static boolean isValidSaveableValue(String s) {
-        return s != null && !s.isBlank();
+    public static boolean isValidSaveableValue(String s, int count) {
+        return s != null && !s.isBlank() && count > 0;
     }
 
     /**
@@ -41,28 +58,54 @@ public class Saveable implements Comparable<Saveable> {
      * @return String stored in the Saveable.
      */
     public String getValue() {
-        return this.savedItem;
+        if (this.savedItem == null) {
+            return null;
+        } else {
+            return this.count + "x " + this.savedItem;
+        }
+    }
+
+    /**
+     * Gets the count value of this Saveable as an
+     * int, which represents the quantity of the item
+     * that is represented by the Saveable.
+     * @return Int representing count, or quantity,
+     *     of this Saveable item.
+     */
+    public int getCount() {
+        return this.count;
     }
 
     @Override
     public int compareTo(Saveable p) {
-        return (int) Math.signum(this.savedItem.length() - p.savedItem.length());
+        // compare count first, before comparing length of string
+        if (this.count == p.count) {
+            return (int) Math.signum(this.savedItem.length() - p.savedItem.length());
+        } else {
+            return (int) Math.signum(this.count - p.count);
+        }
     }
 
     @Override
     public boolean equals(Object o) {
-        return o == this
-                || (o instanceof Saveable)
-                && this.savedItem.equals(((Saveable) o).savedItem);
+        if (o == this) {
+            return true;
+        } else if (o instanceof Saveable) {
+            Saveable s = (Saveable) o;
+            return this.savedItem.equals(s.savedItem)
+                    && this.count == s.count;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public int hashCode() {
-        return this.savedItem.hashCode();
+        return Objects.hash(this.savedItem, this.count);
     }
 
     @Override
     public String toString() {
-        return Objects.toString(this.savedItem);
+        return count + "x " + this.savedItem;
     }
 }
