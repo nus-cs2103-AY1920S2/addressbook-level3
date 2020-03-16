1668a5055441f7136ffb78e1d4c528a205caf156
WanXuanOng <wanxuanong1@gmail.com>
2020-03-16 18:36:55 +0800

    checkstyle

--- src/main/java/seedu/address/logic/commands/ShowCommand.java
+++ src/main/java/seedu/address/logic/commands/ShowCommand.java
@@ -1,13 +1,18 @@
 package seedu.address.logic.commands;
 
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SEMESTER;
+
+import java.util.ArrayList;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.profile.course.module.Module;
 
-import java.util.ArrayList;
 
-import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_SEMESTER;
+
+
 
 /**
  * Displays details requested by user.
