1124eb93d8e1cde75699567d75632b079c3b5d8c
Liao Meng E0148811 <lyhlm5@hotmail.com>
2020-03-17 02:06:12 +0800

    fix ChechStyle error

--- src/main/java/fithelper/ui/HelpWindow.java
+++ src/main/java/fithelper/ui/HelpWindow.java
@@ -1,5 +1,7 @@
 package fithelper.ui;
 
+import java.util.logging.Logger;
+
 import fithelper.commons.core.LogsCenter;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,8 +9,6 @@ import javafx.scene.control.Label;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.layout.AnchorPane;
-
-import java.util.logging.Logger;
 
 /**
  * Controller for a help page
@@ -22,10 +22,9 @@ public class HelpWindow extends UiPart<AnchorPane> {
     public static final String USERGUIDE_URL =
             "https://github.com/AY1920S2-CS2103-T09-4/main/blob/master/docs/UserGuide.adoc";
     public static final String HELP_MESSAGE = LIST_OF_VALID_COMMANDS + LOOK_FOR_URL + USERGUIDE_URL;
-
-    private final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
-
+    private final Logger logger = LogsCenter.getLogger(HelpWindow.class);
+    
     @FXML
     private AnchorPane helpPage;
 
