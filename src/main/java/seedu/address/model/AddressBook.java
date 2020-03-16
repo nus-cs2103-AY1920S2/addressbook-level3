fa4049157d9b16031dde37122ccbcf759c39a048
chuayijing <yijing.chua@gmail.com>
2020-03-16 23:03:54 +0800

    Add UniqueSessionList to AddressBook

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -7,13 +7,16 @@ import java.util.List;
 import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
+import seedu.address.model.session.Session;
+import seedu.address.model.session.UniqueSessionList;
 
 /**
  * Wraps all data at the address-book level
- * Duplicates are not allowed (by .isSamePerson comparison)
+ * Duplicates are not allowed (by .isSameSession comparison)
  */
 public class AddressBook implements ReadOnlyAddressBook {
 
+    private final UniqueSessionList sessions;
     private final UniquePersonList persons;
 
     /*
@@ -24,6 +27,7 @@ public class AddressBook implements ReadOnlyAddressBook {
      *   among constructors.
      */
     {
+        sessions = new UniqueSessionList();
         persons = new UniquePersonList();
     }
 
@@ -48,12 +52,60 @@ public class AddressBook implements ReadOnlyAddressBook {
     }
 
     /**
+     * Replaces the contents of the session list with {@code sessions}.
+     * {@code sessions} must not contain duplicate sessions.
+     */
+    public void setSessions(List<Session> sessions) {
+        this.sessions.setSessions(sessions);
+    }
+
+    /**
      * Resets the existing data of this {@code AddressBook} with {@code newData}.
      */
     public void resetData(ReadOnlyAddressBook newData) {
         requireNonNull(newData);
 
         setPersons(newData.getPersonList());
+        setSessions(newData.getSessionList());
+    }
+
+    //// session-level operations
+
+    /**
+     * Returns true if a session with the same identity as {@code session} exists in the address book.
+     */
+    public boolean hasSession(Session session) {
+        requireNonNull(session);
+        return sessions.contains(session);
+    }
+
+    /**
+     * Adds a session to the address book.
+     * The session must not already exist in the address book.
+     */
+    public void addSession(Session s) {
+        sessions.add(s);
+    }
+
+    /**
+     * Replaces the given session {@code target} in the list with {@code editedSession}.
+     * {@code target} must exist in the address book.
+     * The session identity of {@code editedSession} must not be the same as another
+     * existing session in the address book.
+     */
+    public void setSession(Session target, Session editedSession) {
+        requireNonNull(editedSession);
+
+        sessions.setSession(target, editedSession);
+    }
+
+    /**
+     * Returns true if a session with the same identity as {@code session} exists in the address book.
+     * Removes {@code session} from this {@code AddressBook}.
+     * {@code session} must exist in the address book.
+     */
+    public void removeSession(Session session) {
+        sessions.remove(session);
     }
 
     //// person-level operations
@@ -104,6 +156,11 @@ public class AddressBook implements ReadOnlyAddressBook {
     @Override
     public ObservableList<Person> getPersonList() {
         return persons.asUnmodifiableObservableList();
+    }
+
+    @Override
+    public ObservableList<Session> getSessionList() {
+        return sessions.asUnmodifiableObservableList();
     }
 
     @Override
--- src/main/java/seedu/address/model/ReadOnlyAddressBook.java
+++ src/main/java/seedu/address/model/ReadOnlyAddressBook.java
@@ -2,6 +2,7 @@ package seedu.address.model;
 
 import javafx.collections.ObservableList;
 import seedu.address.model.person.Person;
+import seedu.address.model.session.Session;
 
 /**
  * Unmodifiable view of an address book
@@ -9,9 +10,14 @@ import seedu.address.model.person.Person;
 public interface ReadOnlyAddressBook {
 
     /**
-     * Returns an unmodifiable view of the persons list.
-     * This list will not contain any duplicate persons.
+     * Returns an unmodifiable view of the person list.
+     * This list will not contain any duplicate person.
      */
     ObservableList<Person> getPersonList();
 
+    /**
+     * Returns an unmodifiable view of the session list.
+     * This list will not contain any duplicate session.
+     */
+    ObservableList<Session> getSessionList();
 }
