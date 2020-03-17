b6cd6102723990bec3e904bc00fa43563e863500
watsheldon <chinaempire.cjw@gmail.com>
2020-03-18 05:30:46 +0800

    add DiaryBook

--- src/main/java/seedu/address/model/diary/DiaryBook.java
+++ src/main/java/seedu/address/model/diary/DiaryBook.java
@@ -4,4 +4,30 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class DiaryBook {
+    private List<DiaryEntry> diaryEntries;
+
+    public DiaryBook() {
+        this.diaryEntries = new ArrayList<>();
+    }
+
+    public DiaryBook(List<DiaryEntry> diaryEntries) {
+        this.diaryEntries = diaryEntries;
+    }
+
+    public void addEntry(DiaryEntry diaryEntry) {
+        this.diaryEntries.add(diaryEntry);
+    }
+
+    public String showLog() {
+        StringBuilder sb = new StringBuilder();
+        int entryId = 1;
+        for (DiaryEntry entry : diaryEntries) {
+            sb.append(entryId++).append(entry.getHeading()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    public String showEntry(int entryId) {
+        return diaryEntries.get(entryId - 1).toString();
+    }
 }
--- src/main/java/seedu/address/model/diary/DiaryEntry.java
+++ src/main/java/seedu/address/model/diary/DiaryEntry.java
@@ -9,15 +9,12 @@ import seedu.address.model.diary.mood.Mood;
 import seedu.address.model.diary.weather.Weather;
 
 public class DiaryEntry {
-    private static int NUM_OF_ENTRIES = 0;
-    private int entryId;
     private LocalDate date;
     private Optional<Weather> weather;
     private Optional<Mood> mood;
     private String entryContent;
 
-    private DiaryEntry(int entryId, LocalDate date, Weather weather, Mood mood, String entryContent) {
-        this.entryId = entryId;
+    private DiaryEntry(LocalDate date, Weather weather, Mood mood, String entryContent) {
         this.date = date;
         this.weather = Optional.ofNullable(weather);
         this.mood = Optional.ofNullable(mood);
@@ -25,16 +22,21 @@ public class DiaryEntry {
     }
 
     public DiaryEntry(String entryContent) {
-        this(NUM_OF_ENTRIES + 1, LocalDate.now(), null, null, entryContent);
-        NUM_OF_ENTRIES++;
+        this(LocalDate.now(), null, null, entryContent);
+    }
+
+    public String getHeading() {
+        String heading = "";
+        heading += " DATE: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + " | ";
+        heading += "WEATHER: " + (weather.isPresent() ? weather.get().toString() : "N.A.") + " | ";
+        heading += "MOOD: " + (mood.isPresent() ? mood.get().toString() : "N.A.") + " | ";
+        return heading + "\n";
     }
 
     @Override
     public String toString() {
         String dairyDisplay = "";
-        dairyDisplay += " DATE: " + date.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + "\n";
-        dairyDisplay += "WEATHER: " + (weather.isPresent() ? weather.get().toString() : "N.A.") + "\n";
-        dairyDisplay += "MOOD: " + (mood.isPresent() ? mood.get().toString() : "N.A.") + "\n";
+        dairyDisplay += getHeading();
         dairyDisplay += "_".repeat(50) + "\n";
         dairyDisplay += entryContent + "\n";
         return dairyDisplay;
