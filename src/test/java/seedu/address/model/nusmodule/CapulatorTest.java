8d6849d85db0d2f4ca980ffb97b5600a9d6fdd93
Kangwkk <846199604@qq.com>
2020-03-17 11:46:20 +0800

    add in test cases for Grade class

---
+++ src/test/java/seedu/address/model/nusmodule/CapulatorTest.java
@@ -0,0 +1,12 @@
+package seedu.address.model.nusmodule;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CapulatorTest {
+
+    @Test
+    void calculateCap() {
+    }
+}
---
+++ src/test/java/seedu/address/model/nusmodule/NusModuleTest.java
@@ -0,0 +1,12 @@
+package seedu.address.model.nusmodule;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NusModuleTest {
+
+    @Test
+    void getGradePoint() {
+    }
+}
