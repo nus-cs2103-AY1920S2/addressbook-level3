f5df08157c3db2247715176897c1eb3f3a226465
johannagwan <johannagwan@gmail.com>
2020-03-17 21:52:34 +0800

    Update clearResponseText method

--- src/main/java/com/notably/model/suggestion/SuggestionModelImpl.java
+++ src/main/java/com/notably/model/suggestion/SuggestionModelImpl.java
@@ -37,8 +37,8 @@ public class SuggestionModelImpl implements SuggestionModel {
     }
 
     @Override
-    public void clearCommandInputText() {
-        commandInputText = "";
+    public void clearResponseText() {
+        responseTextProperty.setValue(Optional.of(""));
     }
 
     @Override
