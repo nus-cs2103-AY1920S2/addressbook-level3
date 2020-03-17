e5591061aae18356cca7095619b8a81b7458d4d9
Branson <branson.ng@u.nus.edu>
2020-03-17 13:03:26 +0800

    Format Code

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -53,7 +53,7 @@ public class JsonUserPrefsStorageTest {
     public void readUserPrefs_fileInOrder_successfullyRead() throws DataConversionException {
         UserPrefs expected = getTypicalUserPrefs();
         UserPrefs actual = readUserPrefs("TypicalUserPref.json").get();
-        
+
         assertEquals(expected, actual);
     }
 
@@ -76,7 +76,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(new GuiSettings(1000, 500, 300, 100));
-        userPrefs.setTaskListFilePath(Paths.get("data","tasklist.json"));
+        userPrefs.setTaskListFilePath(Paths.get("data", "tasklist.json"));
         return userPrefs;
     }
 
