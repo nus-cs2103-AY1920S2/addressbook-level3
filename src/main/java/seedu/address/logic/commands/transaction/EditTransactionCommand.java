e4bd09ebc040a10e70ffdd966c1504cf2fb67e9a
YingxuH <yingxu.he1998@gmail.com>
2020-03-18 01:40:15 +0800

    add edit command body

---
+++ src/main/java/seedu/address/logic/commands/transaction/EditTransactionCommand.java
@@ -0,0 +1,232 @@
+package seedu.address.logic.commands.transaction;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CUSTOMER;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATETIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MONEY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PRODUCT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_QUANTITY;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TRANS_DESCIPTION;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_TRANSACTIONS;
+
+import java.util.List;
+import java.util.Optional;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.util.CollectionUtil;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.product.Description;
+import seedu.address.model.transaction.DateTime;
+import seedu.address.model.transaction.Money;
+import seedu.address.model.transaction.Transaction;
+import seedu.address.model.util.Quantity;
+
+/**
+ * Edits the details of an existing transaction in the address book.
+ */
+public class EditTransactionCommand extends Command {
+
+    public static final String COMMAND_WORD = "editt";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the transaction identified "
+            + "by the index number used in the displayed transaction list. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "[" + PREFIX_CUSTOMER + "CUSTOMER] "
+            + "[" + PREFIX_PRODUCT + "PRODUCT] "
+            + "[" + PREFIX_DATETIME + "DATETIME] "
+            + "[" + PREFIX_QUANTITY + "QUANTITY] "
+            + "[" + PREFIX_MONEY + "MONEY] "
+            + "[" + PREFIX_TRANS_DESCIPTION + "DESCRIPTION] \n"
+            + "Example: " + COMMAND_WORD
+            + PREFIX_CUSTOMER + "Bob "
+            + PREFIX_PRODUCT + "WaterMelon "
+            + PREFIX_DATETIME + "2020-02-20 10:00 "
+            + PREFIX_QUANTITY + "30"
+            + PREFIX_MONEY + "30 "
+            + PREFIX_TRANS_DESCIPTION + "under discount ";
+
+    public static final String MESSAGE_EDIT_TRANSACTION_SUCCESS = "Edited Transaction: %1$s";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_TRANSACTION = "This transaction already exists in the transaction list.";
+
+    private final Index index;
+    private final EditTransactionDescriptor editTransactionDescriptor;
+
+    /**
+     * @param index of the transaction in the filtered transaction list to edit
+     * @param editTransactionDescriptor details to edit the transaction with
+     */
+    public EditTransactionCommand(Index index, EditTransactionDescriptor editTransactionDescriptor) {
+        requireNonNull(index);
+        requireNonNull(editTransactionDescriptor);
+
+        this.index = index;
+        this.editTransactionDescriptor = new EditTransactionDescriptor(editTransactionDescriptor);
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        List<Transaction> lastShownList = model.getFilteredTransactionList();
+
+        if (index.getZeroBased() >= lastShownList.size()) {
+            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        }
+
+        Transaction transactionToEdit = lastShownList.get(index.getZeroBased());
+        Transaction editedTransaction = createEditedTransaction(transactionToEdit, editTransactionDescriptor);
+
+        if (!transactionToEdit.isSameTransaction(editedTransaction) && model.hasTransaction(editedTransaction)) {
+            throw new CommandException(MESSAGE_DUPLICATE_TRANSACTION);
+        }
+
+        model.setTransaction(transactionToEdit, editedTransaction);
+        model.updateFilteredTransactionList(PREDICATE_SHOW_ALL_TRANSACTIONS);
+        return new CommandResult(String.format(MESSAGE_EDIT_TRANSACTION_SUCCESS, editedTransaction));
+    }
+
+    /**
+     * Creates and returns a {@code Transaction} with the details of {@code transactionToEdit}
+     * edited with {@code editTransactionDescriptor}.
+     */
+    private static Transaction createEditedTransaction(Transaction transactionToEdit, EditTransactionDescriptor editTransactionDescriptor) {
+        assert transactionToEdit != null;
+
+        Description updatedDescription = editTransactionDescriptor.getDescription().orElse(transactionToEdit.getDescription());
+        Price updatedPrice = editTransactionDescriptor.getPrice().orElse(transactionToEdit.getPrice());
+        Quantity updatedQuantity = editTransactionDescriptor.getQuantity().orElse(transactionToEdit.getQuantity());
+        Sales updatedSales = editTransactionDescriptor.getSales().orElse(transactionToEdit.getSales());
+        System.out.println("createEditedTransaction " + transactionToEdit);
+        return new Transaction(updatedDescription, updatedPrice, updatedQuantity, updatedSales);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof EditTransactionCommand)) {
+            return false;
+        }
+
+        // state check
+        EditTransactionCommand e = (EditTransactionCommand) other;
+        return index.equals(e.index)
+                && editTransactionDescriptor.equals(e.editTransactionDescriptor);
+    }
+
+    /**
+     * Stores the details to edit the transaction with. Each non-empty field value will replace the
+     * corresponding field value of the transaction.
+     */
+    public static class EditTransactionDescriptor {
+        private String customer;
+        private String product;
+        private DateTime dateTime;
+        private Quantity quantity;
+        private Money money;
+        private Description description;
+
+        public EditTransactionDescriptor() {}
+
+        /**
+         * Copy constructor.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public EditTransactionDescriptor(EditTransactionDescriptor toCopy) {
+            setCustomer(toCopy.customer);
+            setProduct(toCopy.product);
+            setDateTime(toCopy.dateTime);
+            setQuantity(toCopy.quantity);
+            setMoney(toCopy.money);
+            setDescription(toCopy.description);
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return CollectionUtil.isAnyNonNull(customer, product, dateTime, quantity,
+                    money, description);
+        }
+
+        public void setCustomer(String customer) {
+            this.customer = customer;
+        }
+
+        public void setProduct(String product) {
+            this.product = product;
+        }
+
+        public void setDateTime(DateTime dateTime) {
+            this.dateTime = dateTime;
+        }
+
+        public void setQuantity(Quantity quantity) {
+            this.quantity = quantity;
+        }
+
+        public void setMoney(Money money) {
+            this.money = money
+        }
+
+        public void setDescription(Description description) {
+            this.description = description;
+        }
+
+        public Optional<String> getCustomer() {
+            return Optional.ofNullable(customer);
+        }
+
+
+        public Optional<Quantity> getProduct() {
+            return Optional.ofNullable(product);
+        }
+
+        public Optional<DateTime> getDateTime() {
+            return Optional.ofNullable(dateTime);
+        }
+
+        public Optional<Description> getDescription() {
+            return Optional.ofNullable(description);
+        }
+
+        public Optional<Quantity> getQuantity() {
+            return Optional.ofNullable(quantity);
+        }
+
+        public Optional<Modey> getDescription() {
+            return Optional.ofNullable(description);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            // short circuit if same object
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof EditTransactionDescriptor)) {
+                return false;
+            }
+
+            // state check
+            EditTransactionDescriptor e = (EditTransactionDescriptor) other;
+
+            return getDescription().equals(e.getDescription())
+                    && getPrice().equals(e.getPrice())
+                    && getQuantity().equals(e.getQuantity())
+                    && getSales().equals(e.getSales());
+        }
+    }
+}
+
