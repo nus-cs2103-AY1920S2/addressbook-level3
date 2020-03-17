a9011667a8c69e85b8cfb7ca81414457496e0787
Kangwkk <846199604@qq.com>
2020-03-17 11:59:22 +0800

    add in test cases for class NusModule

--- src/test/java/seedu/address/model/nusmodule/NusModuleTest.java
+++ src/test/java/seedu/address/model/nusmodule/NusModuleTest.java
@@ -1,6 +1,8 @@
 package seedu.address.model.nusmodule;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +10,9 @@ class NusModuleTest {
 
     @Test
     void getGradePoint() {
+        NusModule module1 = new NusModule(new ModuleCode("CS2030"), 4, false, Optional.of(Grade.A));
+        assertEquals(5.0, module1.getGradePoint());
+        NusModule module2 = new NusModule(new ModuleCode("CS2030"), 4, false, Optional.of(Grade.C));
+        assertEquals(2.0, module2.getGradePoint());
     }
 }
