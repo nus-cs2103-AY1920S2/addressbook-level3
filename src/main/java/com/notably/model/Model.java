e9ab5594fd00e01aebb49d3677a260f575aa7e86
johannagwan <johannagwan@gmail.com>
2020-03-18 02:54:46 +0800

    Fix checkstyle in Model interface

--- src/main/java/com/notably/model/Model.java
+++ src/main/java/com/notably/model/Model.java
@@ -4,9 +4,8 @@ import java.nio.file.Path;
 import java.util.function.Predicate;
 
 import com.notably.commons.core.GuiSettings;
-import com.notably.model.suggestion.SuggestionModel;
-
 import com.notably.model.commandinput.CommandInputModel;
+import com.notably.model.suggestion.SuggestionModel;
 
 import javafx.collections.ObservableList;
 
