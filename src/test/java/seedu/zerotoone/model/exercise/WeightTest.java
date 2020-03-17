66360c382ee503901b3d535a2a538ad83c928c56
wongchishan <wongchishan99@gmail.com>
2020-03-17 23:14:47 +0800

    Fix import statements due to change in directory name to zerotoone

---
+++ src/test/java/seedu/zerotoone/model/exercise/WeightTest.java
@@ -0,0 +1,39 @@
+package seedu.zerotoone.model.exercise;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.zerotoone.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class WeightTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Weight(null));
+    }
+
+    @Test
+    public void constructor_invalidWeight_throwsIllegalArgumentException() {
+        String invalidWeight = "";
+        assertThrows(IllegalArgumentException.class, () -> new Weight(invalidWeight));
+    }
+
+    @Test
+    public void isValidWeight() {
+        // null phone number
+        assertThrows(NullPointerException.class, () -> Weight.isValidWeight(null));
+
+        // invalid phone numbers
+        assertFalse(Weight.isValidWeight("")); // empty string
+        assertFalse(Weight.isValidWeight(" ")); // spaces only
+        assertFalse(Weight.isValidWeight("phone")); // non-numeric
+        assertFalse(Weight.isValidWeight("9011p041")); // alphabets within digits
+        assertFalse(Weight.isValidWeight("9312 1534")); // spaces within digits
+
+        // valid phone numbers
+        assertTrue(Weight.isValidWeight("911"));
+        assertTrue(Weight.isValidWeight("93121534"));
+        assertTrue(Weight.isValidWeight("124293842033123")); // long numbers
+    }
+}
