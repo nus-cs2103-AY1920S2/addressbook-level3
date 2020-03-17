8388b7b23c7d834696a0958c91857af82a158fb9
johan.kok <johan.kok@grabtaxi.com>
2020-03-17 16:16:43 +0800

    AppLogicManager and AppStorage implementation

--- src/main/java/seedu/address/logic/AppLogicManager.java
+++ src/main/java/seedu/address/logic/AppLogicManager.java
@@ -1,11 +1,43 @@
 package seedu.address.logic;
 
-import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.logic.parser.exceptions.ParseException;
+import javafx.collections.ObservableList;
+import seedu.address.logic.conditions.Conditions;
+import seedu.address.storage.AppStorage;
 
-public class BluetoothLogicManager {
-    public 
+public class AppLogicManager<T, M> implements AppLogic<T> {
+    private AppStorage dao;
 
+    /**
+     * Asserts that the user must always declare type M which is subclass of {@code AppStorage}
+     *
+     * @param   dao        Data access object implementation
+     * @throws  Exception
+     */
+    public AppLogicManager(Class<M> dao) throws Exception {
+        M tmpObj = dao.getDeclaredConstructor().newInstance();
+        assert (tmpObj instanceof AppStorage);
+        this.dao = (AppStorage) tmpObj;
+    }
 
+    /**
+     * Fetches all pings from the database
+     *
+     * @return  ObservableList<T>   List of all pings for rendering to UI
+     */
+    @Override
+    public ObservableList<T> getPings() {
+        return null;
+    }
+
+    /**
+     * Fetches all pings that satisfies a conditional filter
+     *
+     * @param   cond                {@code seedu.address.conditions.Conditions Class} Conditional class
+     * @return  ObservableList<T>   List of all pings in condition for rendering to UI
+     */
+    @Override
+    public ObservableList<T> filterPings(Conditions<T> cond) {
+        return null;
+    }
 }
+
