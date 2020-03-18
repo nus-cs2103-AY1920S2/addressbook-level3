b74aeb22464056a75f4add671c419df0cb24f306
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-03-18 16:45:10 +0800

    Add getters to Session

--- src/main/java/seedu/address/model/session/Session.java
+++ src/main/java/seedu/address/model/session/Session.java
@@ -86,10 +86,24 @@ public class Session {
     }
 
     /**
+     * Sets the start time of the session.
+     */
+    public void setStartDateTime(LocalDateTime startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    /**
      * Returns the end time of the session.
      */
     public LocalDateTime getEndDateTime() {
         return this.endDateTime;
+    }
+
+    /**
+     * Sets the end time of the session.
+     */
+    public void setEndDateTime(LocalDateTime endDateTime) {
+        this.endDateTime = endDateTime;
     }
 
     /**
@@ -100,10 +114,24 @@ public class Session {
     }
 
     /**
+     * Sets whether the session is a recurring session.
+     */
+    public void setRecurring(boolean recurring) {
+        isRecurring = recurring;
+    }
+
+    /**
      * Returns the module code associated with this session.
      */
     public String getModuleCode() {
         return this.moduleCode;
+    }
+
+    /**
+     * Sets the module code associated with this session.
+     */
+    public void setModuleCode(String moduleCode) {
+        this.moduleCode = moduleCode;
     }
 
     /**
@@ -114,10 +142,24 @@ public class Session {
     }
 
     /**
+     * Sets the type of session.
+     */
+    public void setType(SessionType type) {
+        this.type = type;
+    }
+
+    /**
      * Returns the description of the session.
      */
     public String getDescription() {
         return this.description;
+    }
+
+    /**
+     * Sets the description of the session.
+     */
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     /**
