b16d835596e9ca27c6df2cd9cbf88bba8e3e7591
Jia Zheng <ljiazh3ng@gmail.com>
2020-03-18 15:31:26 +0800

    CheckStyle

--- src/test/java/com/notably/logic/parser/NotablyParserTest.java
+++ src/test/java/com/notably/logic/parser/NotablyParserTest.java
@@ -1,19 +1,36 @@
-//TODO: To be enabled or changed when refactoring is completed
-//package com.notably.logic.parser;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import com.notably.logic.commands.NewCommand;
-//
-//public class NotablyParserTest {
-//    private final NotablyParser parser = new NotablyParser();
-//
-//    @Test
-//    public void NewCommandTest() throws Exception{
-//        NewCommand command = (NewCommand) parser.parseCommand(
-//                NewCommand.COMMAND_WORD + " " + CliSyntax.PREFIX_TITLE + " CS2103" + CliSyntax.PREFIX_BODY + " Hi");
-//        assertEquals(new NewCommand(new BlockStub("CS2103", "Hi"), false), command);
-//    }
-//}
+package com.notably.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.notably.logic.commands.Command;
+import com.notably.logic.commands.DeleteCommand;
+import com.notably.logic.commands.NewCommand;
+import com.notably.logic.commands.OpenCommand;
+
+
+public class NotablyParserTest {
+    private final NotablyParser parser = new NotablyParser();
+
+    @Test
+    public void parseCommand_newCommandInput_newCommand() throws Exception {
+        Command command = parser.parseCommand("new -t CS2103 -b Hi");
+
+        assertTrue(command instanceof NewCommand);
+    }
+
+    @Test
+    public void parseCommand_openCommandInput_openCommand() throws Exception {
+        Command command = parser.parseCommand("open -t CS2103");
+
+        assertTrue(command instanceof OpenCommand);
+    }
+
+    @Test
+    public void parseCommand_deleteCommandInput_deleteCommand() throws Exception {
+        Command command = parser.parseCommand("delete -t CS2103");
+
+        assertTrue(command instanceof DeleteCommand);
+    }
+}
