58eb12a6cd06268d07eb76db406f51d92cf303ab
PangKimJin <pangkimjin1@gmail.com>
2020-03-18 15:12:41 +0800

    corrected Ben's refactoring mistake's affected dependencies in the EditCommandParser.java file.

--- src/main/java/seedu/expensela/logic/parser/EditCommandParser.java
+++ src/main/java/seedu/expensela/logic/parser/EditCommandParser.java
@@ -6,7 +6,7 @@ import static seedu.expensela.logic.parser.CliSyntax.*;
 
 import seedu.expensela.commons.core.index.Index;
 import seedu.expensela.logic.commands.EditCommand;
-import seedu.expensela.logic.commands.EditCommand.editTransaction;
+import seedu.expensela.logic.commands.EditCommand.EditTransactionDescriptor;
 import seedu.expensela.logic.parser.exceptions.ParseException;
 
 /**
@@ -32,28 +32,28 @@ public class EditCommandParser implements Parser<EditCommand> {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditCommand.MESSAGE_USAGE), pe);
         }
 
-        EditCommand.editTransaction editTransaction = new editTransaction();
+        EditCommand.EditTransactionDescriptor editTransactionDescriptor = new EditTransactionDescriptor();
         if (argMultimap.getValue(PREFIX_NAME).isPresent()) {
-            editTransaction.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
+            editTransactionDescriptor.setName(ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()));
         }
         if (argMultimap.getValue(PREFIX_AMOUNT).isPresent()) {
-            editTransaction.setAmount(ParserUtil.parseAmount(argMultimap.getValue(PREFIX_AMOUNT).get()));
+            editTransactionDescriptor.setAmount(ParserUtil.parseAmount(argMultimap.getValue(PREFIX_AMOUNT).get()));
         }
         if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
-            editTransaction.setDate(ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get()));
+            editTransactionDescriptor.setDate(ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get()));
         }
         if (argMultimap.getValue(PREFIX_REMARK).isPresent()) {
-            editTransaction.setRemark(ParserUtil.parseRemark(argMultimap.getValue(PREFIX_REMARK).get()));
+            editTransactionDescriptor.setRemark(ParserUtil.parseRemark(argMultimap.getValue(PREFIX_REMARK).get()));
         }
         if (argMultimap.getValue(PREFIX_CATEGORY).isPresent()) {
-            editTransaction.setCategory(ParserUtil.parseCategory(argMultimap.getValue(PREFIX_CATEGORY).get()));
+            editTransactionDescriptor.setCategory(ParserUtil.parseCategory(argMultimap.getValue(PREFIX_CATEGORY).get()));
         }
 
-        if (!editTransaction.isAnyFieldEdited()) {
+        if (!editTransactionDescriptor.isAnyFieldEdited()) {
             throw new ParseException(EditCommand.MESSAGE_NOT_EDITED);
         }
 
-        return new EditCommand(index, editTransaction);
+        return new EditCommand(index, editTransactionDescriptor);
     }
 
 }
