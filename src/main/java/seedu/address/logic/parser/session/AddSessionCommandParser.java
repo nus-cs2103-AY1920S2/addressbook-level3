cdfc7d96ad6468e5f3a271309b6dcd1631edfc19
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-03-18 16:47:46 +0800

    Add SessionCommandParser for adding sessions

---
+++ src/main/java/seedu/address/logic/parser/session/AddSessionCommandParser.java
@@ -0,0 +1,76 @@
+package seedu.address.logic.parser.session;
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ENDTIME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MOD_CODE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTES;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_RECUR;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_SESSION_TYPE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_STARTTIME;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import seedu.address.logic.commands.AddSessionCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.session.Session;
+
+/**
+ * Parses input arguments and creates a new AddSessionCommand object
+ */
+public class AddSessionCommandParser implements Parser<AddSessionCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddSessionCommand
+     * and returns an AddSessionCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddSessionCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_STARTTIME, PREFIX_ENDTIME,
+                PREFIX_DATE, PREFIX_RECUR, PREFIX_MOD_CODE, PREFIX_SESSION_TYPE, PREFIX_NOTES);
+
+        LocalDate date = LocalDate.now(); // Arbitrary default value. Will be overwritten by AddSessionCommand
+        Session sessionToAdd = new Session();
+
+        if (argMultimap.getValue(PREFIX_DATE).isPresent()) {
+            date = ParserUtil.parseDate(argMultimap.getValue(PREFIX_DATE).get());
+        }
+
+        if (argMultimap.getValue(PREFIX_STARTTIME).isPresent()) {
+            LocalTime startTime = ParserUtil.parseTime(argMultimap.getValue(PREFIX_STARTTIME).get());
+            sessionToAdd.setStartDateTime(LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(),
+                    startTime.getHour(), startTime.getMinute(), startTime.getSecond()));
+        }
+
+        if (argMultimap.getValue(PREFIX_ENDTIME).isPresent()) {
+            LocalTime endTime = ParserUtil.parseTime(argMultimap.getValue(PREFIX_ENDTIME).get());
+            sessionToAdd.setEndDateTime(LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(),
+                    endTime.getHour(), endTime.getMinute(), endTime.getSecond()));
+        }
+
+        if (argMultimap.getValue(PREFIX_RECUR).isPresent()) {
+            sessionToAdd.setRecurring(argMultimap.getValue(PREFIX_RECUR).isPresent());
+        }
+
+        if (argMultimap.getValue(PREFIX_MOD_CODE).isPresent()) {
+            sessionToAdd.setModuleCode(ParserUtil.parseValue(argMultimap.getValue(PREFIX_MOD_CODE).get()));
+        }
+
+        if (argMultimap.getValue(PREFIX_SESSION_TYPE).isPresent()) {
+            sessionToAdd.setType(
+                    ParserUtil.parseSessionType(argMultimap.getValue(PREFIX_SESSION_TYPE).get()));
+        }
+
+        if (argMultimap.getValue(PREFIX_NOTES).isPresent()) {
+            sessionToAdd.setDescription(ParserUtil.parseValue(argMultimap.getValue(PREFIX_NOTES).get()));
+        }
+
+        return new AddSessionCommand(sessionToAdd);
+    }
+}
