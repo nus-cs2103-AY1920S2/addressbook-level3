e4feee26757b7e3aba97b0d0176ced1a67d27cb3
khsc96 <khsc96@gmail.com>
2020-03-18 18:28:29 +0800

    SearchCommandParser: Refactor method to not have NPath Complexity

--- src/main/java/seedu/address/logic/parser/SearchCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SearchCommandParser.java
@@ -11,7 +11,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TID;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TYPE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_WAREHOUSE;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
@@ -43,51 +42,51 @@ public class SearchCommandParser implements Parser<SearchCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, SearchCommand.MESSAGE_USAGE));
         }
 
-        List<String> keywords = new ArrayList<>();
+        List<String> keywords;
         if (!argMultimap.getPreamble().isEmpty()) {
             keywords = Arrays.asList(argMultimap.getPreamble().split("\\s+"));
             return new SearchCommand(new OrderContainsKeywordsPredicate(keywords));
         }
 
         KeywordContainsOrderPrefix keywordContainsOrderPrefix = new KeywordContainsOrderPrefix(argMultimap);
-
+        StringBuilder keywordsString = new StringBuilder();
         if (keywordContainsOrderPrefix.getHasTid()) {
-            keywords = addPrefixKeywordsToList(keywords, argMultimap, PREFIX_TID);
+            keywordsString = addPrefixKeywordsToList(keywordsString, argMultimap, PREFIX_TID);
         }
 
         if (keywordContainsOrderPrefix.getHasName()) {
-            keywords = addPrefixKeywordsToList(keywords, argMultimap, PREFIX_NAME);
+            keywordsString = addPrefixKeywordsToList(keywordsString, argMultimap, PREFIX_NAME);
         }
 
         if (keywordContainsOrderPrefix.getHasPhone()) {
-            keywords = addPrefixKeywordsToList(keywords, argMultimap, PREFIX_PHONE);
+            keywordsString = addPrefixKeywordsToList(keywordsString, argMultimap, PREFIX_PHONE);
         }
 
         if (keywordContainsOrderPrefix.getHasAddress()) {
-            keywords = addPrefixKeywordsToList(keywords, argMultimap, PREFIX_ADDRESS);
+            keywordsString = addPrefixKeywordsToList(keywordsString, argMultimap, PREFIX_ADDRESS);
         }
 
         if (keywordContainsOrderPrefix.getHasTimeStamp()) {
-            keywords = addPrefixKeywordsToList(keywords, argMultimap, PREFIX_DELIVERY_TIMESTAMP);
+            keywordsString = addPrefixKeywordsToList(keywordsString, argMultimap, PREFIX_DELIVERY_TIMESTAMP);
         }
 
         if (keywordContainsOrderPrefix.getHasWarehouse()) {
-            keywords = addPrefixKeywordsToList(keywords, argMultimap, PREFIX_WAREHOUSE);
+            keywordsString = addPrefixKeywordsToList(keywordsString, argMultimap, PREFIX_WAREHOUSE);
         }
 
         if (keywordContainsOrderPrefix.getHasComment()) {
-            keywords = addPrefixKeywordsToList(keywords, argMultimap, PREFIX_COMMENT);
+            keywordsString = addPrefixKeywordsToList(keywordsString, argMultimap, PREFIX_COMMENT);
         }
 
         if (keywordContainsOrderPrefix.getHasCod()) {
-            keywords = addPrefixKeywordsToList(keywords, argMultimap, PREFIX_COD);
+            keywordsString = addPrefixKeywordsToList(keywordsString, argMultimap, PREFIX_COD);
         }
 
         if (keywordContainsOrderPrefix.getHasItemType()) {
-            keywords = addPrefixKeywordsToList(keywords, argMultimap, PREFIX_TYPE);
+            keywordsString = addPrefixKeywordsToList(keywordsString, argMultimap, PREFIX_TYPE);
         }
 
-
+        keywords = Arrays.asList(keywordsString.toString().split("\\s+"));
         return new SearchCommand(new OrderContainsKeywordsPredicate(keywords, keywordContainsOrderPrefix));
     }
 
@@ -100,23 +99,22 @@ public class SearchCommandParser implements Parser<SearchCommand> {
     }
 
     /**
-     * Add keywords tagged to the prefix given by the user to a list.
-     * Returns a list with all the keywords tagged to the prefix appended to the given list in
-     * {@param givenListOfString}.
-     * @param givenListOfString Existing list of keywords.
-     * @param argumentMultimap An {@code ArgumentMultimap} object.
-     * @param prefix Keywords of this prefix will be added into the output list.
-     * @return A list of string of the updated keywords.
+     * Add keywords tagged to the prefix given by the user to a StringBuilder.
+     * Returns a StringBuilder with all the keywords tagged to the prefix appended to the given StringBuilder of
+     * {@param givenKeyWords}.
+     *
+     * @param givenKeywords StringBuilder of all or partial of the keywords given by the user.
+     * @param argumentMultimap An {@code ArgumentMultimap} object with keywords tagged to prefix.
+     * @param prefix Prefix object to indicate which keywords to extract out from the argumentMultiMap.
+     * @return Returns a StringBuilder object with all the keywords tagged to the prefix appneded to the StringBuilder.
      */
-    private static List<String> addPrefixKeywordsToList(List<String> givenListOfString,
+    private static StringBuilder addPrefixKeywordsToList(StringBuilder givenKeywords,
                                                         ArgumentMultimap argumentMultimap, Prefix prefix) {
-        List<String> copyKeywordsList = givenListOfString;
-        List<String> currentPrefixListOfKeywords = (argumentMultimap.getAllValues(prefix));
-        currentPrefixListOfKeywords
-            .stream()
-            .map(each -> each.split("\\s+"))
-            .flatMap(Arrays::stream)
-            .forEach(copyKeywordsList::add);
-        return copyKeywordsList;
+        StringBuilder copyKeywords = givenKeywords;
+        List<String> currentPrefixListOfKeywords = argumentMultimap.getAllValues(prefix);
+        for (String each : currentPrefixListOfKeywords) {
+            copyKeywords.append(each);
+        }
+        return copyKeywords;
     }
 }
