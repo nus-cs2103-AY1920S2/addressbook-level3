161fce660153b434f7e36ed0291fe744dd736fc1
Aloysius Chan <alloystory@gmail.com>
2020-03-18 22:05:33 +0800

    edit clisyntax

--- src/main/java/seedu/zerotoone/logic/parser/CliSyntax.java
+++ src/main/java/seedu/zerotoone/logic/parser/CliSyntax.java
@@ -11,5 +11,14 @@ public class CliSyntax {
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
-
+    
+    // ZeroToOne
+    public static final Prefix PREFIX_EXERCISE_NAME = new Prefix("e/");
+    public static final Prefix PREFIX_WORKOUT_NAME = new Prefix("w/");
+    public static final Prefix PREFIX_NUM_OF_SETS = new Prefix("s/");
+    public static final Prefix PREFIX_NUM_OF_REPS = new Prefix("r/");
+    public static final Prefix PREFIX_WEIGHT = new Prefix("m/");
+    public static final Prefix PREFIX_DATETIME = new Prefix("d/");
+    public static final Prefix PREFIX_FREQUENCY = new Prefix("f/");
+    public static final Prefix PREFIX_FILEPATH = new Prefix("p/");
 }
