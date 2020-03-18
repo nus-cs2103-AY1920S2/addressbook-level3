a2299ca8aa5c2a55e969dd7ccb94eeac6a865085
Aloysius Chan <alloystory@gmail.com>
2020-03-18 21:25:08 +0800

    edit sample data

--- src/main/java/seedu/zerotoone/model/util/SampleDataUtil.java
+++ src/main/java/seedu/zerotoone/model/util/SampleDataUtil.java
@@ -1,62 +1,59 @@
 package seedu.zerotoone.model.util;
 
-import java.util.Arrays;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 import seedu.zerotoone.model.ExerciseList;
 import seedu.zerotoone.model.ReadOnlyExerciseList;
-// import seedu.zerotoone.model.exercise.Address;
-// import seedu.zerotoone.model.exercise.Email;
-// import seedu.zerotoone.model.exercise.Name;
+import seedu.zerotoone.model.exercise.ExerciseSet;
+import seedu.zerotoone.model.exercise.Weight;
+import seedu.zerotoone.model.exercise.NumReps;
+import seedu.zerotoone.model.exercise.ExerciseName;
 
 import seedu.zerotoone.model.exercise.Exercise;
-// import seedu.zerotoone.model.exercise.Phone;
-import seedu.zerotoone.model.tag.Tag;
 
 /**
  * Contains utility methods for populating {@code ExerciseList} with sample data.
  */
 public class SampleDataUtil {
     public static Exercise[] getSampleExercises() {
-        return new Exercise[]{};
-        // return new Exercise[] {
-        //     new Exercise(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-        //         new Address("Blk 30 Geylang Street 29, #06-40"),
-        //         getTagSet("friends")),
-        //     new Exercise(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-        //         new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-        //         getTagSet("colleagues", "friends")),
-        //     new Exercise(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-        //         new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-        //         getTagSet("neighbours")),
-        //     new Exercise(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-        //         new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-        //         getTagSet("family")),
-        //     new Exercise(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-        //         new Address("Blk 47 Tampines Street 20, #17-35"),
-        //         getTagSet("classmates")),
-        //     new Exercise(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-        //         new Address("Blk 45 Aljunied Street 85, #11-31"),
-        //         getTagSet("colleagues"))
-        // };
+        Exercise[] exercises = new Exercise[3];
+
+        // Exercise 1
+        ExerciseName exerciseOneName = new ExerciseName("Bench Press");
+        List<ExerciseSet> exerciseOneSets = new ArrayList<>();
+        exerciseOneSets.add(new ExerciseSet(new Weight("60"), new NumReps("5")));
+        exerciseOneSets.add(new ExerciseSet(new Weight("60"), new NumReps("5")));
+        exerciseOneSets.add(new ExerciseSet(new Weight("60"), new NumReps("5")));
+        exercises[0] = new Exercise(exerciseOneName, exerciseOneSets);
+
+        // Exercise 2
+        ExerciseName exerciseTwoName = new ExerciseName("Overhead Press");
+        List<ExerciseSet> exerciseTwoSets = new ArrayList<>();
+        exerciseTwoSets.add(new ExerciseSet(new Weight("30"), new NumReps("10")));
+        exerciseTwoSets.add(new ExerciseSet(new Weight("30"), new NumReps("10")));
+        exerciseTwoSets.add(new ExerciseSet(new Weight("30"), new NumReps("10")));
+        exercises[0] = new Exercise(exerciseTwoName, exerciseTwoSets);
+
+        // Exercise 3
+        ExerciseName exerciseThreeName = new ExerciseName("Triceps Pushdown");
+        List<ExerciseSet> exerciseThreeSets = new ArrayList<>();
+        exerciseThreeSets.add(new ExerciseSet(new Weight("15"), new NumReps("10")));
+        exerciseThreeSets.add(new ExerciseSet(new Weight("15"), new NumReps("10")));
+        exerciseThreeSets.add(new ExerciseSet(new Weight("15"), new NumReps("10")));
+        exerciseThreeSets.add(new ExerciseSet(new Weight("15"), new NumReps("10")));
+        exerciseThreeSets.add(new ExerciseSet(new Weight("15"), new NumReps("10")));
+        exercises[0] = new Exercise(exerciseThreeName, exerciseThreeSets);
+
+        return exercises;
     }
 
     public static ReadOnlyExerciseList getSampleExerciseList() {
-        ExerciseList sampleAb = new ExerciseList();
-        for (Exercise sampleExercise : getSampleExercises()) {
-            sampleAb.addExercise(sampleExercise);
+        ExerciseList sampleExerciseList = new ExerciseList();
+        for (Exercise exercise : getSampleExercises()) {
+            sampleExerciseList.addExercise(exercise);
         }
-        return sampleAb;
-    }
-
-    /**
-     * Returns a tag set containing the list of strings given.
-     */
-    public static Set<Tag> getTagSet(String... strings) {
-        return Arrays.stream(strings)
-                .map(Tag::new)
-                .collect(Collectors.toSet());
+        return sampleExerciseList;
     }
 
 }
