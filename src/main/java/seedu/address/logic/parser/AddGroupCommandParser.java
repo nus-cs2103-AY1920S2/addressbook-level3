059a833db3602688e0ba2bfd785b6cecdb3a8311
Aakanksha Rai <rai.aakanksha@gmail.com>
2020-03-19 17:29:43 +0800

    Add AddGroupCommandParser

---
+++ src/main/java/seedu/address/logic/parser/AddGroupCommandParser.java
@@ -0,0 +1,50 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_GROUP;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_MODULE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TYPE;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.AddGroupCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.group.Group;
+import seedu.address.model.group.Group.GroupType;
+
+/**
+ * Parses input arguments and creates a new AddGroupCommand object
+ */
+public class AddGroupCommandParser implements Parser<AddGroupCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddGroupCommand
+     * and returns an AddGroupCommand object for execution.
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddGroupCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                         ArgumentTokenizer.tokenize(args, PREFIX_GROUP, PREFIX_MODULE, PREFIX_TYPE);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_GROUP, PREFIX_MODULE, PREFIX_TYPE)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddGroupCommand.MESSAGE_USAGE));
+        }
+
+        String groupCode = ParserUtil.parseValue(argMultimap.getValue(PREFIX_GROUP).get());
+        String moduleCode = ParserUtil.parseValue(argMultimap.getValue(PREFIX_MODULE).get());
+        GroupType groupType = ParserUtil.parseGroupType(argMultimap.getValue(PREFIX_TYPE).get());
+
+        Group group = new Group(groupCode, groupType);
+
+        return new AddGroupCommand(group, moduleCode);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+ }
