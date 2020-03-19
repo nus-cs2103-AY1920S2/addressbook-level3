751bbe7229a455b3916ff2b9857b12b7e9cf27f9
Aloysius Chan <alloystory@gmail.com>
2020-03-20 02:52:05 +0800

    removed AB3 CliSyntax

--- src/main/java/seedu/zerotoone/logic/parser/CliSyntax.java
+++ src/main/java/seedu/zerotoone/logic/parser/CliSyntax.java
@@ -6,15 +6,6 @@ import seedu.zerotoone.logic.parser.util.Prefix;
  * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
  */
 public class CliSyntax {
-
-    /* Prefix definitions */
-    public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_PHONE = new Prefix("p/");
-    public static final Prefix PREFIX_EMAIL = new Prefix("e/");
-    public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
-    public static final Prefix PREFIX_TAG = new Prefix("t/");
-
-    // ZeroToOne
     public static final Prefix PREFIX_EXERCISE_NAME = new Prefix("e/");
     public static final Prefix PREFIX_WORKOUT_NAME = new Prefix("w/");
     public static final Prefix PREFIX_NUM_OF_SETS = new Prefix("s/");
