87fb1069402cb953b4d08feb79af01b2f5331113
CharmaineKoh <charmaine.koh@u.nus.edu>
2020-03-19 15:45:50 +0800

    Removed remaining addressbook comments from commons.

--- src/main/java/nasa/commons/core/GuiSettings.java
+++ src/main/java/nasa/commons/core/GuiSettings.java
@@ -12,17 +12,17 @@ import java.util.Objects;
  */
 public class GuiSettings implements Serializable {
 
-    //private static final double DEFAULT_HEIGHT = 600;
-    //private static final double DEFAULT_WIDTH = 740;
+    //Fills entire screen when program starts up.
+    private static final double DEFAULT_HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    private static final double DEFAULT_WIDTH = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 
     private final double windowWidth;
     private final double windowHeight;
     private final Point windowCoordinates;
 
     public GuiSettings() {
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        windowWidth = screenSize.getHeight();;
-        windowHeight = screenSize.getWidth();
+        windowWidth = DEFAULT_WIDTH;
+        windowHeight = DEFAULT_HEIGHT;
         windowCoordinates = null; // null represent no coordinates
     }
 
