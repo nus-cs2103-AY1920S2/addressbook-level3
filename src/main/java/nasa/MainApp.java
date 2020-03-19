ca02585e32dd7f01a58a6f2029c2ad16d4490446
CharmaineKoh <charmaine.koh@u.nus.edu>
2020-03-19 15:47:30 +0800

    Removed unnecessary comments.

--- src/main/java/nasa/MainApp.java
+++ src/main/java/nasa/MainApp.java
@@ -50,7 +50,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing NasaBook ]===========================");
+        logger.info("=============================[ Initializing NASA ]===========================");
         super.init();
 
         AppParameters appParameters = AppParameters.parse(getParameters());
@@ -71,9 +71,9 @@ public class MainApp extends Application {
     }
 
     /**
-     * Returns a {@code ModelManager} with the data from {@code storage}'s address book and {@code userPrefs}. <br>
-     * The data from the sample address book will be used instead if {@code storage}'s address book is not found,
-     * or an empty address book will be used instead if errors occur when reading {@code storage}'s address book.
+     * Returns a {@code ModelManager} with the data from {@code storage}'s nasa book and {@code userPrefs}. <br>
+     * The data from the sample nasa book will be used instead if {@code storage}'s nasa book is not found,
+     * or an empty nasa book will be used instead if errors occur when reading {@code storage}'s nasa book.
      */
     private Model initModelManager(Storage storage, ReadOnlyUserPrefs userPrefs) {
         Optional<ReadOnlyNasaBook> nasaBookOptional;
@@ -153,7 +153,7 @@ public class MainApp extends Application {
                     + "Using default user prefs");
             initializedPrefs = new UserPrefs();
         } catch (IOException e) {
-            logger.warning("Problem while reading from the file. Will be starting with an empty AddressBook");
+            logger.warning("Problem while reading from the file. Will be starting with an empty NASA");
             initializedPrefs = new UserPrefs();
         }
 
@@ -169,13 +169,13 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        logger.info("Starting NasaBook " + MainApp.VERSION);
+        logger.info("Starting NASA " + MainApp.VERSION);
         ui.start(primaryStage);
     }
 
     @Override
     public void stop() {
-        logger.info("============================ [ Stopping Nasa Book ] =============================");
+        logger.info("============================ [ Stopping NASA ] =============================");
         try {
             storage.saveUserPrefs(model.getUserPrefs());
         } catch (IOException e) {
