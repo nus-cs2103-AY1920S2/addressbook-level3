915306ac3671cfdbe822d4997ca9326e24f5e716
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-03-21 00:15:59 +0800

    Minor changes to favourite and unfavourite command

--- src/main/java/seedu/address/logic/commands/UnfavouriteCommand.java
+++ src/main/java/seedu/address/logic/commands/UnfavouriteCommand.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_RECIPES;
 
 import java.util.Arrays;
 import java.util.List;
@@ -50,6 +51,7 @@ public class UnfavouriteCommand extends Command {
             }
         }
         sb.append(" from favourites!");
+        model.updateFilteredRecipeList(PREDICATE_SHOW_ALL_RECIPES);
         return new CommandResult(sb.toString());
     }
 
