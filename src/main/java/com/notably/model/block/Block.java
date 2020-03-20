1ae67855f535ed9bac5e319737caac0966828c94
Firzan Armani <mrfirzanarmani@gmail.com>
2020-03-20 20:28:24 +0800

    Update Block interface and implementation

--- src/main/java/com/notably/model/block/Block.java
+++ src/main/java/com/notably/model/block/Block.java
@@ -1,8 +1,5 @@
 package com.notably.model.block;
 
-import java.util.List;
-import java.util.Optional;
-
 /**
  * API of the Block component.
  */
@@ -16,46 +13,4 @@ public interface Block {
      * Gets the body content of a block.
      */
     Body getBody();
-
-    /**
-     * Gets the parent block of a block, if possible.
-     * Returns an {@code Optional.empty()} if the block is the root block.
-     */
-    Optional<Block> getParent();
-
-    /**
-     * Gets a list of children blocks of a block.
-     */
-    List<Block> getChildren();
-
-    /**
-     * Replaces all the children of the block with a new list of children
-     */
-    void setChildren(List<Block> newChildren);
-
-    /**
-     * Finds a child block of a block, with the given input.
-     */
-    Optional<Block> getChild(Title title);
-
-    /**
-     * Replaces a child block of a block, that matches the title, with a new child block.
-     */
-    void setChild(Title title, Block newBlock);
-
-    /**
-     * Adds a single new child to a block.
-     */
-    void addChild(Block newBlock);
-
-    /**
-     * Removes a specified child block from a block.
-     * @param toRemove
-     */
-    void removeChild(Block toRemove);
-
-    /**
-     * Checks if a block is a root block.
-     */
-    boolean isRoot();
 }
