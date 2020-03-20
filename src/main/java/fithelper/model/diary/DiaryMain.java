a7230b156e88931cc5268051542e1a6e759a86e1
WANG-Yuchen-Alice <wangalice2000331@icloud.com>
2020-03-21 01:19:22 +0800

    Checkstyle.

--- src/main/java/fithelper/model/diary/DiaryMain.java
+++ src/main/java/fithelper/model/diary/DiaryMain.java
@@ -1,6 +1,5 @@
 package fithelper.model.diary;
 
-import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import fithelper.logic.parser.exceptions.ParseException;
@@ -15,8 +14,6 @@ public class DiaryMain {
      * @param args user input
      */
     public static void main(String[] args) throws ParseException {
-        String str = "2015-03-15";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         Diary myDiary = new Diary(new DiaryDate("2015-03-16"), new Content("I am enjoying my life."));
         System.out.println(myDiary.toString());
         myDiary.addContent("And I love myself.");
