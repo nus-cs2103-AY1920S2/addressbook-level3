140bd4e54f298cc20268dd1b894f63728b2473e1
Mario Lorenzo <mariolorenzo213@gmail.com>
2020-03-21 12:19:01 +0800

    Add AddMetricCommandParser

---
+++ src/main/java/seedu/address/logic/parser/AddMetricCommandParser.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ALIAS;
+
+/**
+ * Parses input arguments and creates a new object of type AddMetricCommand
+ */
+public class AddMetricCommandParser {
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddMetricCommand
+     * and returns an AddMetricCommand object for execution.
+     *
+     * @param arguments the arguments to be parsed
+     * @throws ParseException if the user input does not conform the expected format
+     */
+}
