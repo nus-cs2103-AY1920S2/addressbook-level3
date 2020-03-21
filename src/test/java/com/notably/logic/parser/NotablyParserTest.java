2fe255d3725eca73ab857a4812dd090467a8c65c
Jia Zheng <ljiazh3ng@gmail.com>
2020-03-21 21:14:40 +0800

    Test case

--- src/test/java/com/notably/logic/parser/NotablyParserTest.java
+++ src/test/java/com/notably/logic/parser/NotablyParserTest.java
@@ -17,34 +17,34 @@ public class NotablyParserTest {
 
     @Test
     public void parseCommand_newCommandInput_newCommand() throws Exception {
-        Object command = parser.parseCommand("new -t CS2103 -b Hi").get(0);
+        Command command = parser.parseCommand("new -t CS2103 -b Hi").get(0);
 
         assertTrue(command instanceof NewCommand);
     }
 
     @Test
     public void parseCommand_newCommandInputJump_false() throws Exception {
-        List<Command> commands = parser.parseCommand("new -t CS2103 -b Hi");
+        List<? extends Command> commands = parser.parseCommand("new -t CS2103 -b Hi");
 
         assertTrue(commands.size() == 1);
     }
 
     @Test
     public void parseCommand_newCommandInputJump_true() throws Exception {
-        List<Command> commands = parser.parseCommand("new -t CS2103 -b Hi -o");
+        List<? extends Command> commands = parser.parseCommand("new -t CS2103 -b Hi -o");
         assertTrue(commands.size() == 2);
     }
 
     @Test
     public void parseCommand_openCommandInput_openCommand() throws Exception {
-        Object command = parser.parseCommand("open -t CS2103").get(0);
+        Command command = parser.parseCommand("open -t CS2103").get(0);
 
         assertTrue(command instanceof OpenCommand);
     }
 
     @Test
     public void parseCommand_deleteCommandInput_deleteCommand() throws Exception {
-        Object command = parser.parseCommand("delete -t CS2103").get(0);
+        Command command = parser.parseCommand("delete -t CS2103").get(0);
 
         assertTrue(command instanceof DeleteCommand);
     }
