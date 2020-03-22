316928e63ed729fd5ca8a842cc89424307442c21
kester-ng <kester.ng@visenze.com>
2020-03-22 22:55:28 +0800

    Added JUNIT test for DeleteActivityCommand

---
+++ src/test/java/nasa/logic/commands/DeleteActivityCommandTest.java
@@ -0,0 +1,60 @@
+package nasa.logic.commands;
+
+import static nasa.logic.commands.CommandTestUtil.assertCommandFailure;
+import static nasa.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static nasa.testutil.TypicalIndexes.INDEX_FIRST_ACTIVITY;
+import static nasa.testutil.TypicalIndexes.INDEX_SECOND_ACTIVITY;
+
+import org.junit.jupiter.api.Test;
+
+import nasa.commons.core.index.Index;
+import nasa.model.Model;
+import nasa.model.ModelManager;
+import nasa.model.UserPrefs;
+import nasa.model.module.Module;
+import nasa.model.module.ModuleCode;
+import nasa.testutil.NasaBookBuilder;
+
+public class DeleteActivityCommandTest {
+
+    private Model model = new ModelManager(new NasaBookBuilder().build(), new UserPrefs());
+
+    @Test
+    public void execute_validActivityUnfilteredList_success() {
+        Module module = model.getFilteredModuleList().get(INDEX_FIRST_ACTIVITY.getZeroBased());
+        ModuleCode moduleCode = module.getModuleCode();
+        DeleteActivityCommand deleteActivityCommand = new DeleteActivityCommand(INDEX_FIRST_ACTIVITY, moduleCode);
+
+        String expectedMessage = String.format(INDEX_FIRST_ACTIVITY.toString()
+            + DeleteActivityCommand.MESSAGE_SUCCESS);
+
+        ModelManager expectedModel = new ModelManager(new NasaBookBuilder().build(), new UserPrefs());
+        expectedModel.removeActivityByIndex(moduleCode, INDEX_FIRST_ACTIVITY);
+
+        assertCommandSuccess(deleteActivityCommand, model ,expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_failure() {
+        // an index that is invalid (> list.size())
+        Module module = model.getFilteredModuleList().get(INDEX_SECOND_ACTIVITY.getZeroBased());
+        ModuleCode moduleCode = module.getModuleCode();
+        DeleteActivityCommand deleteActivityCommand = new DeleteActivityCommand(Index.fromOneBased(6), moduleCode);
+
+        assertCommandFailure(deleteActivityCommand, model, DeleteActivityCommand.MESSAGE_FAILURE);
+    }
+
+    @Test
+    public void execute_invalidModuleUnfilteredList_failure() {
+        // a module that does not exist
+        ModuleCode moduleCode = new ModuleCode("UTW1001K");
+        DeleteActivityCommand deleteActivityCommand = new DeleteActivityCommand(INDEX_FIRST_ACTIVITY, moduleCode);
+
+        assertCommandFailure(deleteActivityCommand, model, DeleteActivityCommand.MESSAGE_FAILURE);
+
+        // a null module
+        deleteActivityCommand = new DeleteActivityCommand(INDEX_FIRST_ACTIVITY, null);
+
+        assertCommandFailure(deleteActivityCommand, model, DeleteActivityCommand.MESSAGE_FAILURE);
+    }
+}
