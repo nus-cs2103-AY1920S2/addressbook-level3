efec82ecf1e2048cde766761766af38e45b0b778
johannagwan <johannagwan@gmail.com>
2020-03-22 16:49:33 +0800

    Add OpenSuggestionCommandTest

--- src/test/java/com/notably/logic/suggestion/commands/OpenSuggestionCommandTest.java
+++ src/test/java/com/notably/logic/suggestion/commands/OpenSuggestionCommandTest.java
@@ -1,39 +1,75 @@
 package com.notably.logic.suggestion.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.notably.commons.core.path.AbsolutePath;
+import com.notably.commons.core.path.exceptions.InvalidPathException;
+import com.notably.logic.suggestion.SuggestionCommand;
+import com.notably.logic.suggestion.block.BlockTreeStub;
+import com.notably.logic.suggestion.model.ModelStub;
+import com.notably.model.Model;
+import com.notably.model.block.Block;
+import com.notably.model.block.BlockImpl;
+import com.notably.model.block.BlockTree;
+import com.notably.model.block.Title;
+import com.notably.model.suggestion.SuggestionItem;
+import com.notably.model.suggestion.SuggestionItemImpl;
+
 public class OpenSuggestionCommandTest {
-    /**
-     * Creates a tree with dummy values. TODO: to be updated with the real DS.
-     * @return The root of the tree.
-     */
-    public static Node<String> createTree() {
-        Node<String> root = new Node<>("nus");
+    private BlockTree blockTree = new BlockTreeStub();
+    private AbsolutePath toRoot;
+    private AbsolutePath toCs2103;
+    private AbsolutePath toCs2103Week1;
+    private AbsolutePath toCs2103Week1Lecture;
+    private OpenSuggestionCommand openSuggestionCommand;
 
-        Node<String> nus = root.addChild(new Node<String>("nus"));
-        Node<String> y2s1 = nus.addChild(new Node<String>("y2s1"));
-        Node<String> y2s2 = nus.addChild(new Node<String>("y2s2"));
-        Node<String> cs2103t = y2s2.addChild(new Node<String>("cs2103t"));
-        Node<String> cs2101 = y2s2.addChild(new Node<String>("cs2101"));
-        Node<String> cs3243 = y2s2.addChild(new Node<String>("cs3243"));
-        Node<String> tp = cs2103t.addChild(new Node<String>("tp"));
-        Node<String> ip = cs2103t.addChild(new Node<String>("ip"));
-        Node<String> notably = tp.addChild(new Node<String>("notably"));
-
-        Node<String> personal = root.addChild(new Node<String>("personal"));
-        Node<String> p1 = personal.addChild(new Node<String>("p1"));
-        Node<String> p2 = personal.addChild(new Node<String>("p2"));
-
-        return root;
+    @BeforeAll
+    public void setUp() throws InvalidPathException {
+        toRoot = AbsolutePath.fromString("/");
+        toCs2103 = AbsolutePath.fromString("/CS2103");
+        toCs2103Week1 = AbsolutePath.fromString("/CS2103/Week1");
+        toCs2103Week1Lecture = AbsolutePath.fromString("/CS2103/Week1/Lecture");
+        openSuggestionCommand = new OpenSuggestionCommand(toRoot);
     }
 
 
     /**
-     * Prints the skeleton of the tree. TODO: to be removed, but will be put here for now in case needed.
-     * @param node The root node.
-     * @param appender A separator to format the display of the tree.
-     * @param <T> A generic.
+     * Creates a tree with dummy values.
      */
-    public static <T> void printTree(Node<T> node, String appender) {
-        System.out.println(appender + node.getTitle());
-        node.getChildren().forEach(each -> printTree(each, appender + appender));
+    public void createTree() {
+        Block cs2103 = new BlockImpl(new Title("CS2103"));
+        Block cs3230 = new BlockImpl(new Title("CS3230"));
+        Block week1 = new BlockImpl(new Title("Week1"));
+        Block week2 = new BlockImpl(new Title("Week2"));
+        Block week3 = new BlockImpl(new Title("Week3"));
+        Block lecture = new BlockImpl(new Title("lecture"));
+        blockTree.add(toRoot, cs2103);
+        blockTree.add(toRoot, cs3230);
+        blockTree.add(toCs2103, week1);
+        blockTree.add(toCs2103, week2);
+        blockTree.add(toCs2103, week3);
+        blockTree.add(toCs2103Week1, lecture);
+    }
+
+    @Test
+    public void executeTest() {
+        openSuggestionCommand = new OpenSuggestionCommand(toCs2103Week1);
+        Model model = new ModelStub();
+        openSuggestionCommand.execute(model);
+        assertEquals("Open a note", model.responseTextProperty().getValue());
+
+        List<AbsolutePath> possiblePaths = new ArrayList<>();
+        possiblePaths.add(toCs2103Week1);
+        possiblePaths.add(toCs2103Week1Lecture);
+        List<SuggestionItem> suggestions = new ArrayList<>();
+        suggestions.add(new SuggestionItemImpl("/CS2103/Week1", null));
+        suggestions.add(new SuggestionItemImpl("/CS2103/Week1/Lecture", null));
+
     }
 }
--- src/test/java/com/notably/logic/suggestion/model/ModelStub.java
+++ src/test/java/com/notably/logic/suggestion/model/ModelStub.java
@@ -4,18 +4,26 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-import com.notably.logic.parser.ArgumentMultimap;
 import com.notably.model.suggestion.SuggestionItem;
 import com.notably.testutil.ModelStubBase;
 
 import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 public class ModelStub extends ModelStubBase {
     private ObservableList<SuggestionItem> suggestions;
     private Property<Optional<String>> responseTextProperty;
     private StringProperty input;
+
+    public ModelStub() {
+        suggestions = FXCollections.observableArrayList();
+        responseTextProperty = new SimpleObjectProperty(Optional.empty());
+        input = new SimpleStringProperty("");
+    }
 
     @Override
     public Property<Optional<String>> responseTextProperty() {
