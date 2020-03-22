e783836de78105d60274d9b9048d7e707b6c9c96
johannagwan <johannagwan@gmail.com>
2020-03-22 15:24:56 +0800

    Add ModelStub

---
+++ src/test/java/com/notably/logic/suggestion/model/ModelStub.java
@@ -0,0 +1,51 @@
+package com.notably.logic.suggestion.model;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import com.notably.logic.parser.ArgumentMultimap;
+import com.notably.model.suggestion.SuggestionItem;
+import com.notably.testutil.ModelStubBase;
+
+import javafx.beans.property.Property;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+
+public class ModelStub extends ModelStubBase {
+    private ObservableList<SuggestionItem> suggestions;
+    private Property<Optional<String>> responseTextProperty;
+    private StringProperty input;
+
+    @Override
+    public Property<Optional<String>> responseTextProperty() {
+        return responseTextProperty;
+    }
+
+    @Override
+    public void setResponseText(String responseText) {
+        Objects.requireNonNull(responseText);
+        responseTextProperty.setValue(Optional.of(responseText));
+    }
+
+    @Override
+    public ObservableList<SuggestionItem> getSuggestions() {
+        return suggestions;
+    }
+
+    @Override
+    public void setSuggestions(List<SuggestionItem> suggestions) {
+        Objects.requireNonNull(suggestions);
+        this.suggestions.setAll(suggestions);
+    }
+
+    @Override
+    public String getInput() {
+        return this.input.getValue();
+    }
+
+    @Override
+    public void setInput(String input) {
+        this.input.setValue(input);
+    }
+}
