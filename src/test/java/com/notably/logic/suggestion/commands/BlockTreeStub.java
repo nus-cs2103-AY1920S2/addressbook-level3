0ba21aa092d86bd58b5fceba8222e0306411a7cd
johannagwan <johannagwan@gmail.com>
2020-03-22 14:53:24 +0800

    Add BlockTreeStub

---
+++ src/test/java/com/notably/logic/suggestion/commands/BlockTreeStub.java
@@ -0,0 +1,35 @@
+package com.notably.logic.suggestion.commands;
+
+import com.notably.commons.core.path.AbsolutePath;
+import com.notably.model.block.Block;
+import com.notably.model.block.BlockTree;
+import com.notably.model.block.BlockTreeItem;
+
+public class BlockTreeStub implements BlockTree {
+    private BlockTreeItem root;
+
+    @Override
+    public BlockTreeItem getRootBlock() {
+        return this.root;
+    }
+
+    @Override
+    public BlockTreeItem get(AbsolutePath path) {
+        return null;
+    }
+
+    @Override
+    public void add(AbsolutePath path, Block newBlock) {
+
+    }
+
+    @Override
+    public void set(AbsolutePath path, Block newBlock) {
+
+    }
+
+    @Override
+    public void remove(AbsolutePath path) {
+
+    }
+}
