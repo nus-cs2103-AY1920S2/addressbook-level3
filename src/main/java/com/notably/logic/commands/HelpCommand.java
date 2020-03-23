19c5d458ce1e6cc10de8694957e72c2363244a09
Jia Zheng <ljiazh3ng@gmail.com>
2020-03-24 04:00:52 +0800

    Change javadoc

--- src/main/java/com/notably/logic/commands/HelpCommand.java
+++ src/main/java/com/notably/logic/commands/HelpCommand.java
@@ -5,7 +5,7 @@ import static java.util.Objects.requireNonNull;
 import com.notably.model.Model;
 
 /**
- * Represent a command that show/disables the help window.
+ * Represent a command that enable/disables the help window.
  */
 public class HelpCommand extends Command {
     public static final String COMMAND_WORD = "help";
