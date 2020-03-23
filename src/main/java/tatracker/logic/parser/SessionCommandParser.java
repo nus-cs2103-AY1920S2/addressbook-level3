ededaeaab736546b3c61f6176e79ce3b08ade36e
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-03-23 19:58:25 +0800

    Use DeleteSessionCommand in SessionCommandParser

--- src/main/java/tatracker/logic/parser/SessionCommandParser.java
+++ src/main/java/tatracker/logic/parser/SessionCommandParser.java
@@ -11,6 +11,7 @@ import tatracker.logic.commands.CommandWords;
 import tatracker.logic.commands.HelpCommand;
 import tatracker.logic.parser.exceptions.ParseException;
 import tatracker.logic.parser.session.AddSessionCommandParser;
+import tatracker.logic.parser.session.DeleteSessionCommandParser;
 import tatracker.logic.parser.session.EditSessionCommandParser;
 
 /**
@@ -44,15 +45,12 @@ public class SessionCommandParser {
 
         case CommandWords.ADD_MODEL:
             return new AddSessionCommandParser().parse(arguments);
-            //throw new ParseException(String.format(UNIMPLEMENTED_CODE_FORMAT, "Add session commands"));
 
         case CommandWords.DELETE_MODEL:
-            // return new DeleteSessionCommandParser().parse(arguments);
-            throw new ParseException(String.format(UNIMPLEMENTED_CODE_FORMAT, "Delete session commands"));
+            return new DeleteSessionCommandParser().parse(arguments);
 
         case CommandWords.EDIT_MODEL:
             return new EditSessionCommandParser().parse(arguments);
-            // throw new ParseException(String.format(UNIMPLEMENTED_CODE_FORMAT, "Edit session commands"));
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
