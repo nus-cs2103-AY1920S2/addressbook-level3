011b9392f630ce18c7dfb3c4e1be9ef47653f625
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-03-23 10:25:08 +0800

    Add viewstate/viewStateModel interface

--- src/main/java/com/notably/model/ModelManager.java
+++ src/main/java/com/notably/model/ModelManager.java
@@ -15,11 +15,12 @@ import com.notably.commons.core.path.AbsolutePath;
 import com.notably.model.block.Block;
 import com.notably.model.block.BlockTree;
 import com.notably.model.block.Body;
-import com.notably.model.commandinput.CommandInputModel;
 
 import com.notably.model.suggestion.SuggestionItem;
 import com.notably.model.suggestion.SuggestionModel;
 
+import com.notably.model.viewstate.ViewStateModel;
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.Property;
 
 import javafx.beans.property.StringProperty;
@@ -39,7 +40,7 @@ public class ModelManager implements Model {
     private FilteredList<Object> filteredPersons;
     // TODO: set the model variables to final after removing the AB3 attributes.
     private SuggestionModel suggestionModel;
-    private CommandInputModel commandInputModel;
+    private ViewStateModel viewStateModel;
     private BlockModel blockModel;
 
     /**
@@ -61,9 +62,9 @@ public class ModelManager implements Model {
     }
 
     // TODO: to update constructor according to our Model classes.
-    public ModelManager(SuggestionModel suggestionModel, CommandInputModel commandInputModel, BlockModel blockModel) {
+    public ModelManager(SuggestionModel suggestionModel, ViewStateModel viewStateModel, BlockModel blockModel) {
         this.suggestionModel = suggestionModel;
-        this.commandInputModel = commandInputModel;
+        this.viewStateModel = viewStateModel;
         this.blockModel = blockModel;
     }
 
@@ -200,21 +201,35 @@ public class ModelManager implements Model {
         suggestionModel.clearSuggestions();
     }
 
-    //========= CommandInputModel==================================================================
-
+    //=========== ViewStateModel ==================================================================
     @Override
     public StringProperty inputProperty() {
-        return commandInputModel.inputProperty();
+        return viewStateModel.inputProperty();
     }
 
     @Override
     public String getInput() {
-        return commandInputModel.getInput();
+        return viewStateModel.getInput();
     }
 
     @Override
     public void setInput(String input) {
-        commandInputModel.setInput(input);
+        viewStateModel.setInput(input);
+    }
+
+    @Override
+    public BooleanProperty boolProperty() {
+        return viewStateModel.boolProperty();
+    }
+
+    @Override
+    public Boolean getBool() {
+        return viewStateModel.getBool();
+    }
+
+    @Override
+    public void setBool(Boolean bool) {
+        viewStateModel.setBool(bool);
     }
 
     //========= BlockModel========================================================================
---
+++ src/main/java/com/notably/model/viewstate/ViewStateModel.java
@@ -0,0 +1,7 @@
+package com.notably.model.viewstate;
+
+/**
+ * API for the ViewStateModel component.
+ */
+public interface ViewStateModel extends CommandInputModel, HelpFlagModel {
+}
