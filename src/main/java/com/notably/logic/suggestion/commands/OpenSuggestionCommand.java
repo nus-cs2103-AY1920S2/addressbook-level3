30cd1d9b7b97d9b3aecf3dce63d1634d157a34df
johannagwan <johannagwan@gmail.com>
2020-03-23 14:08:13 +0800

    Update OpenSuggestionCommand

--- src/main/java/com/notably/logic/suggestion/commands/OpenSuggestionCommand.java
+++ src/main/java/com/notably/logic/suggestion/commands/OpenSuggestionCommand.java
@@ -19,8 +19,8 @@ import com.notably.model.suggestion.SuggestionItemImpl;
 public class OpenSuggestionCommand implements SuggestionCommand {
     public static final String COMMAND_WORD = "open";
     public static final String RESPONSE_MESSAGE = "Open a note";
+
     private AbsolutePath path;
-    private List<AbsolutePath> possiblePaths = new ArrayList<>();
 
     public OpenSuggestionCommand(AbsolutePath path) {
         this.path = path;
@@ -28,73 +28,67 @@ public class OpenSuggestionCommand implements SuggestionCommand {
 
     @Override
     public void execute(Model model) {
-        setResponseTextToModel(model);
-        updatePossiblePaths(path, model);
-        List<AbsolutePath> possiblePaths = getPossiblePaths();
+        // Set response text
+        model.setResponseText(RESPONSE_MESSAGE);
+
+        // Set suggestions
+        List<AbsolutePath> possiblePaths = getPossiblePaths(model);
         List<SuggestionItem> suggestions = getSuggestions(possiblePaths, model);
 
         setSuggestionsToModel(model, suggestions);
-    }
-
-    public void setResponseTextToModel(Model model) {
-        model.setResponseText(RESPONSE_MESSAGE);
     }
 
     public void setSuggestionsToModel(Model model, List<SuggestionItem> suggestions) {
         model.setSuggestions(suggestions);
     }
 
-    public List<AbsolutePath> getPossiblePaths() {
-        return possiblePaths;
-    }
-
     /**
-     * Updates the list of possible paths according to the input path.
-     * @param path The user's input path.
+     * Gets the list of possible absolute paths, based on the user's input path.
      * @param model The app's model.
+     * @return List of possible absolute paths, based on the user's input path.
      */
-    public void updatePossiblePaths(AbsolutePath path, Model model) {
-        BlockTreeItem root = model.getBlockTree().getRootBlock();
+    public List<AbsolutePath> getPossiblePaths(Model model) {
+        List<AbsolutePath> possiblePaths = new ArrayList<>();
 
+        BlockTreeItem currTreeItem = model.getBlockTree().get(path);
         List<String> components = path.getComponents();
+
         List<BlockTreeItem> children = new ArrayList<>();
         Queue<BlockTreeItem> queue = new LinkedList<>();
 
-        for (BlockTreeItem rootChild : root.getBlockChildren()) {
-            queue.offer(rootChild);
-        }
+        queue.offer(currTreeItem);
 
         int index = 0;
-        while (!queue.isEmpty()) {
+        while (index < components.size() && !queue.isEmpty()) {
             BlockTreeItem curr = queue.poll();
             String dir = components.get(index);
-            if (curr.getTitle().equals(dir)) {
+            if (curr.getTitle().getText().equals(dir)) {
                 children = curr.getBlockChildren();
-                if (index == components.size() - 1) { // just get all the children
-                    break;
-                } else {
-                    queue.clear();
-                    for (BlockTreeItem child : children) {
-                        queue.offer(child);
-                    }
-                    index++;
-                }
+
+                queue.clear();
+                queue.addAll(children);
+
+                index++;
             }
         }
 
         possiblePaths.add(path);
+
         if (!children.isEmpty()) {
             for (BlockTreeItem child : children) {
                 List<String> newComponents = new ArrayList<>();
                 newComponents.addAll(components);
                 newComponents.add(child.getTitle().getText());
-                getChildRecursive(child, newComponents);
+                List<AbsolutePath> childrenRecursive = getChildRecursive(child, newComponents, possiblePaths);
+                possiblePaths.addAll(childrenRecursive);
             }
         }
+        return possiblePaths;
     }
 
-    public void getChildRecursive(BlockTreeItem node, List<String> components) {
-        List<BlockTreeItem> children = node.getBlockChildren();
+    public List<AbsolutePath> getChildRecursive(BlockTreeItem blockTreeItem, List<String> components,
+        List<AbsolutePath> possiblePaths) {
+        List<BlockTreeItem> children = blockTreeItem.getBlockChildren();
         if (children.size() == 0) { // if reach the end, add to possible paths
             AbsolutePath newPath = AbsolutePath.fromComponents(components);
             possiblePaths.add(newPath);
@@ -103,9 +97,10 @@ public class OpenSuggestionCommand implements SuggestionCommand {
                 List<String> newComponents = new ArrayList<>();
                 newComponents.addAll(components);
                 newComponents.add(child.getTitle().getText());
-                getChildRecursive(child, newComponents);
+                getChildRecursive(child, newComponents, possiblePaths);
             }
         }
+        return possiblePaths;
     }
 
     List<SuggestionItem> getSuggestions(List<AbsolutePath> possiblePaths, Model model) {
