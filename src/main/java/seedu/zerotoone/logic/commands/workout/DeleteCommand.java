aca5ab668dc9dbbf9db7bf5cd9f0fbdeb19a8aa8
wongchishan <wongchishan99@gmail.com>
2020-03-23 16:16:37 +0800

    Update workout delete command

--- src/main/java/seedu/zerotoone/logic/commands/workout/DeleteCommand.java
+++ src/main/java/seedu/zerotoone/logic/commands/workout/DeleteCommand.java
@@ -10,36 +10,36 @@ import seedu.zerotoone.commons.core.index.Index;
 import seedu.zerotoone.logic.commands.CommandResult;
 import seedu.zerotoone.logic.commands.exceptions.CommandException;
 import seedu.zerotoone.model.Model;
-import seedu.zerotoone.model.exercise.Exercise;
+import seedu.zerotoone.model.workout.Workout;
 
 /**
- * Deletes a exercise identified using it's displayed index from the exercise list.
+ * Deletes a workout identified using it's displayed index from the workout list.
  */
 public class DeleteCommand extends WorkoutCommand {
     public static final String COMMAND_WORD = "delete";
-    public static final String MESSAGE_USAGE = "Usage: exercise delete EXERCISE_ID";
-    public static final String MESSAGE_DELETE_EXERCISE_SUCCESS = "Deleted Exercise: %1$s";
-    private final Index exerciseId;
+    public static final String MESSAGE_USAGE = "Usage: workout delete WORKOUT_ID";
+    public static final String MESSAGE_DELETE_WORKOUT_SUCCESS = "Deleted Workout: %1$s";
+    private final Index workoutId;
 
     public DeleteCommand(Index targetIndex) {
         requireNonNull(targetIndex);
-        this.exerciseId = targetIndex;
+        this.workoutId = targetIndex;
     }
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
-        List<Exercise> lastShownList = model.getFilteredExerciseList();
+        List<Workout> lastShownList = model.getFilteredWorkoutList();
 
-        if (exerciseId.getZeroBased() >= lastShownList.size()) {
+        if (workoutId.getZeroBased() >= lastShownList.size()) {
             throw new CommandException(Messages.MESSAGE_INVALID_INDEX);
         }
 
-        Exercise exerciseToDelete = lastShownList.get(exerciseId.getZeroBased());
-        model.deleteExercise(exerciseToDelete);
+        Workout workoutToDelete = lastShownList.get(workoutId.getZeroBased());
+        model.deleteWorkout(workoutToDelete);
 
-        String outputMessage = String.format(MESSAGE_DELETE_EXERCISE_SUCCESS,
-                exerciseToDelete.getExerciseName().toString());
+        String outputMessage = String.format(MESSAGE_DELETE_WORKOUT_SUCCESS,
+                workoutToDelete.getWorkoutName().toString());
         return new CommandResult(outputMessage);
     }
 
@@ -47,6 +47,6 @@ public class DeleteCommand extends WorkoutCommand {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof DeleteCommand // instanceof handles nulls
-                && exerciseId.equals(((DeleteCommand) other).exerciseId)); // state check
+                && workoutId.equals(((DeleteCommand) other).workoutId)); // state check
     }
 }
