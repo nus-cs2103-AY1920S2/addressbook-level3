406d65c3acc7f9404453b6a9038452729d1c978e
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-03-23 22:26:20 +0800

    Edited RecipeCard (UI)

--- src/main/java/seedu/address/ui/RecipeCard.java
+++ src/main/java/seedu/address/ui/RecipeCard.java
@@ -34,6 +34,9 @@ public class RecipeCard extends UiPart<Region> {
      */
 
     public final Recipe recipe;
+    private final String styleIngredientsAndSteps = "-fx-font-size: 11pt;\n"
+            + "-fx-font-family: \"Segoe UI\";\n"
+            + "-fx-text-fill: #FFFFFF;\n";
 
     @FXML
     private HBox cardPane;
@@ -59,6 +62,10 @@ public class RecipeCard extends UiPart<Region> {
     private VBox steps;
     @FXML
     private FlowPane goals;
+    @FXML
+    private Label ingredientsHeader;
+    @FXML
+    private Label stepsHeader;
 
     public RecipeCard(Recipe recipe, int displayedIndex) throws IOException {
         super(FXML);
@@ -67,32 +74,50 @@ public class RecipeCard extends UiPart<Region> {
         name.setText(recipe.getName().fullName);
         name.setWrapText(true);
 
-        time.setText(recipe.getTime().value + " min");
-        time.setPadding(new Insets(0, 0, 10, 0));
-
         if (recipe.isFavourite()) {
             Path favouriteIconPath = Paths.get("src", "main", "resources", "images", "favourite.png");
             favourite.setImage(new Image(Files.newInputStream(favouriteIconPath)));
         }
 
+        recipe.getGoals().stream()
+                .sorted(Comparator.comparing(goal -> goal.goalName))
+                .forEach(goal -> goals.getChildren().add(new Label(goal.goalName)));
+
+        time.setText(recipe.getTime().value + " min");
+
+        ingredientsHeader.setText("Ingredients");
+        ingredientsHeader.setUnderline(true);
+        ingredientsHeader.setPadding(new Insets(10, 0, 0, 0));
+
         recipe.getGrains().forEach(grain -> grains.getChildren().add(new Label(grain.toString())));
+        grains.getChildren().forEach(grain -> grain.setStyle(styleIngredientsAndSteps));
+
         recipe.getVegetables().forEach(vegetable -> vegetables.getChildren().add(new Label(vegetable.toString())));
+        vegetables.getChildren().forEach(vegetable -> vegetable.setStyle(styleIngredientsAndSteps));
+
         recipe.getProteins().forEach(protein -> proteins.getChildren().add(new Label(protein.toString())));
+        proteins.getChildren().forEach(protein -> protein.setStyle(styleIngredientsAndSteps));
+
         recipe.getFruits().forEach(fruit -> fruits.getChildren().add(new Label(fruit.toString())));
+        fruits.getChildren().forEach(fruit -> fruit.setStyle(styleIngredientsAndSteps));
+
         recipe.getOthers().forEach(other -> others.getChildren().add(new Label(other.toString())));
+        others.getChildren().forEach(other -> other.setStyle(styleIngredientsAndSteps));
+
+        stepsHeader.setText("Steps");
+        stepsHeader.setUnderline(true);
+        stepsHeader.setPadding(new Insets(10, 0, 0, 0));
 
         // Calculates step number and displays with along with the step
         AtomicInteger stepNumber = new AtomicInteger(1);
         recipe.getSteps().forEach(step -> {
             Label stepLabel = new Label("Step " + stepNumber.getAndIncrement() + ": " + step.value);
             stepLabel.setWrapText(true);
+            stepLabel.setStyle(styleIngredientsAndSteps);
             steps.getChildren().add(stepLabel);
         });
-        steps.setPadding(new Insets(10, 0, 0, 0));
+        steps.setSpacing(5);
 
-        recipe.getGoals().stream()
-                .sorted(Comparator.comparing(goal -> goal.goalName))
-                .forEach(goal -> goals.getChildren().add(new Label(goal.goalName)));
     }
 
     @Override
