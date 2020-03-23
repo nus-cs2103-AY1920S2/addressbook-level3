167798f7a750dc9376782048c23f0095e8e18b66
johannagwan <johannagwan@gmail.com>
2020-03-23 14:12:55 +0800

    Remove unneccessary test cases

--- src/test/java/com/notably/logic/suggestion/commands/OpenSuggestionCommandTest.java
+++ src/test/java/com/notably/logic/suggestion/commands/OpenSuggestionCommandTest.java
@@ -1,26 +1,22 @@
 package com.notably.logic.suggestion.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import java.util.Optional;
-
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.notably.commons.core.path.AbsolutePath;
 import com.notably.commons.core.path.exceptions.InvalidPathException;
-import com.notably.logic.suggestion.block.BlockTreeStub;
 import com.notably.logic.suggestion.model.ModelStub;
 import com.notably.model.Model;
 import com.notably.model.block.Block;
 import com.notably.model.block.BlockImpl;
 import com.notably.model.block.BlockTree;
+import com.notably.model.block.BlockTreeImpl;
 import com.notably.model.block.Title;
 import com.notably.model.suggestion.SuggestionItem;
 import com.notably.model.suggestion.SuggestionItemImpl;
@@ -34,6 +30,7 @@ public class OpenSuggestionCommandTest {
     private static AbsolutePath toCs2103Week2;
     private static AbsolutePath toCs2103Week3;
     private static AbsolutePath toCs2103Week1Lecture;
+
     private Model model;
     private OpenSuggestionCommand openSuggestionCommand;
 
@@ -50,13 +47,13 @@ public class OpenSuggestionCommandTest {
 
     @BeforeEach
     public void setUpTestTree() {
-        blockTree = new BlockTreeStub();
+        blockTree = new BlockTreeImpl();
         Block cs2103 = new BlockImpl(new Title("CS2103"));
         Block cs3230 = new BlockImpl(new Title("CS3230"));
         Block week1 = new BlockImpl(new Title("Week1"));
         Block week2 = new BlockImpl(new Title("Week2"));
         Block week3 = new BlockImpl(new Title("Week3"));
-        Block lecture = new BlockImpl(new Title("lecture"));
+        Block lecture = new BlockImpl(new Title("Lecture"));
         blockTree.add(toRoot, cs2103);
         blockTree.add(toRoot, cs3230);
         blockTree.add(toCs2103, week1);
@@ -70,23 +67,6 @@ public class OpenSuggestionCommandTest {
         model = new ModelStub();
     }
 
-    @AfterEach
-    public void tearDownTree() {
-        blockTree = new BlockTreeStub();
-    }
-
-    @AfterEach
-    public void tearDownModel() {
-        model = new ModelStub();
-    }
-
-    @Test
-    public void setResponseTextToModelTest() {
-        OpenSuggestionCommand openSuggestionCommand = new OpenSuggestionCommand(toRoot);
-        openSuggestionCommand.setResponseTextToModel(model);
-        assertEquals(Optional.of("Open a note"), model.responseTextProperty().getValue());
-    }
-
     @Test
     public void setSuggestionsToModelTest() {
         OpenSuggestionCommand openSuggestionCommand = new OpenSuggestionCommand(toCs2103Week1);
@@ -94,16 +74,17 @@ public class OpenSuggestionCommandTest {
         possiblePaths.add(toCs2103Week1);
         possiblePaths.add(toCs2103Week1Lecture);
         List<SuggestionItem> suggestions = new ArrayList<>();
-        suggestions.add(new SuggestionItemImpl("/CS2103/Week1", null));
-        suggestions.add(new SuggestionItemImpl("/CS2103/Week1/Lecture", null));
+        suggestions.add(new SuggestionItemImpl("/CS2103/Week1", () -> model.setInput("/CS2103/Week1")));
+        suggestions.add(new SuggestionItemImpl("/CS2103/Week1/Lecture", () ->
+            model.setInput("/CS2103/Week1/Lecture")));
         openSuggestionCommand.setSuggestionsToModel(model, suggestions);
-        assertEquals("/CS2103/Week1", model.getSuggestions().get(0).getDisplayText());
-        assertEquals("/CS2103/Week1/Lecture", model.getSuggestions().get(1).getDisplayText());
+        assertEquals(suggestions.get(0), model.getSuggestions().get(0));
+        assertEquals(suggestions.get(1), model.getSuggestions().get(1));
     }
 
     /* TODO: add after BlockModelImpl is done.
     @Test
-    public void getPossiblePaths_fromRoot_test() {
+    public void updatePossiblePaths_fromRoot_test() {
         OpenSuggestionCommand openSuggestionCommand = new OpenSuggestionCommand(toRoot);
         openSuggestionCommand.updatePossiblePaths(toRoot, model);
         List<AbsolutePath> possiblePathsFromCommand = openSuggestionCommand.getPossiblePaths();
@@ -120,8 +101,25 @@ public class OpenSuggestionCommandTest {
         }
     }*/
 
+    /*
     @Test
-    public void getSuggestionsTest() {
+    public void getChildRecursiveTest() {
+        OpenSuggestionCommand openSuggestionCommand = new OpenSuggestionCommand(toCs2103);
+        BlockTreeItem cs2103BlockTreeItem = blockTree.get(toCs2103);
+        //openSuggestionCommand.getChildRecursive(cs2103BlockTreeItem, toCs2103.getComponents());
+        List<AbsolutePath> possiblePaths = openSuggestionCommand.getPossiblePaths();
+
+        List<AbsolutePath> expectedPossiblePaths = new ArrayList<>();
+        expectedPossiblePaths.add(toCs2103);
+        expectedPossiblePaths.add(toCs2103Week1);
+        expectedPossiblePaths.add(toCs2103Week2);
+        expectedPossiblePaths.add(toCs2103Week3);
+        expectedPossiblePaths.add(toCs2103Week1Lecture);
+        //assertEquals(expectedPossiblePaths, possiblePaths);
+    }*/
+
+    @Test
+    public void getSuggestions_displayText_test() {
         OpenSuggestionCommand openSuggestionCommand = new OpenSuggestionCommand(toCs2103Week1);
         List<AbsolutePath> possiblePaths = new ArrayList<>();
         possiblePaths.add(toCs2103Week1);
