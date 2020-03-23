caaf94d9063e3d52a5613230760735459ac4357d
Samuel Van Allen <samuel.huanghaoyi@gmail.com>
2020-03-23 17:16:51 +0800

    Add system tray icon and comments

--- src/main/java/tatracker/commons/core/Notification.java
+++ src/main/java/tatracker/commons/core/Notification.java
@@ -3,38 +3,48 @@ package tatracker.commons.core;
 import java.awt.AWTException;
 import java.awt.Image;
 import java.awt.SystemTray;
-import java.awt.Toolkit;
 import java.awt.TrayIcon;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
- * Throws AWTException if SystemTray is not supported on the current platform.
+ * A class that handles the displaying of System Notifications.
  */
 public class Notification {
+    public static final String APPICON_PATH = "images/address_book_32.png";
     private static Notification singleton;
     private final TrayIcon trayIcon;
 
-    private Notification() throws AWTException {
+    private Notification() throws AWTException, IOException {
         SystemTray tray = SystemTray.getSystemTray();
-        Image image = Toolkit.getDefaultToolkit().createImage("NotifyIcon.png");
+        Image image = ImageIO.read(getClass().getClassLoader().getResource(APPICON_PATH));
         trayIcon = new TrayIcon(image, "TrayIcon");
         trayIcon.setImageAutoSize(true);
-        trayIcon.setToolTip("TAT Tracker TEMP");
+        trayIcon.setToolTip("TA Tracker");
         tray.add(trayIcon);
     }
 
-    private void notify(String caption, String message, TrayIcon.MessageType type) throws AWTException {
-        trayIcon.displayMessage(caption, message, type);
-    }
-
-    public static void sendNotification(String caption, String message, TrayIcon.MessageType type) throws AWTException {
-        Notification.getInstance().notify(caption, message, type);
-    }
-
-    public static Notification getInstance() throws AWTException {
+    private static Notification getInstance() throws AWTException, IOException {
         if (singleton == null) {
             singleton = new Notification();
         }
 
         return singleton;
+    }
+
+    private void notify(String caption, String message, TrayIcon.MessageType type) {
+        trayIcon.displayMessage(caption, message, type);
+    }
+
+    /**
+     * Sends
+     * @param caption The title displayed in the notification
+     * @param message The message displayed in the notification
+     * @param type the type of notification (error, info, warning, etc.)
+     * @throws AWTException if {@code SystemTray} is not supported on the current platform.
+     */
+    public static void sendNotification(String caption, String message, TrayIcon.MessageType type) throws AWTException, IOException {
+        Notification.getInstance().notify(caption, message, type);
     }
 }
