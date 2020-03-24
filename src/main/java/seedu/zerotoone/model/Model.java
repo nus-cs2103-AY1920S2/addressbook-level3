6b06e631b2be105a60aef73fc417843a511805d6
wongchishan <wongchishan99@gmail.com>
2020-03-24 15:48:55 +0800

    Add WorkoutModel interface

--- src/main/java/seedu/zerotoone/model/Model.java
+++ src/main/java/seedu/zerotoone/model/Model.java
@@ -8,17 +8,14 @@ import seedu.zerotoone.commons.core.GuiSettings;
 import seedu.zerotoone.model.exercise.Exercise;
 import seedu.zerotoone.model.exercise.ReadOnlyExerciseList;
 import seedu.zerotoone.model.userprefs.ReadOnlyUserPrefs;
-import seedu.zerotoone.model.workout.ReadOnlyWorkoutList;
-import seedu.zerotoone.model.workout.Workout;
+import seedu.zerotoone.model.workout.WorkoutModel;
 
 /**
  * The API of the Model component.
  */
-public interface Model {
+public interface Model extends WorkoutModel {
     /** {@code Predicate} that always evaluate to true */
     Predicate<Exercise> PREDICATE_SHOW_ALL_EXERCISES = unused -> true;
-
-    Predicate<Workout> PREDICATE_SHOW_ALL_WORKOUTS = unused -> true;
 
     // -----------------------------------------------------------------------------------------
     // Common - User Preferences
@@ -98,61 +95,4 @@ public interface Model {
      * @throws NullPointerException if {@code predicate} is null.
      */
     void updateFilteredExerciseList(Predicate<Exercise> predicate);
-
-     // -----------------------------------------------------------------------------------------
-    // Workout List
-    /**
-     * Returns the user prefs' workout list file path.
-     */
-    Path getWorkoutListFilePath();
-
-    /**
-     * Sets the user prefs' workout list file path.
-     */
-    void setWorkoutListFilePath(Path workoutListFilePath);
-
-    /**
-     * Replaces workout list data with the data in {@code workoutList}.
-     */
-    void setWorkoutList(ReadOnlyWorkoutList workoutList);
-
-    /** Returns the WorkoutList */
-    ReadOnlyWorkoutList getWorkoutList();
-
-    /**
-     * Returns true if a exercise with the same identity as {@code workout} exists in the workout list.
-     */
-    boolean hasWorkout(Workout workout);
-
-    /**
-     * Deletes the given workout.
-     * The workout must exist in the workout list.
-     */
-    void deleteWorkout(Workout target);
-
-    /**
-     * Adds the given workout.
-     * {@code workout} must not already exist in the workout list.
-     */
-    void addWorkout(Workout workout);
-
-    /**
-     * Replaces the given workout {@code target} with {@code editedWorkout}.
-     * {@code target} must exist in the workout list.
-     * The workout identity of {@code editedWorkout} must not be the same as another
-     * existing workout in the workout list.
-     */
-    void setWorkout(Workout target, Workout editedExercise);
-
-    /**
-     * Returns an unmodifiable view of the list of {@code Workout} backed by the internal list of
-     * {@code versionedWorkoutList}
-     */
-    ObservableList<Workout> getFilteredWorkoutList();
-
-    /**
-     * Updates the filter of the filtered workout list to filter by the given {@code predicate}.
-     * @throws NullPointerException if {@code predicate} is null.
-     */
-    void updateFilteredWorkoutList(Predicate<Workout> predicate);
 }
---
+++ src/main/java/seedu/zerotoone/model/workout/WorkoutModel.java
@@ -0,0 +1,67 @@
+package seedu.zerotoone.model.workout;
+
+import java.nio.file.Path;
+import java.util.function.Predicate;
+
+import javafx.collections.ObservableList;
+
+public interface WorkoutModel {
+    Predicate<Workout> PREDICATE_SHOW_ALL_WORKOUTS = unused -> true;
+
+    // -----------------------------------------------------------------------------------------
+    // Workout List
+    /**
+     * Returns the user prefs' workout list file path.
+     */
+    Path getWorkoutListFilePath();
+
+    /**
+     * Sets the user prefs' workout list file path.
+     */
+    void setWorkoutListFilePath(Path workoutListFilePath);
+
+    /**
+     * Replaces workout list data with the data in {@code workoutList}.
+     */
+    void setWorkoutList(ReadOnlyWorkoutList workoutList);
+
+    /** Returns the WorkoutList */
+    ReadOnlyWorkoutList getWorkoutList();
+
+    /**
+     * Returns true if a exercise with the same identity as {@code workout} exists in the workout list.
+     */
+    boolean hasWorkout(Workout workout);
+
+    /**
+     * Deletes the given workout.
+     * The workout must exist in the workout list.
+     */
+    void deleteWorkout(Workout target);
+
+    /**
+     * Adds the given workout.
+     * {@code workout} must not already exist in the workout list.
+     */
+    void addWorkout(Workout workout);
+
+    /**
+     * Replaces the given workout {@code target} with {@code editedWorkout}.
+     * {@code target} must exist in the workout list.
+     * The workout identity of {@code editedWorkout} must not be the same as another
+     * existing workout in the workout list.
+     */
+    void setWorkout(Workout target, Workout editedExercise);
+
+    /**
+     * Returns an unmodifiable view of the list of {@code Workout} backed by the internal list of
+     * {@code versionedWorkoutList}
+     */
+    ObservableList<Workout> getFilteredWorkoutList();
+
+    /**
+     * Updates the filter of the filtered workout list to filter by the given {@code predicate}.
+     * @throws NullPointerException if {@code predicate} is null.
+     */
+    void updateFilteredWorkoutList(Predicate<Workout> predicate);
+}
