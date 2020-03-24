b4a536a9841e2f05f4b120c2b5a97f0424fe8906
johannagwan <johannagwan@gmail.com>
2020-03-24 16:27:47 +0800

    Add javadocs

--- src/main/java/com/notably/logic/suggestion/commands/HelpSuggestionCommand.java
+++ src/main/java/com/notably/logic/suggestion/commands/HelpSuggestionCommand.java
@@ -6,6 +6,9 @@ import com.notably.commons.core.path.AbsolutePath;
 import com.notably.logic.suggestion.SuggestionCommand;
 import com.notably.model.Model;
 
+/**
+ * Represents a suggestion command object to display a list of available commands.
+ */
 public class HelpSuggestionCommand implements SuggestionCommand {
     private static final String COMMAND_WORD = "help";
     private static final String RESPONSE_MESSAGE = "Display a list of available commands";
---
+++ src/test/java/com/notably/logic/suggestion/commands/HelpSuggestionCommandTest.java
@@ -0,0 +1,4 @@
+package com.notably.logic.suggestion.commands;
+
+public class HelpSuggestionCommandTest {
+}
