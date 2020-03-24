f22f6fdf4351332a006d8234beac16d0ce56fc51
Aakanksha Rai <rai.aakanksha@gmail.com>
2020-03-24 18:45:18 +0800

    Add support to Module to sort

--- src/main/java/tatracker/model/module/Module.java
+++ src/main/java/tatracker/model/module/Module.java
@@ -89,6 +89,24 @@ public class Module {
     }
 
     /**
+     * Sorts students in the groups alphabetically.
+     */
+    public void sortGroupsAlphabetically() {
+        for(int i = 0; i < groups.size(); ++i) {
+            groups.get(i).sortStudentsAlphabetically();
+        }
+    }
+
+    /**
+     * Sorts the students in the groups by rating.
+     */
+    public void sortGroupsByRating() {
+        for(int i = 0; i < groups.size(); ++i) {
+            groups.get(i).sortStudentsByRating();
+        }
+    }
+
+    /**
      * Returns the group list.
      */
     public ObservableList<Group> getGroupList() {
