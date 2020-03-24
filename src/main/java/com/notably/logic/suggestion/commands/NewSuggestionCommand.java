bab085d5aceadf92144bdfcb82ffa1c187405e56
johannagwan <johannagwan@gmail.com>
2020-03-24 11:25:10 +0800

    Add NewSuggestionCommand

---
+++ src/main/java/com/notably/logic/suggestion/commands/NewSuggestionCommand.java
@@ -0,0 +1,28 @@
+package com.notably.logic.suggestion.commands;
+
+import java.util.Objects;
+
+import com.notably.commons.core.path.AbsolutePath;
+import com.notably.logic.suggestion.SuggestionCommand;
+import com.notably.model.Model;
+
+public class NewSuggestionCommand implements SuggestionCommand {
+    private static final String COMMAND_WORD = "new";
+    private static final String RESPONSE_MESSAGE = "Create a new note";
+
+    private AbsolutePath path;
+
+    public NewSuggestionCommand(AbsolutePath path) {
+        Objects.requireNonNull(path);
+        this.path = path;
+    }
+
+    @Override
+    public void execute(Model model) {
+        // Nullity check
+        Objects.requireNonNull(model);
+
+        // Set response text
+        model.setResponseText(RESPONSE_MESSAGE);
+    }
+}
