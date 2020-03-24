60790fc68dfcd564d09aa7fbf5428e9da7ad9d1a
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-03-24 17:14:57 +0800

    Populate SideBarTreeView using block data instead of hardcoded data

--- src/main/java/com/notably/view/SideBarTreeView.java
+++ src/main/java/com/notably/view/SideBarTreeView.java
@@ -2,9 +2,9 @@ package com.notably.view;
 
 import com.notably.model.block.Block;
 import com.notably.model.block.BlockTree;
-import com.notably.model.block.BlockTreeItem;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeCell;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.Region;
 
@@ -16,17 +16,35 @@ public class SideBarTreeView extends ViewPart<Region> {
     private static final String FXML = "SideBarTreeView.fxml";
 
     @FXML
-    private TreeView<String> sideBarTreeViewRoot;
+    private TreeView<Block> sideBarTreeView;
 
     public SideBarTreeView(BlockTree blockTree) {
         super(FXML);
+        initializeTree(blockTree);
+    }
+
+    /**
+     * Calls helper functions that set the tree settings and populate the SideBarTreeView with data.
+     * @param blockTree The custom tree used to populate the TreeView.
+     */
+    private void initializeTree(BlockTree blockTree) {
+        initializeTreeSettings();
         loadTreeItems(blockTree);
     }
 
-    private void loadTreeItems(BlockTree blockTree) {
-        loadTreeItemsStub();
+    /**
+     * Provides an alternative way to specify TreeView settings, versus via fxml tags.
+     */
+    private void initializeTreeSettings() {
+        sideBarTreeView.setEditable(false);
     }
 
+    private void loadTreeItems(BlockTree blockTree) {
+        sideBarTreeView.setRoot(blockTree.getRootBlock().getTreeItem());
+        sideBarTreeView.setCellFactory(treeView -> new SideBarTreeViewCell());
+    }
+
+    /*
     private void loadTreeItemsStub() {
         TreeItem<String> root = new TreeItem<>("Root");
         root.setExpanded(true);
@@ -37,6 +55,26 @@ public class SideBarTreeView extends ViewPart<Region> {
         notably.getChildren().add(new TreeItem<>("CS2103"));
         notably.getChildren().add(new TreeItem<>("ST2334"));
 
-        sideBarTreeViewRoot.setRoot(root);
+        //sideBarTreeViewRoot.setRoot(root);
+    }*/
+
+    /**
+     * Custom {@code TreeCell} that displays the text of a {@code Block}.
+     */
+    class SideBarTreeViewCell extends TreeCell<Block> {
+        @Override
+        protected void updateItem(Block block, boolean empty) {
+            super.updateItem(block, empty);
+
+            if (empty || block == null) {
+                setText(null);
+            } else {
+                setText(getNoteTitle());
+            }
+        }
+
+        private String getNoteTitle() {
+            return getItem().getTitle().getText();
+        }
     }
 }
