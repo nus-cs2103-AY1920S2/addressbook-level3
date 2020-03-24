20ec2bd262ec771057fd3cd9d7a742f8d05581d3
KwanXinJie <xjkwan1998@gmail.com>
2020-03-24 17:02:41 +0800

    checkstyle

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -12,7 +12,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_ASSIGNMENT_DISPLAYED_INDEX = "The assignment index is invalid.";
     public static final String MESSAGE_NO_INFO = "The person has no existing information!";
     public static final String MESSAGE_INVALID_LINE_NUMBER = "Line number does not exist in person's information.";
-    public static final String MESSAGE_TOO_MANY_FILTERS = "Too many filters! Either deadline or estimated completion" +
-            "time can be used to sort each time.";
+    public static final String MESSAGE_TOO_MANY_FILTERS = "Too many filters! Either deadline or estimated completion"
+            + "time can be used to sort each time.";
 
 }
