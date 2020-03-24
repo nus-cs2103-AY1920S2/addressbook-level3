cbe6a9224ac07452411e31de5e9548b59497b35f
sheexiong <chen97xiong@yahoo.com>
2020-03-24 12:17:49 +0800

    Add the amount field to store in the storage.

--- src/main/java/seedu/eylah/expensesplitter/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/eylah/expensesplitter/storage/JsonAdaptedPerson.java
@@ -1,9 +1,13 @@
 package seedu.eylah.expensesplitter.storage;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.eylah.commons.exceptions.IllegalValueException;
+import seedu.eylah.expensesplitter.model.person.Amount;
 import seedu.eylah.expensesplitter.model.person.Name;
 import seedu.eylah.expensesplitter.model.person.Person;
 
@@ -15,13 +19,15 @@ public class JsonAdaptedPerson {
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Person's %s field is missing!";
 
     private final String name;
+    private final BigDecimal amount;
 
     /**
      * Constructs a {@code JsonAdaptedPerson} with the given person details.
      */
     @JsonCreator
-    public JsonAdaptedPerson(@JsonProperty("name") String name) {
+    public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("amount") BigDecimal amount) {
         this.name = name;
+        this.amount = amount;
     }
 
     /**
@@ -29,7 +35,7 @@ public class JsonAdaptedPerson {
      */
     public JsonAdaptedPerson(Person source) {
         name = source.getName().fullName;
-
+        amount = source.getAmount().amount;
     }
 
     /**
@@ -45,10 +51,17 @@ public class JsonAdaptedPerson {
         if (!Name.isValidName(name)) {
             throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
         }
-
         final Name modelName = new Name(name);
 
-        return new Person(modelName, null);
+        if (amount == null) {
+            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Amount.class.getSimpleName()));
+        }
+        if (!Amount.isValidAmount(amount)) {
+            throw new IllegalValueException(Amount.MESSAGE_CONSTRAINTS);
+        }
+        final Amount modelAmount = new Amount(amount);
+
+        return new Person(modelName, modelAmount);
     }
 
 
