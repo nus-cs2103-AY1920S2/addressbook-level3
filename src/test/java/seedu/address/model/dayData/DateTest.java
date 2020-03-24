0ab74d2ee3072ebb8a8b6685b1e383bc01711044
Permas-Teo <permasteo@gmail.com>
2020-03-24 11:46:50 +0800

    add tests for Date, PomDurationData and TasksDoneData

---
+++ src/test/java/seedu/address/model/dayData/DateTest.java
@@ -0,0 +1,36 @@
+package seedu.address.model.dayData;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class DateTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new Date(null));
+    }
+
+    @Test
+    public void constructor_invalidDate_throwsIllegalArgumentException() {
+        String invalidDate = "hi";
+        assertThrows(IllegalArgumentException.class, () -> new Date(invalidDate));
+    }
+
+    @Test
+    public void isValidDate() {
+        // null date
+        assertThrows(NullPointerException.class, () -> Date.isValidDate(null));
+
+        // invalid dates
+        assertFalse(Date.isValidDate("")); // empty string
+        assertFalse(Date.isValidDate("12-12-1997")); // wrong format
+        assertFalse(Date.isValidDate("12/12/1997")); // wrong format
+        assertFalse(Date.isValidDate("not a date")); // wrong format
+
+        // valid dates
+        assertTrue(Date.isValidDate("2020-03-19"));
+    }
+}
