5c50a0fb9bf950a873dc240e18293fd24305b4ef
Kevin Putera <kevintiosanna@gmail.com>
2020-03-24 13:25:31 +0800

    Update Model and ModelManager dependencies

--- src/main/java/com/notably/logic/Logic.java
+++ src/main/java/com/notably/logic/Logic.java
@@ -5,7 +5,6 @@ import java.nio.file.Path;
 import com.notably.commons.core.GuiSettings;
 import com.notably.logic.commands.exceptions.CommandException;
 import com.notably.logic.parser.exceptions.ParseException;
-import com.notably.model.ReadOnlyAddressBook;
 
 /**
  * API of the Logic component
@@ -18,16 +17,6 @@ public interface Logic {
      * @throws ParseException If an error occurs during parsing.
      */
     void execute(String commandText) throws CommandException, ParseException;
-
-    /**
-     * Returns the AddressBook.
-     *
-     * @see com.notably.model.Model#getAddressBook()
-     */
-    ReadOnlyAddressBook getAddressBook();
-
-    /** Returns an unmodifiable view of the filtered list of persons */
-    void getFilteredPersonList();
 
     /**
      * Returns the user prefs' address book file path.
