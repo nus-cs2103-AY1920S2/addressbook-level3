42d8a80cef8256ef4fba44277084c6c56ee3e297
johannagwan <johannagwan@gmail.com>
2020-03-24 11:27:50 +0800

    Add javadocs

--- src/main/java/com/notably/logic/suggestion/commands/NewSuggestionCommand.java
+++ src/main/java/com/notably/logic/suggestion/commands/NewSuggestionCommand.java
@@ -6,6 +6,9 @@ import com.notably.commons.core.path.AbsolutePath;
 import com.notably.logic.suggestion.SuggestionCommand;
 import com.notably.model.Model;
 
+/**
+ * Represents a suggestion command object to create a new note.
+ */
 public class NewSuggestionCommand implements SuggestionCommand {
     private static final String COMMAND_WORD = "new";
     private static final String RESPONSE_MESSAGE = "Create a new note";
