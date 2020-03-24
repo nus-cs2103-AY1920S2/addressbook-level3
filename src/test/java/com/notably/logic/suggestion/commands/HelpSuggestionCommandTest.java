d7bfb692b307f01dd8e7d60a44f8686d9a2df452
johannagwan <johannagwan@gmail.com>
2020-03-24 18:48:24 +0800

    Remove path field from test

--- src/test/java/com/notably/logic/suggestion/commands/HelpSuggestionCommandTest.java
+++ src/test/java/com/notably/logic/suggestion/commands/HelpSuggestionCommandTest.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import com.notably.commons.core.path.AbsolutePath;
 import com.notably.model.Model;
 import com.notably.model.ModelManager;
 import com.notably.model.block.BlockModel;
@@ -18,7 +17,6 @@ import com.notably.model.viewstate.ViewStateModel;
 import com.notably.model.viewstate.ViewStateModelImpl;
 
 public class HelpSuggestionCommandTest {
-    private static AbsolutePath toRoot;
     private static Model model;
 
     @BeforeAll
@@ -28,9 +26,6 @@ public class HelpSuggestionCommandTest {
         SuggestionModel suggestionModel = new SuggestionModelImpl();
         ViewStateModel viewStateModel = new ViewStateModelImpl();
         model = new ModelManager(blockModel, suggestionModel, viewStateModel);
-
-        // Set up path
-        toRoot = AbsolutePath.fromString("/");
     }
 
     @Test
