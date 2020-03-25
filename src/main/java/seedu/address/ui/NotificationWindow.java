bf9a0b9746d9bab976ef944113ae37fbb9d49b55
junhaotan <40597739+junhaotan@users.noreply.github.com>
2020-03-25 15:27:40 +0800

    Add NotificationWindow class and fxml

---
+++ src/main/java/seedu/address/ui/NotificationWindow.java
@@ -0,0 +1,84 @@
+package seedu.address.ui;
+
+import java.util.logging.Logger;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import seedu.address.commons.core.LogsCenter;
+
+/**
+ * Controller for notification page.
+ */
+public class NotificationWindow extends UiPart<Stage> {
+
+    public static final String NOTIFICATION_MESSAGE = "Product %s quantity low! Remaining balance: %d";
+
+    private static final Logger logger = LogsCenter.getLogger(NotificationWindow.class);
+    private static final String FXML = "NotificationWindow.fxml";
+
+    @FXML
+    private Label notificationMessage;
+
+    /**
+     * Creates a new NotificationWindow.
+     *
+     * @param root Stage to use as root of the NotificationWindow.
+     */
+    public NotificationWindow(Stage root) {
+        super(FXML, root);
+        notificationMessage.setText(NOTIFICATION_MESSAGE);
+    }
+
+    /**
+     * Creates a new NotificationWindow.
+     */
+    public NotificationWindow() {
+        this(new Stage());
+    }
+
+    /**
+     * Shows the help window.
+     * @throws IllegalStateException
+     * <ul>
+     *     <li>
+     *         if this method is called on a thread other than the JavaFX Application Thread.
+     *     </li>
+     *     <li>
+     *         if this method is called during animation or layout processing.
+     *     </li>
+     *     <li>
+     *         if this method is called on the primary stage.
+     *     </li>
+     *     <li>
+     *         if {@code dialogStage} is already showing.
+     *     </li>
+     * </ul>
+     */
+    public void show() {
+        logger.fine("Showing notification window.");
+        getRoot().show();
+        getRoot().centerOnScreen();
+    }
+
+    /**
+     * Returns true if the notification window is currently being shown.
+     */
+    public boolean isShowing() {
+        return getRoot().isShowing();
+    }
+
+    /**
+     * Hides the notification window.
+     */
+    public void hide() {
+        getRoot().hide();
+    }
+
+    /**
+     * Focuses on the notification window.
+     */
+    public void focus() {
+        getRoot().requestFocus();
+    }
+}
