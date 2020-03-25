02b1dd1e5087ec3e3844c2d91d1bb62bebee88f1
beatricetay <beatricetayyt@gmail.com>
2020-03-25 16:29:01 +0800

    added more functionalities to planning feature

--- src/main/java/seedu/address/model/plan/PlannedRecipeMap.java
+++ src/main/java/seedu/address/model/plan/PlannedRecipeMap.java
@@ -38,8 +38,26 @@ public class PlannedRecipeMap {
      * Gets the recipes planned at a specific date.
      * If no recipes were planned on that day, return null.
      */
-    public List<Recipe> get(Date date) {
+    public List<Recipe> getRecipeAt(Date date) {
         return plannedRecipes.get(date);
+    }
+
+    /**
+     * Returns a list of recipes that were planned from the period starting from date
+     * to 'daysFromDate' number of days since that day.
+     */
+    public List<Recipe> getRecipeInRange(Date date, int daysFromDate) {
+        List<Recipe> recipes = new ArrayList<>();
+        while (daysFromDate > 0 && hasLaterDate(date)) {
+            // check if later date is more than days from date
+            date = plannedRecipes.higherKey(date);
+            recipes.addAll(getRecipeAt(date));
+            // subtract jump and update days from date
+        }
+    }
+
+    public boolean hasLaterDate(Date date) {
+        return plannedRecipes.higherKey(date) != null;
     }
 
     /**
