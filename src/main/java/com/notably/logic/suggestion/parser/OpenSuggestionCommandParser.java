2e0b4a3067f73ffa34c4b033e51612f3a58df2d9
johannagwan <johannagwan@gmail.com>
2020-03-25 22:11:23 +0800

    Add constructor with model as param

--- src/main/java/com/notably/logic/suggestion/parser/OpenSuggestionCommandParser.java
+++ src/main/java/com/notably/logic/suggestion/parser/OpenSuggestionCommandParser.java
@@ -9,6 +9,7 @@ import com.notably.commons.core.path.RelativePath;
 import com.notably.commons.core.path.exceptions.InvalidPathException;
 import com.notably.logic.parser.ArgumentMultimap;
 import com.notably.logic.parser.ArgumentTokenizer;
+import com.notably.logic.parser.ParserUtil;
 import com.notably.logic.parser.Prefix;
 import com.notably.logic.parser.exceptions.ParseException;
 import com.notably.logic.suggestion.commands.OpenSuggestionCommand;
@@ -19,6 +20,12 @@ import com.notably.model.ModelManager;
  * Represents a Parser for OpenSuggestionCommand.
  */
 public class OpenSuggestionCommandParser implements SuggestionCommandParser<OpenSuggestionCommand> {
+    Model model;
+
+    public OpenSuggestionCommandParser(Model model) {
+        this.model = model;
+    }
+
     /**
      * Parses user input in the context of the OpenSuggestionCommand.
      * @param userInput The user's input.
@@ -29,7 +36,7 @@ public class OpenSuggestionCommandParser implements SuggestionCommandParser<Open
     public OpenSuggestionCommand parse(String userInput) throws ParseException {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(userInput, PREFIX_TITLE);
-        if (!arePrefixesPresent(argMultimap, PREFIX_TITLE)
+        if (!ParserUtil.arePrefixesPresent(argMultimap, PREFIX_TITLE)
                 || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format("Invalid input"));
         }
@@ -42,7 +49,6 @@ public class OpenSuggestionCommandParser implements SuggestionCommandParser<Open
                 openSuggestionCommand = new OpenSuggestionCommand(AbsolutePath.fromString(titles));
             } else {
                 // convert relative path to absolute path first, as suggestion commands only deal with absolute path.
-                Model model = new ModelManager();
                 RelativePath relativePath = RelativePath.fromString(titles);
                 AbsolutePath currentWorkingPath = model.getCurrentlyOpenPath();
                 AbsolutePath absolutePath = AbsolutePath.fromRelativePath(relativePath, currentWorkingPath);
@@ -53,14 +59,5 @@ public class OpenSuggestionCommandParser implements SuggestionCommandParser<Open
         } catch (InvalidPathException ex) {
             throw new ParseException(ex.getMessage());
         }
-    }
-
-    //TODO: to be removed when this method is moved to ParserUtil by Jia Zheng.
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
-        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
