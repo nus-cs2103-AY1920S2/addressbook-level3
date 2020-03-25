f4399129af49b50fb6acf25bfb6e5f2d63410d5a
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-03-25 22:48:48 +0800

    Added addIngredient command

--- src/main/java/seedu/address/logic/parser/RecipeBookParser.java
+++ src/main/java/seedu/address/logic/parser/RecipeBookParser.java
@@ -7,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddIngredientCommand;
 import seedu.address.logic.commands.AddStepCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
@@ -99,6 +100,9 @@ public class RecipeBookParser {
 
         case EditStepCommand.COMMAND_WORD:
             return new EditStepCommandParser().parse(arguments);
+
+        case AddIngredientCommand.COMMAND_WORD:
+            return new AddIngredientCommandParser().parse(arguments);
 
         default:
             throw new ParseException(MESSAGE_UNKNOWN_COMMAND);
