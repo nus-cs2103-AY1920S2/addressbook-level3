ac41d6f3f41dffdbc5998f319dbfd96be73d9ecc
johannagwan <johannagwan@gmail.com>
2020-03-25 23:43:34 +0800

    Link to AbsolutePathCorrectionEngine

--- src/main/java/com/notably/logic/suggestion/parser/OpenSuggestionCommandParser.java
+++ src/main/java/com/notably/logic/suggestion/parser/OpenSuggestionCommandParser.java
@@ -2,35 +2,35 @@ package com.notably.logic.suggestion.parser;
 
 import static com.notably.logic.parser.CliSyntax.PREFIX_TITLE;
 
-import java.util.stream.Stream;
+import java.util.Optional;
 
 import com.notably.commons.core.path.AbsolutePath;
-import com.notably.commons.core.path.RelativePath;
-import com.notably.commons.core.path.exceptions.InvalidPathException;
+import com.notably.logic.correction.AbsolutePathCorrectionEngine;
 import com.notably.logic.parser.ArgumentMultimap;
 import com.notably.logic.parser.ArgumentTokenizer;
 import com.notably.logic.parser.ParserUtil;
-import com.notably.logic.parser.Prefix;
 import com.notably.logic.parser.exceptions.ParseException;
 import com.notably.logic.suggestion.commands.OpenSuggestionCommand;
 import com.notably.model.Model;
-import com.notably.model.ModelManager;
 
 /**
  * Represents a Parser for OpenSuggestionCommand.
  */
 public class OpenSuggestionCommandParser implements SuggestionCommandParser<OpenSuggestionCommand> {
-    Model model;
+    private final int distanceThreshold = 2;
+    private Model model;
+    private AbsolutePathCorrectionEngine correctionEngine;
 
     public OpenSuggestionCommandParser(Model model) {
         this.model = model;
+        this.correctionEngine = new AbsolutePathCorrectionEngine(model, distanceThreshold);
     }
 
     /**
      * Parses user input in the context of the OpenSuggestionCommand.
      * @param userInput The user's input.
-     * @return An OpenSuggestionCommand object with an absolute path.
-     * @throws ParseException if the user input does not conform to the expected format.
+     * @return An OpenSuggestionCommand object with a corrected absolute path.
+     * @throws ParseException if the user input path cannot be found.
      */
     @Override
     public OpenSuggestionCommand parse(String userInput) throws ParseException {
@@ -42,22 +42,13 @@ public class OpenSuggestionCommandParser implements SuggestionCommandParser<Open
         }
 
         String titles = argMultimap.getValue(PREFIX_TITLE).get();
-        OpenSuggestionCommand openSuggestionCommand;
+        AbsolutePath uncorrectedPath = ParserUtil.createAbsolutePath(titles, model.getCurrentlyOpenPath());
+        Optional<AbsolutePath> correctedPath = correctionEngine.correct(uncorrectedPath).getCorrectedItem();
 
-        try {
-            if (titles.charAt(0) == '/') {
-                openSuggestionCommand = new OpenSuggestionCommand(AbsolutePath.fromString(titles));
-            } else {
-                // convert relative path to absolute path first, as suggestion commands only deal with absolute path.
-                RelativePath relativePath = RelativePath.fromString(titles);
-                AbsolutePath currentWorkingPath = model.getCurrentlyOpenPath();
-                AbsolutePath absolutePath = AbsolutePath.fromRelativePath(relativePath, currentWorkingPath);
-
-                openSuggestionCommand = new OpenSuggestionCommand(absolutePath);
-            }
-            return openSuggestionCommand;
-        } catch (InvalidPathException ex) {
-            throw new ParseException(ex.getMessage());
+        if (correctedPath.equals(Optional.empty())) {
+            throw new ParseException("Invalid Path");
         }
+
+        return new OpenSuggestionCommand(correctedPath.get());
     }
 }
