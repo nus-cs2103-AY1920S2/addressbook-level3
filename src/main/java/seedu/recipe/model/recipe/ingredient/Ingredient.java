86bae05473eec86534022da61a058cc013a1ff6a
beatricetay <beatricetayyt@gmail.com>
2020-03-26 13:10:08 +0800

    fixed

--- src/main/java/seedu/recipe/model/recipe/ingredient/Ingredient.java
+++ src/main/java/seedu/recipe/model/recipe/ingredient/Ingredient.java
@@ -8,12 +8,12 @@ import static seedu.recipe.commons.util.AppUtil.checkArgument;
  */
 public abstract class Ingredient implements Comparable<Ingredient> {
 
-    public static final String MESSAGE_CONSTRAINTS = "Ingredient names should contain only "
-            + "alphabetical letters, spaces, or special characters from this set {&, %, (, ), \\-}.";
+    public static final String MESSAGE_CONSTRAINTS = "Ingredient names should contain "
+            + "alphanumeric characters, spaces, or special characters from this set {&, %, (, ), -}.";
     public static final String MESSAGE_MISSING_FIELD = "Ingredients require a quantity and name written in the format: "
             + "Tag/Quantity, Name\n"
             + "For example, to enter 50g of Broccoli, the format is iv/50g, Broccoli";
-    public static final String VALIDATION_REGEX = "^[A-Za-z()&%\\-]+$+";
+    public static final String VALIDATION_REGEX = "^[\\sA-Za-z0-9\\()&%-]+$+";
 
     protected String ingredientName;
     protected Quantity quantity;
