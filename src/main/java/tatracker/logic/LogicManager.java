fa42267b7245639bc1315f21e894ff7e4143a84b
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2020-03-26 17:18:59 +0800

    Update LogicManager constructor for consistency

--- src/main/java/tatracker/logic/LogicManager.java
+++ src/main/java/tatracker/logic/LogicManager.java
@@ -34,7 +34,7 @@ public class LogicManager implements Logic {
     public LogicManager(Model model, Storage storage) {
         this.model = model;
         this.storage = storage;
-        taTrackerParser = new TaTrackerParser();
+        this.taTrackerParser = new TaTrackerParser();
     }
 
     @Override
