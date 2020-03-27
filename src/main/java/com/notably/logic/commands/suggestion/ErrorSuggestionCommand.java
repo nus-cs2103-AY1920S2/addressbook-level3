1d149db3ad2d9960c44afd98e80f97963bad7ec8
johannagwan <johannagwan@gmail.com>
2020-03-28 01:44:21 +0800

    Add javadoc

--- src/main/java/com/notably/logic/commands/suggestion/ErrorSuggestionCommand.java
+++ src/main/java/com/notably/logic/commands/suggestion/ErrorSuggestionCommand.java
@@ -4,10 +4,14 @@ import java.util.Objects;
 
 import com.notably.model.Model;
 
+/**
+ * Represents a suggestion command object to notify that the user command input is invalid.
+ */
 public class ErrorSuggestionCommand implements SuggestionCommand {
     private String errorMessage;
 
     public ErrorSuggestionCommand(String errorMessage) {
+        Objects.requireNonNull(errorMessage);
         this.errorMessage = errorMessage;
     }
 
---
+++ src/test/java/com/notably/logic/commands/suggestion/ErrorSuggestionCommandTest.java
@@ -0,0 +1,4 @@
+package com.notably.logic.commands.suggestion;
+
+public class ErrorSuggestionCommandTest {
+}
