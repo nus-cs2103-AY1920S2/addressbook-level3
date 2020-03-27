27bd6b05b4e94db772c525a26efd662d1c7d3640
Cherweijie <44356103+Cherweijie@users.noreply.github.com>
2020-03-28 01:31:18 +0800

    Implement tests for new Done Command

--- src/test/java/seedu/address/logic/parser/DoneCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DoneCommandParserTest.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.FLAG_ORDER_LIST;
+import static seedu.address.logic.parser.CliSyntax.FLAG_RETURN_LIST;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_ORDER;
@@ -18,9 +20,14 @@ public class DoneCommandParserTest {
     @Test
     public void parse_validArgs_returnsDoneCommand() {
         DoneCommand.DoneOrderDescriptor descriptor = new DoneCommand.DoneOrderDescriptor();
-        assertParseSuccess(parser, "1", new DoneCommand(INDEX_FIRST_ORDER, descriptor));
-        assertParseSuccess(parser, "2", new DoneCommand(INDEX_SECOND_ORDER, descriptor));
-        assertParseSuccess(parser, "3", new DoneCommand(INDEX_THIRD_ORDER, descriptor));
+        assertParseSuccess(parser, "1 -o", new DoneCommand(INDEX_FIRST_ORDER, FLAG_ORDER_LIST, descriptor));
+        assertParseSuccess(parser, "2 -o", new DoneCommand(INDEX_SECOND_ORDER, FLAG_ORDER_LIST, descriptor));
+        assertParseSuccess(parser, "3 -o", new DoneCommand(INDEX_THIRD_ORDER, FLAG_ORDER_LIST, descriptor));
+
+        assertParseSuccess(parser, "1 -r", new DoneCommand(INDEX_FIRST_ORDER, FLAG_RETURN_LIST, descriptor));
+        assertParseSuccess(parser, "2 -r", new DoneCommand(INDEX_SECOND_ORDER, FLAG_RETURN_LIST, descriptor));
+        assertParseSuccess(parser, "3 -r", new DoneCommand(INDEX_THIRD_ORDER, FLAG_RETURN_LIST, descriptor));
+
     }
 
     @Test
