54d815d642631fd2b1ec1f2109e39f40222f57ec
NicholasCF <nicholas.fernando@u.nus.edu>
2020-03-27 14:50:27 +0800

    Add success message for UndoCommand

--- src/main/java/seedu/address/logic/commands/UndoCommand.java
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -6,6 +6,8 @@ import seedu.address.model.Model;
 public class UndoCommand extends Command {
     public static final String COMMAND_WORD = "undo";
 
+    public static final String MESSAGE_SUCCESS = "Managed to undo previous action!";
+
     @Override
     public CommandResult execute(Model model) throws CommandException {
         return null;
