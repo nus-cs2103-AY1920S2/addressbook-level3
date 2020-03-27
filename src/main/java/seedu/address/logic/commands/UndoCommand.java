05e3801b04f01c0fa323a70f746cdf5f167cea82
NicholasCF <nicholas.fernando@u.nus.edu>
2020-03-27 12:25:46 +0800

    Add UndoCommand skeleton

---
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -0,0 +1,13 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+public class UndoCommand extends Command {
+    public static final String COMMAND_WORD = "undo";
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        return null;
+    }
+}
