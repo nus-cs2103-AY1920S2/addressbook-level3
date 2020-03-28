5e6b41d074b52bf6721774d21d7909dd81f0d990
Guofeng-Tang <tgf.tang.guofeng@gmail.com>
2020-03-28 23:57:26 +0800

    add RecurringSchedule skeleton class

---
+++ src/main/java/seedu/zerotoone/model/schedule/RecurringSchedule.java
@@ -0,0 +1,39 @@
+package seedu.zerotoone.model.schedule;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import seedu.zerotoone.model.exercise.Exercise;
+
+/**
+ * STEPH_TODO_JAVADOC
+ */
+public class RecurringSchedule implements Schedule {
+
+    private final Exercise workoutToSchedule; // TO_CHANGE_EXERCISE_TO_WORKOUT
+    private final DateTime startDate;
+    private final DateTime endDate;
+
+    // private final List<CanceledScheudledWorkout>
+    // private final List<EditedScheduledWorkout>
+
+    public RecurringSchedule(Exercise workoutToSchedule, DateTime startDate, DateTime endDate) {
+        // TO_CHANGE_EXERCISE_TO_WORKOUT
+        this.workoutToSchedule = workoutToSchedule;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    @Override
+    public Optional<List<ScheduledWorkout>> getScheduledWorkout() {
+        return Optional.empty();
+        // do I have any scheduled workouts after today?
+        // are some instances of them edited/deleted?
+        // return the list of scheduled workouts that are not deleted along with those that are edited, that are
+        // scheduled after today
+
+        // ScheduledWorkout scheduledWorkout = new ScheduledWorkout(workoutToSchedule, dateTime);
+        // return Optional.of(Collections.singletonList(scheduledWorkout));
+    }
+}
---
+++ src/main/java/seedu/zerotoone/storage/schedule/ScheduleListStorage.java
@@ -0,0 +1,4 @@
+package seedu.zerotoone.storage.schedule;
+
+public class ScheduleListStorage {
+}
