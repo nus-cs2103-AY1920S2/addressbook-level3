961e3853d8e1856a6abfc0945a848415b91ed827
Kevin Putera <kevintiosanna@gmail.com>
2020-03-28 15:42:09 +0800

    Define types of tokens

---
+++ src/main/java/com/notably/commons/compiler/tokenizer/TokenType.java
@@ -0,0 +1,15 @@
+package com.notably.commons.compiler.tokenizer;
+
+/**
+ * TODO: Add Javadoc
+ */
+public enum TokenType {
+    MINUS,
+    HASH,
+    CR,
+    LF,
+    SPACE,
+    TEXT,
+    EOF;
+}
+
