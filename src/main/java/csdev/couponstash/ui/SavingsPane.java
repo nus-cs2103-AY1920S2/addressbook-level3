720e990a57df52a92e6f6d1375927f604d2189d1
Alden <49450743+alcen@users.noreply.github.com>
2020-03-29 01:32:08 +0800

    Dynamic font size for SavingsPane numerical amount
    * Centre saveable items in FlowPane
    
    * Set font size for saveable items

--- src/main/java/csdev/couponstash/ui/SavingsPane.java
+++ src/main/java/csdev/couponstash/ui/SavingsPane.java
@@ -9,8 +9,9 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 public class SavingsPane extends UiPart<Region> {
-
     private static final String FXML = "SavingsPane.fxml";
+    private static final String HIDDEN = "visibility: hidden;";
+    private static final String SAVEABLE_CLASS = "sv-label";
 
     @FXML
     private VBox savingsPane;
@@ -32,12 +33,20 @@ public class SavingsPane extends UiPart<Region> {
     public void setSavings(Savings s, String moneySymbol) {
         String savingsNumber = getSavingsString(s, moneySymbol);
         if (savingsNumber.isBlank()) {
-            this.numericalAmount.setStyle("visibility: hidden;");
+            this.numericalAmount.setStyle(SavingsPane.HIDDEN);
         } else {
             this.numericalAmount.setText(savingsNumber);
+            this.numericalAmount.setStyle("-fx-font-size: "
+                    + (125 / savingsNumber.length())
+                    + ";");
         }
-        s.getSaveables().ifPresent(saveablesList -> saveablesList.stream()
-            .forEach(sva -> saveables.getChildren().add(new Label(sva.getValue()))));
+        s.getSaveables().ifPresentOrElse(saveablesList -> saveablesList.stream()
+            .forEach(sva -> {
+                Label label = new Label(sva.getValue());
+                // ensure that label has the correct CSS style
+                label.getStyleClass().add(SavingsPane.SAVEABLE_CLASS);
+                saveables.getChildren().add(label);
+            }), () -> this.saveables.setStyle(SavingsPane.HIDDEN));
     }
 
     private static String getSavingsString(Savings s, String moneySymbol) {
