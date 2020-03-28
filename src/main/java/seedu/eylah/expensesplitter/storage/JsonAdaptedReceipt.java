43dd9239c7da748b41f5d7bfbcff47e13a6a98d5
sheexiong <chen97xiong@yahoo.com>
2020-03-28 18:19:57 +0800

    Temp work on the JsonAdaptedReceipt.

--- src/main/java/seedu/eylah/expensesplitter/storage/JsonAdaptedReceipt.java
+++ src/main/java/seedu/eylah/expensesplitter/storage/JsonAdaptedReceipt.java
@@ -1,5 +1,15 @@
 package seedu.eylah.expensesplitter.storage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import seedu.eylah.addressbook.model.person.Person;
+import seedu.eylah.expensesplitter.model.receipt.Receipt;
+
 /**
  * Jackson-friendly version of {@link Receipt}.
  */
@@ -7,5 +17,24 @@ public class JsonAdaptedReceipt {
 
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Receipt's %s field is missing!";
 
+    private final List<JsonAdaptedPerson> itemPersons = new ArrayList<>();
 
+    /**
+     * Constructs a {@code JsonAdaptedPerson} with the given person details.
+     */
+    @JsonCreator
+    public JsonAdaptedReceipt(@JsonProperty("itemPersons") List<JsonAdaptedPerson> itemPersons) {
+        if (itemPersons != null) {
+            this.itemPersons.addAll(itemPersons);
+        }
+    }
+
+    /**
+     * Converts a given {@code Person} into this class for Jackson use.
+     */
+    public JsonAdaptedReceipt(Receipt source) {
+        itemPersons.addAll(source.getReceipt().stream()
+                .map(JsonAdaptedPerson::new)
+                .collect(Collectors.toList()));
+    }
 }
