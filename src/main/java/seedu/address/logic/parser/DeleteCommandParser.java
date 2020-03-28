8d34a9b8db02180d42d29db42d9e71258bd98d8b
KwanXinJie <xjkwan1998@gmail.com>
2020-03-28 19:35:22 +0800

    edited commands with organization field

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -2,13 +2,7 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_PREFIX;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_BIRTHDAY;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -38,7 +32,7 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
         Index index;
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS, PREFIX_REMARK,
-                        PREFIX_BIRTHDAY, PREFIX_TAG);
+                        PREFIX_BIRTHDAY, PREFIX_ORGANIZATION, PREFIX_TAG);
 
         try {
             index = ParserUtil.parseIndex(argMultimap.getPreamble());
@@ -58,6 +52,9 @@ public class DeleteCommandParser implements Parser<DeleteCommand> {
             editPersonDescriptor.setAddress(ParserUtil.parseAddress(""));
         }
         if (argMultimap.getValue(PREFIX_BIRTHDAY).isPresent()) {
+            editPersonDescriptor.setBirthday(ParserUtil.parseBirthday(""));
+        }
+        if (argMultimap.getValue(PREFIX_ORGANIZATION).isPresent()) {
             editPersonDescriptor.setBirthday(ParserUtil.parseBirthday(""));
         }
         parseRemarksToDelete(argMultimap.getAllValues(PREFIX_REMARK)).ifPresent(editPersonDescriptor::setRemarks);
