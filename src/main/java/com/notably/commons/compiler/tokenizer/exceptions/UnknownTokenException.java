14340445ce897394659e125e508d265c42b891e1
Kevin Putera <kevintiosanna@gmail.com>
2020-03-28 15:42:28 +0800

    Define UnknownTokenException

---
+++ src/main/java/com/notably/commons/compiler/tokenizer/exceptions/UnknownTokenException.java
@@ -0,0 +1,15 @@
+package com.notably.commons.compiler.tokenizer.exceptions;
+
+/**
+ * TODO: Add Javadoc
+ */
+public class UnknownTokenException extends RuntimeException {
+    public UnknownTokenException() {
+        super();
+    }
+
+    public UnknownTokenException(String message) {
+        super(message);
+    }
+}
+
