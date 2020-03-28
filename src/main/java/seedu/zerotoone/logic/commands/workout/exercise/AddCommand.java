7cdeeca5a12d786bbc2f58491d686e85a6b7d1f5
wongchishan <wongchishan99@gmail.com>
2020-03-28 22:15:08 +0800

    Rename workout exercise command from ExerciseCommand to WorkoutExerciseCommand

--- src/main/java/seedu/zerotoone/logic/commands/workout/exercise/AddCommand.java
+++ src/main/java/seedu/zerotoone/logic/commands/workout/exercise/AddCommand.java
@@ -11,7 +11,7 @@ import seedu.zerotoone.commons.core.Messages;
 import seedu.zerotoone.commons.core.index.Index;
 import seedu.zerotoone.logic.commands.CommandResult;
 import seedu.zerotoone.logic.commands.exceptions.CommandException;
-import seedu.zerotoone.logic.commands.workout.exercise.ExerciseCommand;
+import seedu.zerotoone.logic.commands.workout.exercise.WorkoutExerciseCommand;
 import seedu.zerotoone.model.Model;
 import seedu.zerotoone.model.exercise.Exercise;
 import seedu.zerotoone.model.workout.Workout;
@@ -19,7 +19,7 @@ import seedu.zerotoone.model.workout.Workout;
 /**
  * Edits the details of an existing exercise in the exercise list.
  */
-public class AddCommand extends ExerciseCommand {
+public class AddCommand extends WorkoutExerciseCommand {
     public static final String COMMAND_WORD = "add";
     public static final String MESSAGE_USAGE = "Usage: exercise set add WORKOUT_ID EXERCISE_ID";
     public static final String MESSAGE_EDIT_WORKOUT_SUCCESS = "Added workout exercise: %1$s";
--- src/main/java/seedu/zerotoone/logic/commands/workout/exercise/WorkoutExerciseCommand.java
+++ src/main/java/seedu/zerotoone/logic/commands/workout/exercise/WorkoutExerciseCommand.java
@@ -5,6 +5,6 @@ import seedu.zerotoone.logic.commands.Command;
 /**
  * Represents a command with hidden internal logic and the ability to be executed.
  */
-public abstract class ExerciseCommand extends Command {
+public abstract class WorkoutExerciseCommand extends Command {
     public static final String COMMAND_WORD = "exercise";
 }
