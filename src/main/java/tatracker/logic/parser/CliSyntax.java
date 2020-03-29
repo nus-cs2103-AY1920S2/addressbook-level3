8f8a0762fb1e9cd09539f706491d99d07b6ec98c
Aakanksha Rai <rai.aakanksha@gmail.com>
2020-03-29 22:45:42 +0800

    Add EditGroupCommand class

--- src/main/java/tatracker/logic/parser/CliSyntax.java
+++ src/main/java/tatracker/logic/parser/CliSyntax.java
@@ -21,5 +21,5 @@ public class CliSyntax {
     public static final Prefix PREFIX_MODULE = new Prefix("m/");
     public static final Prefix PREFIX_GROUP = new Prefix("g/");
     public static final Prefix PREFIX_TYPE = new Prefix("t/");
-
-}
+    public static final Prefix PREFIX_NEWTYPE = new Prefix("nt/");
+    public static final Prefix PREFIX_NEWGROUP = new Prefix("ng/");}
