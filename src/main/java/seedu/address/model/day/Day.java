9e67e82313d867d9f696883e09cfe66aadfea486
Elysia Tan <>
2020-03-30 02:09:14 +0800

    Components for the Schedule Visual.

---
+++ src/main/java/seedu/address/model/day/Day.java
@@ -0,0 +1,45 @@
+package seedu.address.model.day;
+
+import java.util.ArrayList;
+
+/**
+ * Represents a day in the schedule visual.
+ */
+public class Day {
+    private Hours hours;
+    private ArrayList<DueAssignment> dueAssignments;
+
+    public Day() {
+        this.dueAssignments = new ArrayList<DueAssignment>();
+    }
+
+    public Hours getHours() {
+        return hours;
+    }
+
+    public void setHours(float toAdd) {
+        hours = new Hours(toAdd);
+    }
+
+    public ArrayList<DueAssignment> getDueAssignments() {
+        return dueAssignments;
+    }
+
+    public void addDueAssignment(String assignment) {
+        dueAssignments.add(new DueAssignment(assignment));
+    }
+
+    public String getDueAssignmentsToString() {
+        String result = "";
+
+        if (dueAssignments.size() != 0) {
+            result = result + "1. " + dueAssignments.get(0).assignment;
+        }
+
+        for (int i = 1; i < dueAssignments.size(); i++) {
+            result = result + "\n" + (i + 1) + ". " + dueAssignments.get(i).assignment;
+        }
+        return result;
+    }
+
+}
