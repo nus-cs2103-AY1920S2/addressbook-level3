7276c56d059e826fd486e061c00079f7aaf1c974
chuayijing <yijing.chua@gmail.com>
2020-03-30 00:02:33 +0800

    Update List to show all sessions

--- src/main/java/tatracker/logic/commands/ListCommand.java
+++ src/main/java/tatracker/logic/commands/ListCommand.java
@@ -3,6 +3,7 @@ package tatracker.logic.commands;
 import tatracker.model.Model;
 
 import static java.util.Objects.requireNonNull;
+import static tatracker.model.Model.PREDICATE_SHOW_ALL_SESSIONS;
 
 /**
  * Lists all students in the TA-Tracker to the user.
@@ -11,13 +12,14 @@ public class ListCommand extends Command {
 
     public static final String COMMAND_WORD = "list";
 
-    public static final String MESSAGE_SUCCESS = "Listed all students";
+    public static final String MESSAGE_SUCCESS = "Listed all sessions";
 
 
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        //model.updateFilteredStudentList(PREDICATE_SHOW_ALL_STUDENTS);
+        model.updateFilteredSessionList(PREDICATE_SHOW_ALL_SESSIONS);
+        model.updateFilteredDoneSessionList(PREDICATE_SHOW_ALL_SESSIONS);
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
