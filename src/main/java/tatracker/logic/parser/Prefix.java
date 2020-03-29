ee4ac882e35faa4d24ba858de5e261aa09c46f8e
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2020-03-30 05:19:51 +0800

    Add prefix info and String formatting

--- src/main/java/tatracker/logic/parser/Prefix.java
+++ src/main/java/tatracker/logic/parser/Prefix.java
@@ -1,18 +1,64 @@
 package tatracker.logic.parser;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * A prefix that marks the beginning of an argument in an arguments string.
  * E.g. 't/' in 'add James t/ friend'.
  */
 public class Prefix {
+    public static final String DELIMITER = "/";
+
+    private static final String OPTIONAL_FORMAT = "[%s]";
+    private static final String MULTIPLE_FORMAT = "%s...";
+
     private final String prefix;
 
+    private final String constraint;
+    private final String usage;
+    private final List<String> examples;
+
     public Prefix(String prefix) {
+        this(prefix, "", "", "");
+    }
+
+    public Prefix(String prefix, String constraint, String usage, String ... examples) {
         this.prefix = prefix;
+        this.constraint = constraint;
+        this.usage = usage;
+        this.examples = Arrays.asList(examples);
     }
 
     public String getPrefix() {
         return prefix;
+    }
+
+    public String getConstraint() {
+        return constraint;
+    }
+
+    public String getExamples() {
+        return examples.stream()
+                .map(e -> prefix + e)
+                .collect(Collectors.joining(" "));
+    }
+
+    public String getUsage() {
+        return getMultipleUsage(prefix + usage);
+    }
+
+    public String getOptionalUsage() {
+        return getMultipleUsage(String.format(OPTIONAL_FORMAT, prefix + usage));
+    }
+
+    private String getMultipleUsage(String usage) {
+        if (examples.size() <= 1) {
+            return usage;
+        } else {
+            return String.format(MULTIPLE_FORMAT, usage);
+        }
     }
 
     public String toString() {
--- src/main/java/tatracker/logic/parser/Prefixes.java
+++ src/main/java/tatracker/logic/parser/Prefixes.java
@@ -1,5 +1,17 @@
 package tatracker.logic.parser;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import tatracker.model.student.Email;
+import tatracker.model.student.Matric;
+import tatracker.model.student.Name;
+import tatracker.model.student.Phone;
+import tatracker.model.student.Rating;
+import tatracker.model.tag.Tag;
+
 /**
  * Contains Command Line Interface (CLI) syntax definitions common to multiple commands
  */
@@ -9,24 +21,41 @@ public class Prefixes {
     public static final Prefix START_TIME = new Prefix("s/");
     public static final Prefix END_TIME = new Prefix("e/");
     public static final Prefix DATE = new Prefix("d/");
-    public static final Prefix RECUR = new Prefix("-r");
+    public static final Prefix RECUR = new Prefix("w/");
     public static final Prefix SESSION_TYPE = new Prefix("t/");
-
-    /* Module definitions */
-    public static final Prefix MODULE = new Prefix("m/");
     public static final Prefix NOTES = new Prefix("n/");
 
+    /* Module definitions */
+    public static final Prefix MODULE = new Prefix("m/", "", "MODULE", "CS3243");
+    public static final Prefix MODULE_NAME = new Prefix("n/", "", "DESCRIPTION", "Introduction to AI");
+
     /* Group definitions */
-    public static final Prefix GROUP = new Prefix("g/");
-    public static final Prefix TYPE = new Prefix("t/");
+    public static final Prefix GROUP = new Prefix("g/", "", "GROUP", "G06");
+    public static final Prefix TYPE = new Prefix("t/", "", "GROUP_TYPE", "tutorial");
     public static final Prefix NEWTYPE = new Prefix("nt/");
     public static final Prefix NEWGROUP = new Prefix("ng/");
 
     /* Student definitions */
-    public static final Prefix MATRIC = new Prefix("id/");
-    public static final Prefix NAME = new Prefix("n/");
-    public static final Prefix PHONE = new Prefix("p/");
-    public static final Prefix EMAIL = new Prefix("e/");
-    public static final Prefix RATING = new Prefix("r/");
-    public static final Prefix TAG = new Prefix("t/");
+    public static final Prefix MATRIC = new Prefix("id/", Matric.MESSAGE_CONSTRAINTS, "MATRIC", "A0181234G");
+    public static final Prefix NAME = new Prefix("n/", Name.MESSAGE_CONSTRAINTS, "NAME", "John Doe");
+    public static final Prefix PHONE = new Prefix("p/", Phone.MESSAGE_CONSTRAINTS, "PHONE", "98765432");
+    public static final Prefix EMAIL = new Prefix("e/", Email.MESSAGE_CONSTRAINTS, "EMAIL", "johnd@example.com");
+    public static final Prefix RATING = new Prefix("r/", Rating.MESSAGE_CONSTRAINTS, "RATING", "3");
+    public static final Prefix TAG = new Prefix("t/", Tag.MESSAGE_CONSTRAINTS, "TAG", "friends", "owes money");
+
+    public static String getExamples(Prefix ... prefixes) {
+        return formatPrefixes(Arrays.asList(prefixes), Prefix::getExamples);
+    }
+
+    public static String getUsages(List<Prefix> parameters, List<Prefix> optionals) {
+        return String.format("%s %s",
+                formatPrefixes(parameters, Prefix::getUsage),
+                formatPrefixes(optionals, Prefix::getOptionalUsage));
+    }
+
+    private static String formatPrefixes(List<Prefix> prefixes, Function<Prefix, String> mapper) {
+        return prefixes.stream()
+                .map(mapper)
+                .collect(Collectors.joining(" "));
+    }
 }
