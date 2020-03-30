0deb1c842a6691bda04a945d0b8ff9521bced0ad
maxiepangz <mpang45456@gmail.com>
2020-03-30 08:34:56 +0800

    added some gets

--- src/main/java/seedu/address/searcher/ModulePlanner.java
+++ src/main/java/seedu/address/searcher/ModulePlanner.java
@@ -1,4 +1,14 @@
 package seedu.address.searcher;
 
+import javax.lang.model.element.ModuleElement;
+import java.util.ArrayList;
+
 public class ModulePlanner {
+    private ArrayList<Module> myMods;
+
+    public ModulePlanner() {
+        myMods = new ArrayList<>();
+    }
+
+
 }
