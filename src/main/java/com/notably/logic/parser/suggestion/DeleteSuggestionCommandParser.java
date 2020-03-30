e70d3bdeccf7f7f00698d6404f9ba58576c34f30
johannagwan <johannagwan@gmail.com>
2020-03-30 11:42:56 +0800

    Refactor DeleteSuggestionCommandParser

--- src/main/java/com/notably/logic/parser/suggestion/DeleteSuggestionCommandParser.java
+++ src/main/java/com/notably/logic/parser/suggestion/DeleteSuggestionCommandParser.java
@@ -43,14 +43,12 @@ public class DeleteSuggestionCommandParser implements SuggestionCommandParser<De
             throw new ParseException(String.format("Invalid input"));
         }
 
-        String titles = argMultimap.getValue(PREFIX_TITLE).get();
-        AbsolutePath uncorrectedPath = ParserUtil.createAbsolutePath(titles, model.getCurrentlyOpenPath());
+        String title = argMultimap.getValue(PREFIX_TITLE).get();
+        AbsolutePath uncorrectedPath = ParserUtil.createAbsolutePath(title, model.getCurrentlyOpenPath());
         Optional<AbsolutePath> correctedPath = correctionEngine.correct(uncorrectedPath).getCorrectedItem();
 
-        if (correctedPath.equals(Optional.empty())) {
-            throw new ParseException("Invalid path");
-        }
-
-        return new DeleteSuggestionCommand(correctedPath.get());
+        return correctedPath
+                .map(DeleteSuggestionCommand::new)
+                .orElseThrow(() -> new ParseException("Invalid path"));
     }
 }
