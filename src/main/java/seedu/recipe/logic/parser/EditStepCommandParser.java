59af18a5594c25f09749178481aef2e8b8b278b6
pekchinsiongbrian <57068044+pekchinsiongbrian@users.noreply.github.com>
2020-03-30 18:32:04 +0800

    Removed unnecessary catch block in EditStepCommandParser

--- src/main/java/seedu/recipe/logic/parser/EditStepCommandParser.java
+++ src/main/java/seedu/recipe/logic/parser/EditStepCommandParser.java
@@ -36,8 +36,6 @@ public class EditStepCommandParser implements Parser<EditStepCommand> {
         try {
             index = ParserUtil.parseIndex(argsArray[0]);
             stepNumber = ParserUtil.parseIndex(argsArray[1]).getZeroBased();
-        } catch (ParseException pe) {
-            throw new ParseException(pe.getMessage());
         } catch (ArrayIndexOutOfBoundsException aioobe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, EditStepCommand.MESSAGE_USAGE));
         }
