54aad5945b2d9803cfc0218af70c3cf3fb28c75d
PangJiaDa <pangjiada52@gmail.com>
2020-03-31 18:46:33 +0800

    Implement DeleteSupplierCommandParserTest tests

--- src/test/java/seedu/address/logic/parser/DeleteGoodCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteGoodCommandParserTest.java
@@ -3,32 +3,32 @@ package seedu.address.logic.parser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_GOOD;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_SUPPLIER;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.address.logic.commands.DeleteGoodCommand;
 import seedu.address.logic.commands.DeleteSupplierCommand;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the DeleteSupplierCommand code. For example, inputs "1" and "1 abc" take the
- * same path through the DeleteSupplierCommand, and therefore we test only one of them.
+ * outside of the DeleteGoodCommand code. For example, inputs "1" and "1 abc" take the
+ * same path through the DeleteGoodCommand, and therefore we test only one of them.
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
-public class DeleteSupplierCommandParserTest {
+public class DeleteGoodCommandParserTest {
 
-    private DeleteSupplierCommandParser parser = new DeleteSupplierCommandParser();
+    private DeleteGoodCommandParser parser = new DeleteGoodCommandParser();
 
     @Test
     public void parse_validArgs_returnsDeleteCommand() {
-        assertParseSuccess(parser, "1", new DeleteSupplierCommand(INDEX_FIRST_GOOD));
+        assertParseSuccess(parser, "1", new DeleteGoodCommand(INDEX_FIRST_SUPPLIER));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                DeleteSupplierCommand.MESSAGE_USAGE));
+                DeleteGoodCommand.MESSAGE_USAGE));
     }
 }
