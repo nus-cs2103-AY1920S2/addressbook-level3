ea67fc1aed2ea438a5e610036eb50b4552298e06
Kevin Putera <kevintiosanna@gmail.com>
2020-03-31 17:57:08 +0800

    Implement BlockContent

---
+++ src/main/java/com/notably/view/blockcontent/BlockContent.java
@@ -0,0 +1,24 @@
+package com.notably.view.blockcontent;
+
+import java.util.Objects;
+
+import com.notably.model.Model;
+
+import javafx.scene.layout.StackPane;
+
+public class BlockContent {
+    private final BlockContentDisplayView blockContentDisplayView;
+    private final BlockContentEditView blockContentEditView;
+
+    public BlockContent(StackPane blockContentPlaceholder, Model model) {
+        Objects.requireNonNull(blockContentPlaceholder);
+        Objects.requireNonNull(model);
+
+        blockContentDisplayView = new BlockContentDisplayView(model);
+        blockContentEditView = new BlockContentEditView(model);
+
+        // TODO: Integrate BlockContentEditView
+        blockContentPlaceholder.getChildren().addAll(blockContentDisplayView.getRoot());
+    }
+}
+
