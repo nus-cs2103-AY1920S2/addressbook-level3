6620fcb580944c94786a99b345562e83defb5cd7
Liu Chao <liu.chao5693@gmail.com>
2020-03-31 23:10:27 +0800

    Update formatting

--- src/main/java/seedu/address/logic/commands/EditSupplierCommand.java
+++ src/main/java/seedu/address/logic/commands/EditSupplierCommand.java
@@ -57,7 +57,7 @@ public class EditSupplierCommand extends Command {
     private final Index index;
     private final EditSupplierDescriptor editSupplierDescriptor;
 
-    public static Set<Offer> supplierToEditOffer;
+    //public static Set<Offer> supplierToEditOffer;
 
     /**
      * @param index of the supplier in the filtered supplier list to edit
