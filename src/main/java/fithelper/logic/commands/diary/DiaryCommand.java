9b51f40fac2862c4caa92af367e112d5f2602dd8
WANG-Yuchen-Alice <wangalice2000331@icloud.com>
2020-03-31 13:00:41 +0800

    Undo, redo: Diary.

--- src/main/java/fithelper/logic/commands/diary/DiaryCommand.java
+++ src/main/java/fithelper/logic/commands/diary/DiaryCommand.java
@@ -3,9 +3,12 @@ package fithelper.logic.commands.diary;
 import static fithelper.logic.commands.CommandResult.DisplayedPage.DIARY;
 import static java.util.Objects.requireNonNull;
 
+import fithelper.commons.core.LogsCenter;
 import fithelper.logic.commands.Command;
 import fithelper.logic.commands.CommandResult;
 import fithelper.model.Model;
+
+import java.util.logging.Logger;
 
 /**
  * Adds a entry to FitHelper.
@@ -18,9 +21,17 @@ public class DiaryCommand extends Command {
 
     public static final String MESSAGE_SUCCESS = "Now you are at Diary Page ~";
 
+    private static final String MESSAGE_COMMIT = "View my diary";
+
+    private static final Logger logger = LogsCenter.getLogger(DiaryCommand.class);
+
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
+
+        model.commit(MESSAGE_COMMIT);
+        logger.info("Switched to Diary Page");
+
         return new CommandResult(String.format(MESSAGE_SUCCESS), DIARY, false);
     }
 
