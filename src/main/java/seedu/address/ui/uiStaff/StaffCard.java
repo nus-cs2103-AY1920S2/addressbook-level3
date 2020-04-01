1d577ad4320714d83c1efa458624d5970e50c943
tiendat161 <tiendat98vn@gmail.com>
2020-04-01 15:39:14 +0800

    Change UI to show ID, assigned Courses for Staff

--- src/main/java/seedu/address/ui/uiStaff/StaffCard.java
+++ src/main/java/seedu/address/ui/uiStaff/StaffCard.java
@@ -57,7 +57,7 @@ public class StaffCard extends UiPart<Region> {
     phone.setText(staff.getPhone().value);
     address.setText(staff.getAddress().value);
     email.setText(staff.getEmail().value);
-
+    assignedCourses.setText(staff.getAssignedCourses().toString());
     salary.setText(staff.getSalary().value);
     staff.getTags().stream()
         .sorted(Comparator.comparing(tag -> tag.tagName))
