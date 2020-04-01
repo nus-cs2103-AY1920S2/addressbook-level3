10a2cfb7d4c080ddf5961ef78674bd4cc84d1a5a
PotatoCombat <39809238+PotatoCombat@users.noreply.github.com>
2020-04-01 12:43:21 +0800

    Add utilities in SessionType and GroupType

--- src/main/java/tatracker/model/group/GroupType.java
+++ src/main/java/tatracker/model/group/GroupType.java
@@ -1,22 +1,43 @@
 package tatracker.model.group;
 
+import static java.util.Objects.requireNonNull;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
  * Represents a group type.
  * Can be a lab or a tutorial.
  */
 public enum GroupType {
-    LAB("Lab"),
-    TUTORIAL("Tutorial"),
-    RECITATION("Recitation"),
-    OTHER("Other");
+    LAB,
+    TUTORIAL,
+    RECITATION,
+    OTHER;
 
-    private String typeName;
-    GroupType(String typeName) {
-        this.typeName = typeName;
+    public static final String MESSAGE_CONSTRAINTS =
+            "These are the only group types: lab, tutorial, recitation, other";
+
+    private static final Map<String, GroupType> GROUP_TYPES = Arrays.stream(values())
+            .collect(Collectors.toUnmodifiableMap(type -> type.name().toLowerCase(), type -> type));
+
+    public static boolean isValidGroupType(String test) {
+        return GROUP_TYPES.containsKey(test.toLowerCase());
+    }
+
+    public static GroupType getGroupType(String groupType) {
+        requireNonNull(groupType);
+        return GROUP_TYPES.get(groupType.toLowerCase());
     }
 
     @Override
     public String toString() {
-        return typeName;
+        String name = name();
+        if (name.length() > 0) {
+            // Capitalise first letter
+            name = name.substring(0, 1).toUpperCase() + name.substring(1);
+        }
+        return name;
     }
 }
