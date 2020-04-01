b681048522090f8879bc989984426c3397eefb7a
Alden <49450743+alcen@users.noreply.github.com>
2020-04-01 20:19:57 +0800

    Savings graph now functional

--- src/main/java/csdev/couponstash/ui/SummaryTab.java
+++ src/main/java/csdev/couponstash/ui/SummaryTab.java
@@ -1,9 +1,16 @@
 package csdev.couponstash.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import csdev.couponstash.commons.moneysymbol.MoneySymbol;
+import csdev.couponstash.commons.util.DateUtil;
 import csdev.couponstash.model.coupon.Coupon;
+import csdev.couponstash.model.coupon.savings.DateSavingsSumMap;
+import csdev.couponstash.model.coupon.savings.MonetaryAmount;
 import csdev.couponstash.model.coupon.savings.PureMonetarySavings;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
@@ -43,6 +50,8 @@ public class SummaryTab extends UiPart<Region> {
     @FXML
     private VBox allSaveables;
 
+    private MonetaryAmount shownMonetaryAmount = new MonetaryAmount(0.0);
+
 
     public SummaryTab(ObservableList<Coupon> allCoupons, MoneySymbol moneySymbol) {
         super(FXML);
@@ -55,14 +64,21 @@ public class SummaryTab extends UiPart<Region> {
 
     public void updateView() {
         // set the graph
-        XYChart.Series<String, Number> savingsPerWeek = new XYChart.Series<String, Number>();
-        savingsChart.getData().add(savingsPerWeek);
+        DateSavingsSumMap mapOfAllCoupons = getMapOfAllCoupons();
+        XYChart.Series<String, Number> savingsPerWeek = this.getSeries(mapOfAllCoupons);
+        this.savingsChart.getData().add(savingsPerWeek);
+
         // sum up the total savings
+        PureMonetarySavings totalSavings = mapOfAllCoupons.values().stream()
+                .reduce(new PureMonetarySavings(),
+                        PureMonetarySavings::add);
 
         // set saveables
+        this.addToSaveables(totalSavings);
 
         // set the total amount
-        this.numericalAmount.setText(this.moneySymbol.getString());
+        this.shownMonetaryAmount = totalSavings.getMonetaryAmount().orElseThrow();
+        this.updateTotalAmount();
     }
 
     private void addToSaveables(PureMonetarySavings savingsSum) {
@@ -71,5 +87,32 @@ public class SummaryTab extends UiPart<Region> {
                     Label label = new Label(sva.getValue());
                     allSaveables.getChildren().add(label);
                 }));
+    }
+
+    private DateSavingsSumMap getMapOfAllCoupons() {
+        DateSavingsSumMap map = new DateSavingsSumMap();
+        this.allCoupons.stream()
+                .map(Coupon::getSavingsMap)
+                .forEach(map::addAll);
+        System.out.println(map.size());
+        return map;
+    }
+
+    private XYChart.Series<String, Number> getSeries(DateSavingsSumMap mapOfAllCoupons) {
+        XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
+        List<XYChart.Data<String, Number>> listOfData =
+                new ArrayList<XYChart.Data<String, Number>>();
+        mapOfAllCoupons.forEach((date, savings) ->
+                listOfData.add(new XYChart.Data<String, Number>(
+                        DateUtil.formatDate(date),
+                        savings.getMonetaryAmountAsDouble()
+                )));
+        series.setData(FXCollections.observableList(listOfData));
+        return series;
+    }
+
+    private void updateTotalAmount() {
+        this.numericalAmount.setText(
+                this.shownMonetaryAmount.getStringWithMoneySymbol(this.moneySymbol.getString()));
     }
 }
