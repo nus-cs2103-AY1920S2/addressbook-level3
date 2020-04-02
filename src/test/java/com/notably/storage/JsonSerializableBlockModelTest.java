3282563c58b63a1a58e71d951199b25e74af0080
Firzan Armani <mrfirzanarmani@gmail.com>
2020-04-02 12:06:00 +0800

    Add more tests for lastOpenedPath

--- src/test/java/com/notably/storage/JsonSerializableBlockModelTest.java
+++ src/test/java/com/notably/storage/JsonSerializableBlockModelTest.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import org.junit.jupiter.api.Test;
 
 import com.notably.commons.exceptions.IllegalValueException;
+import com.notably.commons.path.AbsolutePath;
 import com.notably.commons.util.JsonUtil;
 import com.notably.model.block.BlockModel;
 import com.notably.testutil.TypicalBlockModel;
@@ -19,6 +20,8 @@ public class JsonSerializableBlockModelTest {
     private static final Path INVALID_BLOCK_FILE = TEST_DATA_FOLDER.resolve("invalidBlockData.json");
     private static final Path DUPLICATE_BLOCK_FILE = TEST_DATA_FOLDER.resolve("duplicateBlockData.json");
     private static final Path MISSING_PATH_BLOCK_FILE = TEST_DATA_FOLDER.resolve("missingPathBlockData.json");
+    private static final Path PATH_DOESNT_EXIST_BLOCK_FILE = TEST_DATA_FOLDER.resolve("pathDoesntExistBlockData.json");
+    private static final Path BLANK_DATA_INVALID_PATH_BLOCK_FILE = TEST_DATA_FOLDER.resolve("blankDataInvalidPathBlockData.json");
 
     @Test
     public void toModelType_typicalBlocksFile_success() throws Exception {
@@ -51,5 +54,21 @@ public class JsonSerializableBlockModelTest {
             JsonSerializableBlockModel.class).get();
         assertThrows(IllegalValueException.class, JsonSerializableBlockModel.MISSING_LAST_OPENED_PATH_FIELD,
             dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_pathDoesntExistBlockFile_defaultFirstRootChild() throws Exception {
+        JsonSerializableBlockModel dataFromFile = JsonUtil.readJsonFile(PATH_DOESNT_EXIST_BLOCK_FILE,
+            JsonSerializableBlockModel.class).get();
+        BlockModel blockModel = dataFromFile.toModelType();
+        assertEquals(blockModel.getCurrentlyOpenPath(), AbsolutePath.fromString("/Y2S2"));
+    }
+
+    @Test
+    public void toModelType_blankDataInvalidPathBlockFile_defaultFirstRootChild() throws Exception {
+        JsonSerializableBlockModel dataFromFile = JsonUtil.readJsonFile(BLANK_DATA_INVALID_PATH_BLOCK_FILE,
+            JsonSerializableBlockModel.class).get();
+        BlockModel blockModel = dataFromFile.toModelType();
+        assertEquals(blockModel.getCurrentlyOpenPath(), AbsolutePath.fromString("/"));
     }
 }
