a958e859ff6dc83b6f1fd2bc2084b66413baeec2
WANG-Yuchen-Alice <wangalice2000331@icloud.com>
2020-04-02 10:54:29 +0800

    Checkstyle.

--- src/main/java/fithelper/model/util/SampleDataUtil.java
+++ src/main/java/fithelper/model/util/SampleDataUtil.java
@@ -9,15 +9,21 @@ import fithelper.model.WeightRecords;
 import fithelper.model.diary.Content;
 import fithelper.model.diary.Diary;
 import fithelper.model.diary.DiaryDate;
-import fithelper.model.entry.*;
+import fithelper.model.entry.Calorie;
+import fithelper.model.entry.Duration;
+import fithelper.model.entry.Entry;
+import fithelper.model.entry.Location;
+import fithelper.model.entry.Name;
+import fithelper.model.entry.Remark;
+import fithelper.model.entry.Status;
+import fithelper.model.entry.Time;
+import fithelper.model.entry.Type;
 import fithelper.model.profile.Address;
 import fithelper.model.profile.Age;
 import fithelper.model.profile.Gender;
 import fithelper.model.profile.Height;
 import fithelper.model.profile.Profile;
 import fithelper.model.profile.TargetWeight;
-
-import java.time.LocalDate;
 
 /**
  * Contains utility methods for populating {@code FitHelper} with sample data.
@@ -35,35 +41,35 @@ public class SampleDataUtil {
                     new Location("Utown canteen"), new Calorie("300"), new Status("Done"), new Remark("Too expensive"),
                     new Duration("1")),
             new Entry(new Type("food"), new Name("Mala"), new Time(new DiaryDate().toString() + " 17:30"),
-                        new Location("Utown canteen"), new Calorie("150.5"), new Status("Done"), new Remark("Too expensive")),
+                    new Location("Utown canteen"), new Calorie("150.5"), new Status("Done"), new Remark("Too expensive")),
             new Entry(new Type("food"), new Name("Apple juice"), new Time("2020-03-01 11:30"),
                     new Location("Utown 711"), new Calorie("79")),
             new Entry(new Type("food"), new Name("Lemon juice"), new Time("2020-03-01 11:30"),
-                        new Location("Utown 711"), new Calorie("79"), new Duration("1")),
+                    new Location("Utown 711"), new Calorie("79"), new Duration("1")),
             new Entry(new Type("sports"), new Name("Running"), new Time("2020-04-02 10:00"),
                     new Location("Utown gym"), new Calorie("300")),
             new Entry(new Type("sports"), new Name("Running"), new Time(new DiaryDate().toString() + " 10:00"),
-                        new Location("Track"), new Calorie("300"), new Status("Done"), new Duration("1")),
+                    new Location("Track"), new Calorie("300"), new Status("Done"), new Duration("1")),
             new Entry(new Type("s"), new Name("Swimming"), new Time(new DiaryDate().toString() + " 20:20"),
                     new Location("Sports Center"), new Calorie("450.5"), new Remark("Very tired.")),
             new Entry(new Type("s"), new Name("Jogging"), new Time("2020-03-01 20:20"),
-                        new Location("Track"), new Calorie("450.5"), new Remark("Very tired."), new Duration("1")),
+                    new Location("Track"), new Calorie("450.5"), new Remark("Very tired."), new Duration("1")),
             new Entry(new Type("f"), new Name("Ice-cream"), new Time("2020-03-31 14:30"),
                     new Location("YIH 711"), new Calorie("700"), new Remark("Not healthy")),
             new Entry(new Type("f"), new Name("Ice-cream"), new Time("2020-03-31 14:30"),
-                        new Location("YIH"), new Calorie("700"), new Remark("Not healthy"), new Duration("1")),
+                    new Location("YIH"), new Calorie("700"), new Remark("Not healthy"), new Duration("1")),
             new Entry(new Type("food"), new Name("Chicken Rice"), new Time("2020-03-01 11:30"),
             new Location("Utown FineFood"), new Calorie("200")),
             new Entry(new Type("food"), new Name("Chicken Rice"), new Time("2020-03-01 11:30"),
-                        new Location("The Deck"), new Calorie("200"), new Duration("1")),
+                    new Location("The Deck"), new Calorie("200"), new Duration("1")),
             new Entry(new Type("sports"), new Name("Football"), new Time("2019-12-01 14:00"),
                     new Location("Utown towngreen"), new Calorie("400")),
             new Entry(new Type("sports"), new Name("Football"), new Time("2019-12-01 14:00"),
-                        new Location("NUS Trackfield"), new Calorie("400"), new Duration("1")),
+                    new Location("NUS Trackfield"), new Calorie("400"), new Duration("1")),
             new Entry(new Type("s"), new Name("Walking"), new Time("2020-02-01 19:20"),
                     new Location("PGP"), new Calorie("450.5"), new Remark("Feels good")),
             new Entry(new Type("s"), new Name("Walking"), new Time("2020-02-01 19:20"),
-                        new Location("PGPR"), new Calorie("450.5"), new Remark("Feels good"), new Duration("1")),
+                    new Location("PGPR"), new Calorie("450.5"), new Remark("Feels good"), new Duration("1")),
         };
     }
 
