5961000b940dafcd1fee09e64b5fb8fcb1a4d070
ZhangYiping126 <e0357438@u.nus.edu>
2020-04-02 23:05:34 +0800

    allow max 1 time clash

--- src/main/java/fithelper/model/FitHelper.java
+++ src/main/java/fithelper/model/FitHelper.java
@@ -228,13 +228,25 @@ public class FitHelper implements ReadOnlyFitHelper {
         long hasTimeClashesWithFood = foodEntries.countClashes(entry);
         long hasTimeClashesWithSports = sportsEntries.countClashes(entry);
         long count = hasTimeClashesWithFood + hasTimeClashesWithSports;
-        if (hasEntry(entry)) {
-            count--;
-        }
-        hasTimeClashes = count >= 2;
+        hasTimeClashes = count >= 1;
         return hasTimeClashes;
     }
 
+    /**
+     * Returns true if a entry has time clashes with at least two other entries{@code entry}.
+     */
+    public boolean hasTimeClashes(Entry entry, Entry editedEntry) {
+        requireNonNull(entry);
+        boolean hasTimeClashes;
+        long hasTimeClashesWithFood = foodEntries.countClashes(editedEntry);
+        long hasTimeClashesWithSports = sportsEntries.countClashes(editedEntry);
+        long count = hasTimeClashesWithFood + hasTimeClashesWithSports;
+        if (hasEntry(entry)) {
+            count--;
+        }
+        hasTimeClashes = count >= 1;
+        return hasTimeClashes;
+    }
     /**
      * Adds an entry to FitHelper.
      * The entry must not already exist in FitHelper.
