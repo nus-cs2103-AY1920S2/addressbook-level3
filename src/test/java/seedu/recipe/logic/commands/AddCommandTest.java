7e90682e985a26aa354ffb45c6d0822512b09d1c
beatricetay <beatricetayyt@gmail.com>
2020-04-03 00:50:38 +0800

    added some tests to plancommand

--- src/test/java/seedu/recipe/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/recipe/logic/commands/AddCommandTest.java
@@ -19,12 +19,12 @@ import seedu.recipe.commons.core.GuiSettings;
 import seedu.recipe.logic.commands.exceptions.CommandException;
 import seedu.recipe.model.Model;
 import seedu.recipe.model.ReadOnlyCookedRecordBook;
+import seedu.recipe.model.ReadOnlyPlannedBook;
 import seedu.recipe.model.ReadOnlyRecipeBook;
 import seedu.recipe.model.ReadOnlyUserPrefs;
 import seedu.recipe.model.RecipeBook;
 import seedu.recipe.model.cooked.Record;
 import seedu.recipe.model.plan.PlannedRecipe;
-import seedu.recipe.model.ReadOnlyPlannedBook;
 import seedu.recipe.model.recipe.Recipe;
 import seedu.recipe.testutil.RecipeBuilder;
 
