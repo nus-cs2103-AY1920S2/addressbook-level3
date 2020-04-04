1a53c28fffb2c48974ae0569940e36b014320f1c
maxiepangz <mpang45456@gmail.com>
2020-04-05 03:51:55 +0800

    Added some support

---
+++ src/main/java/seedu/address/searcher/ModPlanClass.java
@@ -0,0 +1,26 @@
+package seedu.address.searcher;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ModPlanClass {
+    public static ArrayList<String> allMajors = new ArrayList<>(
+            Arrays.asList("Chinese Language", "Chinese Studies", "Japanese Studies", "Malay Studies",
+                    "South Asian Studies", "Southeast Asian Studies", "English Language", "English Literature",
+                    "History", "Philosophy", "Theater Studies", "Communications and New Media", "Economics",
+                    "Geography", "Political Science", "Psychology", "Social Work", "Sociology",
+                    "Environmental Studies in Geography", "Global Studies", "Business Administration(Accountancy)",
+                    "Business Administration", "Business Analytics", "Computer Science", "Information Systems",
+                    "Information Security", "Computer Engineering", "Dentistry", "Architecture", "Industrial Design",
+                    "Landscape Architecture", "Project and Facilities Management", "Real Estate",
+                    "Biomedical Engineering", "Chemical Engineering", "Civil Engineering", "Engineering Science",
+                    "Environmental Engineering", "Electrical Engineering", "Industrial and Systems Engineering",
+                    "Material Science Engineering", "Mechanical Engineering", "Computer Engineering",
+                    "Undergraduate Law Programme", "Graduate LL.B. Programme", "Medicine", "Nursing", "Music",
+                    "Applied Mathematics", "Chemistry", "Computational Biology", "Data Science and Analytics",
+                    "Environmental Studies in Biology", "Food Science and Technology", "Life Sciences", "Mathematics",
+                    "Pharmacy", "Pharmaceutical Science", "Physics", "Quantitative Finance", "Statistics"
+    ));
+
+    
+}
