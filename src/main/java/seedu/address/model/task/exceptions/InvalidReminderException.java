4284a951e1f3f562bcc7f9ee919212b478d61db6
Arthur Lee <ykarthurlee@gmail.com>
2020-04-04 09:44:36 +0800

    "Changed copywriting for invalid reminder"

--- src/main/java/seedu/address/model/task/exceptions/InvalidReminderException.java
+++ src/main/java/seedu/address/model/task/exceptions/InvalidReminderException.java
@@ -5,6 +5,6 @@ import seedu.address.logic.parser.exceptions.ParseException;
 /** Signals that the reminder's time is before the current time */
 public class InvalidReminderException extends ParseException {
     public InvalidReminderException() {
-        super("Operation would result in invalid reminder due to the wrong timing");
+        super("Operation would result in invalid reminder due to time set being in the past.");
     }
 }
