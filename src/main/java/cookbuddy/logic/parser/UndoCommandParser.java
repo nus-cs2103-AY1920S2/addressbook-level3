e963c8b1847382c814b8b5fb270f73458b10a8ec
muhd97 <zain.sma.mz@gmail.com>
2020-04-04 15:40:49 +0800

    Fix: undo 0 displays wrong error message

--- src/main/java/cookbuddy/logic/parser/UndoCommandParser.java
+++ src/main/java/cookbuddy/logic/parser/UndoCommandParser.java
@@ -15,6 +15,7 @@ public class UndoCommandParser implements Parser<UndoCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the UndoCommand
      * and returns a UndoCommand object for execution.
+     *
      * @throws ParseException if the user input does not conform the expected format
      */
     public UndoCommand parse(String args) throws ParseException {
@@ -22,8 +23,8 @@ public class UndoCommandParser implements Parser<UndoCommand> {
             Index index = ParserUtil.parseIndex(args);
             return new UndoCommand(index);
         } catch (ParseException pe) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, UndoCommand.MESSAGE_USAGE), pe);
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, pe.getLocalizedMessage())
+                + "\nFor a command summary, type \"help undo\"");
         }
     }
 
