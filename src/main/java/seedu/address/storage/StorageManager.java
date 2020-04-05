a32b522c135d2c39eb2ecc2b8e8be84a73e72acd
chanckben <chanchoonchan@BenRig.localdomain>
2020-04-05 17:13:53 +0800

    Clean up code and edited test cases to reflect latest changes

--- src/main/java/seedu/address/storage/StorageManager.java
+++ src/main/java/seedu/address/storage/StorageManager.java
@@ -17,7 +17,6 @@ import seedu.address.model.UserPrefs;
 public class StorageManager implements Storage {
 
     private static final Logger logger = LogsCenter.getLogger(StorageManager.class);
-    //private AddressBookStorage addressBookStorage;
     private ProfileListStorage profileListStorage;
     private UserPrefsStorage userPrefsStorage;
 
@@ -26,12 +25,6 @@ public class StorageManager implements Storage {
         this.profileListStorage = profileListStorage;
         this.userPrefsStorage = userPrefsStorage;
     }
-
-    /*public StorageManager(AddressBookStorage addressBookStorage, UserPrefsStorage userPrefsStorage) {
-        super();
-        this.addressBookStorage = addressBookStorage;
-        this.userPrefsStorage = userPrefsStorage;
-    }*/
 
     // ================ UserPrefs methods ==============================
 
@@ -78,35 +71,5 @@ public class StorageManager implements Storage {
         logger.fine("Attempting to read data from file: " + filePath);
         profileListStorage.saveProfileList(profileList, filePath);
     }
-
-
-    // ================ AddressBook methods ==============================
-
-    /*@Override
-    public Path getAddressBookFilePath() {
-        return addressBookStorage.getAddressBookFilePath();
-    }
-
-    @Override
-    public Optional<ReadOnlyAddressBook> readAddressBook() throws DataConversionException, IOException {
-        return readAddressBook(addressBookStorage.getAddressBookFilePath());
-    }
-
-    @Override
-    public Optional<ReadOnlyAddressBook> readAddressBook(Path filePath) throws DataConversionException, IOException {
-        logger.fine("Attempting to read data from file: " + filePath);
-        return addressBookStorage.readAddressBook(filePath);
-    }
-
-    @Override
-    public void saveAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
-        saveAddressBook(addressBook, addressBookStorage.getAddressBookFilePath());
-    }
-
-    @Override
-    public void saveAddressBook(ReadOnlyAddressBook addressBook, Path filePath) throws IOException {
-        logger.fine("Attempting to write to data file: " + filePath);
-        addressBookStorage.saveAddressBook(addressBook, filePath);
-    }*/
 
 }
