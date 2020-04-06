6e3d7134393c4cce213b7aedbbd8cd3de70fcf3e
wongchishan <wongchishan99@gmail.com>
2020-04-06 12:28:35 +0800

    Fix checkstyle

--- src/main/java/seedu/zerotoone/commons/core/Messages.java
+++ src/main/java/seedu/zerotoone/commons/core/Messages.java
@@ -9,7 +9,7 @@ public class Messages {
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_INDEX = "The index provided is invalid";
 
-    public static final String MESSAGE_INVALID_WORKOUT_EXERCISE
-            = "The exercise provided does not contain any sets! Please add a set and try again.";
+    public static final String MESSAGE_INVALID_WORKOUT_EXERCISE =
+            "The exercise provided does not contain any sets! Please add a set and try again.";
 
 }
