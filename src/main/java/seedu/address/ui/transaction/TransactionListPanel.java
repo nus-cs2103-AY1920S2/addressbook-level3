21a0fab5c0b89a3f22a76f6b28cfce0686365076
YingxuH <yingxu.he1998@gmail.com>
2020-04-07 01:42:56 +0800

    change transaction to table format

--- src/main/java/seedu/address/ui/transaction/TransactionListPanel.java
+++ src/main/java/seedu/address/ui/transaction/TransactionListPanel.java
@@ -2,10 +2,13 @@ package seedu.address.ui.transaction;
 
 import java.util.logging.Logger;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.transaction.Transaction;
@@ -19,12 +22,71 @@ public class TransactionListPanel extends UiPart<Region> {
     private final Logger logger = LogsCenter.getLogger(TransactionListPanel.class);
 
     @FXML
-    private ListView<Transaction> transactionListView;
+    private TableView<Transaction> transactionTableView;
+
+    @FXML
+    private TableColumn<Transaction, String> customerCol;
+
+    @FXML
+    private TableColumn<Transaction, String> productCol;
+
+    @FXML
+    private TableColumn<Transaction, String> dateTimeCol;
+
+    @FXML
+    private TableColumn<Transaction, String> quantityCol;
+
+    @FXML
+    private TableColumn<Transaction, String> amountCol;
+
+    @FXML
+    private TableColumn<Transaction, String> descriptionCol;
 
     public TransactionListPanel(ObservableList<Transaction> transactionList) {
         super(FXML);
-        transactionListView.setItems(transactionList);
-        transactionListView.setCellFactory(listView -> new TransactionListViewCell());
+        transactionTableView.setItems(transactionList);
+        customerCol.setCellValueFactory(t -> {
+            SimpleStringProperty property = new SimpleStringProperty();
+            property.setValue(t.getValue().getCustomer().getName().toString());
+            return property;
+        });
+        customerCol.setSortable(false);
+
+        productCol.setCellValueFactory(t -> {
+            SimpleStringProperty property = new SimpleStringProperty();
+            property.setValue(t.getValue().getProduct().getDescription().toString());
+            return property;
+        });
+        productCol.setSortable(false);
+
+        dateTimeCol.setCellValueFactory(t -> {
+            SimpleStringProperty property = new SimpleStringProperty();
+            property.setValue(t.getValue().getDateTime().toString());
+            return property;
+        });
+        dateTimeCol.setSortable(false);
+
+        quantityCol.setCellValueFactory(t -> {
+            SimpleStringProperty property = new SimpleStringProperty();
+            property.setValue(t.getValue().getQuantity().toString());
+            return property;
+        });
+        quantityCol.setSortable(false);
+
+        amountCol.setCellValueFactory(t -> {
+            SimpleStringProperty property = new SimpleStringProperty();
+            property.setValue(t.getValue().getMoney().toString());
+            return property;
+        });
+        amountCol.setSortable(false);
+
+        descriptionCol.setCellValueFactory(t -> {
+            SimpleStringProperty property = new SimpleStringProperty();
+            property.setValue(t.getValue().getDescription().toString());
+            return property;
+        });
+        descriptionCol.setSortable(false);
+
     }
 
     /**
