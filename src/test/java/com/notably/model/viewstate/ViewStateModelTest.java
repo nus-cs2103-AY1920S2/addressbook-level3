334c0573b38462a3f55acf6ecb92bcb20c7ee53b
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-04-08 01:22:11 +0800

    Add test cases for viewStateModel

---
+++ src/test/java/com/notably/model/viewstate/ViewStateModelTest.java
@@ -0,0 +1,35 @@
+package com.notably.model.viewstate;
+
+import static com.notably.testutil.Assert.assertThrows;
+
+import static java.util.Objects.requireNonNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javafx.beans.property.BooleanProperty;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ViewStateModelTest {
+    private ViewStateModel viewStateModel;
+
+    @BeforeEach
+    public void setUpBeforeEach() {
+        viewStateModel = new ViewStateModelImpl();
+    }
+
+    //============ BlockEditFlagModel =============================================================
+    @Test
+    public void blockEditableProperty_void_returnsValidProperty() {
+        assertNotNull(viewStateModel.blockEditableProperty());
+    }
+
+    @Test
+    public void isBlockEditable_void_returnsValidBoolean() {
+        assertNotNull(viewStateModel.isBlockEditable());
+    }
+
+    @Test
+    public void setBlockEditable_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> viewStateModel.setBlockEditable(null));
+    }
+}
