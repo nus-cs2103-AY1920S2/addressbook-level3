ea8af58aa0300e9f066b8327e916801dac4980db
chuayijing <yijing.chua@gmail.com>
2020-04-07 16:42:21 +0800

    Fix add session

--- src/main/java/tatracker/logic/parser/session/AddSessionCommandParser.java
+++ src/main/java/tatracker/logic/parser/session/AddSessionCommandParser.java
@@ -12,12 +12,15 @@ import static tatracker.logic.parser.Prefixes.START_TIME;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.stream.Stream;
 
 import tatracker.logic.commands.session.AddSessionCommand;
+import tatracker.logic.commands.student.FilterStudentViewCommand;
 import tatracker.logic.parser.ArgumentMultimap;
 import tatracker.logic.parser.ArgumentTokenizer;
 import tatracker.logic.parser.Parser;
 import tatracker.logic.parser.ParserUtil;
+import tatracker.logic.parser.Prefix;
 import tatracker.logic.parser.exceptions.ParseException;
 import tatracker.model.session.Session;
 
@@ -41,16 +44,22 @@ public class AddSessionCommandParser implements Parser<AddSessionCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public AddSessionCommand parse(String args) throws ParseException {
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, START_TIME, END_TIME,
-                DATE, RECUR, MODULE, SESSION_TYPE, NOTES);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, MODULE, START_TIME, END_TIME,
+                DATE, RECUR, SESSION_TYPE, NOTES);
 
-        if (!argMultimap.getPreamble().isEmpty()) {
+        if (!arePrefixesPresent(argMultimap, MODULE)
+               || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     AddSessionCommand.DETAILS.getUsage()));
         }
 
         LocalDate date = LocalDate.now();
         Session sessionToAdd = new Session();
+
+
+        if (argMultimap.getValue(MODULE).isPresent()) {
+            sessionToAdd.setModuleCode(ParserUtil.parseValue(argMultimap.getValue(MODULE).get().toUpperCase()));
+        }
 
         if (argMultimap.getValue(DATE).isPresent()) {
             date = ParserUtil.parseDate(argMultimap.getValue(DATE).get());
@@ -66,14 +75,16 @@ public class AddSessionCommandParser implements Parser<AddSessionCommand> {
             LocalTime endTime = ParserUtil.parseTime(argMultimap.getValue(END_TIME).get());
             sessionToAdd.setEndDateTime(LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(),
                     endTime.getHour(), endTime.getMinute(), endTime.getSecond()));
+        } else {
+            if (argMultimap.getValue(START_TIME).isPresent()) {
+                LocalTime startTime = ParserUtil.parseTime(argMultimap.getValue(START_TIME).get());
+                sessionToAdd.setEndDateTime(LocalDateTime.of(date.getYear(), date.getMonth(), date.getDayOfMonth(),
+                        startTime.getHour(), startTime.getMinute(), startTime.getSecond()));
+            }
         }
 
         if (argMultimap.getValue(RECUR).isPresent()) {
             sessionToAdd.setRecurring(ParserUtil.parseNumWeeks(argMultimap.getValue(RECUR).get()));
-        }
-
-        if (argMultimap.getValue(MODULE).isPresent()) {
-            sessionToAdd.setModuleCode(ParserUtil.parseValue(argMultimap.getValue(MODULE).get().toUpperCase()));
         }
 
         if (argMultimap.getValue(SESSION_TYPE).isPresent()) {
@@ -86,5 +97,13 @@ public class AddSessionCommandParser implements Parser<AddSessionCommand> {
         }
 
         return new AddSessionCommand(sessionToAdd);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
