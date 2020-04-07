e6d070a4d36df6f291b006ce014e3924f7c2c41a
agnesnatasya <e0313565@u.nus.edu>
2020-04-07 20:19:46 +0800

    Adds test for DeleteCommandParser

--- src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteCommandParserTest.java
@@ -1,19 +1,22 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtility.INVALID_QUESTION_NUMBER_1;
 import static seedu.address.logic.commands.CommandTestUtility.VALID_ATTRIBUTE_PERSISTENCE;
 import static seedu.address.logic.commands.CommandTestUtility.VALID_INTERVIEWEE_JANE;
 import static seedu.address.logic.commands.CommandTestUtility.VALID_PROPERTY_ATTRIBUTE;
 import static seedu.address.logic.commands.CommandTestUtility.VALID_PROPERTY_INTERVIEWEE;
-//import static seedu.address.logic.commands.CommandTestUtility.VALID_PROPERTY_QUESTION;
+import static seedu.address.logic.commands.CommandTestUtility.VALID_PROPERTY_QUESTION;
 import static seedu.address.logic.commands.CommandTestUtility.WHITESPACE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.logic.parser.DeleteCommandParser.INVALID_QUESTION_NUMBER_MESSAGE;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.DeleteAttributeCommand;
 import seedu.address.logic.commands.DeleteIntervieweeCommand;
+import seedu.address.logic.commands.DeleteQuestionCommand;
 
 
 class DeleteCommandParserTest {
@@ -33,18 +36,24 @@ class DeleteCommandParserTest {
                 new DeleteAttributeCommand("persistence"));
     }
 
-    //    @Test
-    //    void parse_compulsoryFieldsMissing_success() {
-    //
-    //        assertParseFailure(parser, WHITESPACE + VALID_PROPERTY_INTERVIEWEE + WHITESPACE,
-    //                MESSAGE_UNKNOWN_COMMAND);
-    //
-    //        assertParseFailure(parser, WHITESPACE + VALID_PROPERTY_ATTRIBUTE + WHITESPACE,
-    //                MESSAGE_UNKNOWN_COMMAND);
-    //
-    //        assertParseFailure(parser, WHITESPACE + VALID_PROPERTY_QUESTION + WHITESPACE,
-    //                MESSAGE_UNKNOWN_COMMAND);
-    //    }
+    @Test
+    void parse_compulsoryFieldsMissing_success() {
+        assertParseFailure(parser, WHITESPACE + VALID_PROPERTY_INTERVIEWEE + WHITESPACE,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteIntervieweeCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, WHITESPACE + VALID_PROPERTY_ATTRIBUTE + WHITESPACE,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteAttributeCommand.MESSAGE_USAGE));
+
+        assertParseFailure(parser, WHITESPACE + VALID_PROPERTY_QUESTION + WHITESPACE,
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteQuestionCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    void parse_invalidField_success() {
+
+        assertParseFailure(parser, WHITESPACE + VALID_PROPERTY_QUESTION + WHITESPACE + INVALID_QUESTION_NUMBER_1,
+                INVALID_QUESTION_NUMBER_MESSAGE);
+    }
 
     @Test
     public void parse_argumentMissing_failure() {
