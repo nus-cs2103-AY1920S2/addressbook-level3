04eecaf1eef10d47da4ce0ca1c95761c49fb5068
gyant <e0325592@u.nus.edu>
2020-04-08 02:28:39 +0800

    Add ProfileListBuilder

---
+++ src/test/java/seedu/address/testutil/ProfileListBuilder.java
@@ -0,0 +1,34 @@
+package seedu.address.testutil;
+
+import seedu.address.model.ProfileList;
+import seedu.address.model.profile.Profile;
+
+/**
+ * A utility class to help with building ProfileList objects.
+ * Example usage: <br>
+ *     {@code ProfileList pl = new ProfileListBuilder().withPerson("John", "Doe").build();}
+ */
+public class ProfileListBuilder {
+
+    private ProfileList profileList;
+
+    public ProfileListBuilder() {
+        profileList = new ProfileList();
+    }
+
+    public ProfileListBuilder(ProfileList profileList) {
+        this.profileList = profileList;
+    }
+
+    /**
+     * Adds a new {@code Profile} to the {@code ProfileList} that we are building.
+     */
+    public ProfileListBuilder withProfile(Profile profile) {
+        profileList.addProfile(profile);
+        return this;
+    }
+
+    public ProfileList build() {
+        return profileList;
+    }
+}
