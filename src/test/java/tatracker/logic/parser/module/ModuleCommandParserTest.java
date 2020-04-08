dcc832377a08f81ab2d2ccf5404e5c87c31da78e
Aakanksha Rai <rai.aakanksha@gmail.com>
2020-04-08 20:08:16 +0800

    AddModuleCommandParserTest

---
+++ src/test/java/tatracker/logic/parser/module/ModuleCommandParserTest.java
@@ -0,0 +1,14 @@
+package tatracker.logic.parser.module;
+
+import org.junit.jupiter.api.Test;
+
+public class ModuleCommandParserTest {
+
+    private ModuleCommandParser parser = new ModuleCommandParser();
+
+    @Test
+    public void parse_allFieldsPresent_success() {
+
+    }
+
+}
