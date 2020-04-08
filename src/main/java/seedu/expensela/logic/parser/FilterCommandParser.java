bff3956a4a8004c7822cd687badc057ffa6637aa
PangKimJin <pangkimjin1@gmail.com>
2020-04-08 23:25:43 +0800

    fixed the filter by month "ALL" not working bug.

--- src/main/java/seedu/expensela/logic/parser/FilterCommandParser.java
+++ src/main/java/seedu/expensela/logic/parser/FilterCommandParser.java
@@ -130,10 +130,10 @@ public class FilterCommandParser implements Parser<FilterCommand> {
      * @return
      */
     public boolean isValidMonth(String input) {
-        if (input.length() != 7) {
-            return false;
-        }
         try {
+            if (input.equals("ALL")) {
+                return true;
+            }
             String inputYear = input.split("-")[0];
             int inputYearInteger = Integer.parseInt(inputYear);
             String inputMonth = input.split("-")[1];
