83103fd381b1c7c7afe8bcc4df4b6043a9c879f5
ZhangYiping126 <e0357438@u.nus.edu>
2020-04-08 20:19:57 +0800

    add test

---
+++ src/test/java/fithelper/model/ModelManagerTest.java
@@ -0,0 +1,69 @@
+package fithelper.model;
+
+import static fithelper.testutil.Assert.assertThrows;
+import static fithelper.testutil.TypicalEntries.FOOD;
+import static fithelper.testutil.TypicalEntries.SPORTS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import fithelper.testutil.FitHelperBuilder;
+
+public class ModelManagerTest {
+
+    private ModelManager modelManager = new ModelManager();
+
+    @Test
+    public void constructor() {
+        assertEquals(new FitHelper(), new FitHelper(modelManager.getFitHelper()));
+    }
+
+    @Test
+    public void hasEntry_nullPerson_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> modelManager.hasEntry(null));
+    }
+
+    @Test
+    public void hasEntry_entryNotInFitHelper_returnsFalse() {
+        assertFalse(modelManager.hasEntry(FOOD));
+    }
+
+    @Test
+    public void hasEntry_entryInFitHelper_returnsTrue() {
+        modelManager.addEntry(FOOD);
+        assertTrue(modelManager.hasEntry(FOOD));
+    }
+
+    @Test
+    public void getFilteredFoodEntryList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredFoodEntryList().remove(0));
+    }
+    @Test
+    public void getFilteredSportsEntryList_modifyList_throwsUnsupportedOperationException() {
+        assertThrows(UnsupportedOperationException.class, () -> modelManager.getFilteredSportsEntryList().remove(0));
+    }
+
+
+    @Test
+    public void equals() {
+        FitHelper fitHelper = new FitHelperBuilder().withEntry(FOOD).withEntry(SPORTS).build();
+        UserProfile userProfile = new UserProfile();
+        WeightRecords weightRecords = new WeightRecords();
+
+        // same values -> returns true
+        modelManager = new ModelManager(fitHelper, userProfile, weightRecords);
+        ModelManager modelManagerCopy = new ModelManager(fitHelper, userProfile, weightRecords);
+        assertTrue(modelManager.equals(modelManagerCopy));
+
+        // same object -> returns true
+        assertTrue(modelManager.equals(modelManager));
+
+        // null -> returns false
+        assertFalse(modelManager.equals(null));
+
+        // different types -> returns false
+        assertFalse(modelManager.equals(5));
+    }
+}
