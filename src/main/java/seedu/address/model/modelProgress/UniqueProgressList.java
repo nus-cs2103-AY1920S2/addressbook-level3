2c420f21b46c03708de37f48c03633943116a4d5
ivanteejj <58815457+ivanteejj@users.noreply.github.com>
2020-04-09 01:15:47 +0800

    updated UniqueProgressList

--- src/main/java/seedu/address/model/modelProgress/UniqueProgressList.java
+++ src/main/java/seedu/address/model/modelProgress/UniqueProgressList.java
@@ -6,12 +6,4 @@ import seedu.address.model.modelGeneric.UniqueList;
 import static java.util.Objects.requireNonNull;
 
 public class UniqueProgressList extends UniqueList<Progress> {
-    public void addUndoProgress(Progress undoneProgress) {
-        try{
-            requireNonNull(undoneProgress);
-            this.add(undoneProgress);
-        } catch (DuplicateException de) {
-            System.out.println(de.getMessage());
-        }
-    }
 }
