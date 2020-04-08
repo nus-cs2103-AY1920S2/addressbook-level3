7fa94084e7b733de1f60fb65a2cb2e72c42c8f36
WillySeahh <willy.seah@hotmail.com>
2020-04-08 16:30:18 +0800

    Done ListReceiptCommandTest.java

---
+++ src/test/java/seedu/eylah/expensesplitter/logic/commands/ListReceiptCommandTest.java
@@ -0,0 +1,41 @@
+package seedu.eylah.expensesplitter.logic.commands;
+
+import static seedu.eylah.expensesplitter.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.eylah.expensesplitter.logic.commands.CommandTestUtil.showPersonAtIndex;
+import static seedu.eylah.expensesplitter.testutil.TypicalEntries.getTypicalReceiptBook;
+import static seedu.eylah.expensesplitter.testutil.TypicalPersons.getTypicalPersonAmountBook;
+import static seedu.eylah.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.eylah.commons.model.UserPrefs;
+import seedu.eylah.expensesplitter.model.SplitterModel;
+import seedu.eylah.expensesplitter.model.SplitterModelManager;
+
+public class ListReceiptCommandTest {
+
+    private SplitterModel splitterModel;
+    private SplitterModel expectedSplitterModel;
+
+    @BeforeEach
+    public void setUp() {
+        splitterModel = new SplitterModelManager(getTypicalReceiptBook(), getTypicalPersonAmountBook(),
+            new UserPrefs());
+        expectedSplitterModel = new SplitterModelManager(splitterModel.getReceiptBook(),
+            splitterModel.getPersonAmountBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute_listIsNotFiltered_showsSameList() {
+        assertCommandSuccess(new ListReceiptCommand(), splitterModel, ListReceiptCommand.MESSAGE_SUCCESS,
+            expectedSplitterModel);
+    }
+
+    @Test
+    public void execute_listIsFiltered_showsEverything() {
+        showPersonAtIndex(splitterModel, INDEX_FIRST_PERSON);
+        assertCommandSuccess(new ListReceiptCommand(), splitterModel, ListReceiptCommand.MESSAGE_SUCCESS,
+            expectedSplitterModel);
+    }
+}
--- src/test/java/seedu/eylah/expensesplitter/testutil/TypicalEntries.java
+++ src/test/java/seedu/eylah/expensesplitter/testutil/TypicalEntries.java
@@ -23,6 +23,8 @@ public class TypicalEntries {
             .withPrice(new BigDecimal("19.00")).build();
     private static final Item PASTA = new ItemBuilder().withName("Pasta")
             .withPrice(new BigDecimal("5.00")).build();
+    private static final Item NOODLES = new ItemBuilder().withName("Noodles")
+            .withPrice(new BigDecimal("10.00")).build();
     private static final Person ANNABELLE = new PersonBuilder().withName("Annabelle").build();
     private static final Person BOBBY = new PersonBuilder().withName("Bobby").build();
     private static final ArrayList<Person> PERSONS_LIST_TWO = new ArrayList<>(Arrays.asList(ANNABELLE, BOBBY));
@@ -33,12 +35,13 @@ public class TypicalEntries {
             .withPersons(PERSONS_LIST_TWO).build();
     public static final Entry ENTRY_THREE = new EntryBuilder().withItem(PASTA)
             .withPersons(PERSONS_LIST_THREE).build();
+    public static final Entry ENTRY_FOUR = new EntryBuilder().withItem(NOODLES)
+            .withPersons(PERSONS_LIST_THREE).build();
 
     public static ReceiptBook getTypicalReceiptBook() {
         ReceiptBook receiptBook = new ReceiptBook();
         Receipt receipt = new Receipt();
         for (Entry entry : getTypicalEntries()) {
-            //receipt.addEntry(entry);
             receipt.addEntry(entry);
         }
         receiptBook.addReceipt(receipt);
@@ -46,6 +49,6 @@ public class TypicalEntries {
     }
 
     public static ArrayList<Entry> getTypicalEntries() {
-        return new ArrayList<>(Arrays.asList(ENTRY_ONE, ENTRY_TWO, ENTRY_THREE));
+        return new ArrayList<>(Arrays.asList(ENTRY_ONE, ENTRY_TWO, ENTRY_THREE, ENTRY_FOUR));
     }
 }
