45fac9eeddd9d95a5ec7096136e0317abb349155
johannagwan <johannagwan@gmail.com>
2020-04-09 05:01:20 +0800

    Add NewSugCmdParserTest

--- src/test/java/com/notably/logic/parser/suggestion/NewSuggestionCommandParserTest.java
+++ src/test/java/com/notably/logic/parser/suggestion/NewSuggestionCommandParserTest.java
@@ -1,37 +1,38 @@
 package com.notably.logic.parser.suggestion;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
-import com.notably.logic.commands.suggestion.SuggestionCommand;
 import com.notably.logic.suggestion.SuggestionTestUtil;
 import com.notably.model.Model;
 
-public class EditSuggestionCommandParserTest {
-    private static EditSuggestionCommandParser editSuggestionCommandParser;
+public class NewSuggestionCommandParserTest {
+    private static NewSuggestionCommandParser newSuggestionCommandParser;
     private static Model model;
 
-    private static final String RESPONSE_MESSAGE = "Edit this note";
+    private static final String RESPONSE_MESSAGE = "Create a new note";
+    private static final String RESPONSE_MESSAGE_WITH_TITLE = "Create a new note titled \"%s\".";
+    private static final String RESPONSE_MESSAGE_WITH_TITLE_AND_OPEN = "Create a new note titled \"%s\" and open it.";
+    private static final String ERROR_MESSAGE = "\"%s\" is an invalid command format. "
+            + "The correct format is \"new -t TITLE [-b BODY] [-o]\"";
+    private static final String ERROR_MESSAGE_INVALID_TITLE = "Title \"%s\" is invalid. "
+            + "Titles should only contain alphanumeric characters";
 
     @BeforeAll
     public static void setUp() {
         model = SuggestionTestUtil.getModel();
 
         // initialize parser
-        editSuggestionCommandParser = new EditSuggestionCommandParser(model);
+        newSuggestionCommandParser = new NewSuggestionCommandParser(model);
     }
 
+    /*
     @Test
     public void parse() {
         String arg = "";
 
-        Optional<? extends SuggestionCommand> command = editSuggestionCommandParser.parse(arg);
+        Optional<? extends SuggestionCommand> command = newSuggestionCommandParser.parse(arg);
 
         assertEquals(Optional.of(RESPONSE_MESSAGE),
                 model.responseTextProperty().getValue());
-    }
+    }*/
 }
