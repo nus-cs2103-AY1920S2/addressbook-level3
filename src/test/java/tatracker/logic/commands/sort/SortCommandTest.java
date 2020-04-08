4baff3a29f5461a0e74ef2a280f1d7845e63f03f
Aakanksha Rai <rai.aakanksha@gmail.com>
2020-04-08 17:18:13 +0800

    Add SortCommand tests

--- src/test/java/tatracker/logic/commands/sort/SortCommandTest.java
+++ src/test/java/tatracker/logic/commands/sort/SortCommandTest.java
@@ -1,4 +1,37 @@
 package tatracker.logic.commands.sort;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class SortCommandTest {
+
+    @Test
+    public void equals() {
+        SortCommand sortCommand =
+                new SortCommand(SortType.ALPHABETIC);
+
+        SortCommand sortCommandCopy =
+                new SortCommand(SortType.ALPHABETIC);
+
+        SortCommand sortNewTypeCommand =
+                new SortCommand(SortType.RATING_ASC);
+
+        // same object -> returns true
+        assertTrue(sortCommand.equals(sortCommand));
+
+        // same values -> returns true
+        assertTrue(sortCommand.equals(sortCommandCopy));
+
+        // different types -> returns false
+        assertFalse(sortCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(sortCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(sortCommand.equals(sortNewTypeCommand));
+    }
+
 }
