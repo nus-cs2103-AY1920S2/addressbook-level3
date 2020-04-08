e842499942fcd21d0277552711453e7374fc6511
PangKimJin <pangkimjin1@gmail.com>
2020-04-09 00:13:37 +0800

    added test cases for FilterCommandParserTest.

--- src/test/java/seedu/expensela/logic/parser/FilterCommandParserTest.java
+++ src/test/java/seedu/expensela/logic/parser/FilterCommandParserTest.java
@@ -52,4 +52,49 @@ class FilterCommandParserTest {
         assertParseSuccess(parser, " m/2020-01", expectedFilterCommand);
     }
 
+    @Test
+    public void parse_invalidCatArgs_throwsParseException() {
+        // category listed not in category enum
+        assertParseFailure(parser, " c/PETS", String.format(MESSAGE_INVALID_FILTER,
+                FilterCommand.MESSAGE_USAGE));
+
+        // category not in uppercase
+        assertParseFailure(parser, " c/food", String.format(MESSAGE_INVALID_FILTER,
+                FilterCommand.MESSAGE_USAGE));
+
+        // category mispelt
+        assertParseFailure(parser, " c/HEATH", String.format(MESSAGE_INVALID_FILTER,
+                FilterCommand.MESSAGE_USAGE));
+
+        // category not a String
+        assertParseFailure(parser, " c/1234", String.format(MESSAGE_INVALID_FILTER,
+                FilterCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidDateArgs_throwsParseException() {
+        // date year before 2000
+        assertParseFailure(parser, " m/1999-12", String.format(MESSAGE_INVALID_FILTER,
+                FilterCommand.MESSAGE_USAGE));
+
+        // date year is not an integer
+        assertParseFailure(parser, " m/two thousand and two-12", String.format(MESSAGE_INVALID_FILTER,
+                FilterCommand.MESSAGE_USAGE));
+
+        // invalid date month (too big)
+        assertParseFailure(parser, " m/2999-13", String.format(MESSAGE_INVALID_FILTER,
+                FilterCommand.MESSAGE_USAGE));
+
+        // invalid date month (too small)
+        assertParseFailure(parser, " m/2999-00", String.format(MESSAGE_INVALID_FILTER,
+                FilterCommand.MESSAGE_USAGE));
+
+        // invalid date month (not an integer)
+        assertParseFailure(parser, " m/January", String.format(MESSAGE_INVALID_FILTER,
+                FilterCommand.MESSAGE_USAGE));
+
+        // month and category swapped
+        assertParseFailure(parser, " c/2020-01 m/FOOD", String.format(MESSAGE_INVALID_FILTER,
+                FilterCommand.MESSAGE_USAGE));
+    }
 }
