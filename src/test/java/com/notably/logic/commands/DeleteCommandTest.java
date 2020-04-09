832f647d0c553ff4914da6bb8b10ec82522f05e1
Jia Zheng <ljiazh3ng@gmail.com>
2020-04-09 15:41:15 +0800

    DeleteCommandtestcase

--- src/test/java/com/notably/logic/commands/DeleteCommandTest.java
+++ src/test/java/com/notably/logic/commands/DeleteCommandTest.java
@@ -7,18 +7,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.notably.commons.path.AbsolutePath;
+import com.notably.commons.path.RelativePath;
 import com.notably.logic.commands.exceptions.CommandException;
 import com.notably.model.Model;
 import com.notably.model.ModelManager;
-import com.notably.model.block.Block;
-import com.notably.model.block.BlockImpl;
 import com.notably.model.block.BlockModel;
-import com.notably.model.block.BlockModelImpl;
-import com.notably.model.block.Title;
 import com.notably.model.suggestion.SuggestionModel;
 import com.notably.model.suggestion.SuggestionModelImpl;
 import com.notably.model.viewstate.ViewStateModel;
 import com.notably.model.viewstate.ViewStateModelImpl;
+import com.notably.testutil.TypicalBlockModel;
 
 class DeleteCommandTest {
     private static Model model;
@@ -27,24 +25,50 @@ class DeleteCommandTest {
     @BeforeEach
     public void setUp() {
         // Set up model
-        BlockModel blockModel = new BlockModelImpl();
+        BlockModel blockModel = TypicalBlockModel.getTypicalBlockModel();
         SuggestionModel suggestionModel = new SuggestionModelImpl();
         ViewStateModel viewStateModel = new ViewStateModelImpl();
         model = new ModelManager(blockModel, suggestionModel, viewStateModel);
 
-        // Populate model with test data
-        Block currentBlock = (new BlockImpl(new Title("CS2103")));
-        model.addBlockToCurrentPath(currentBlock);
-        toDeletePath = AbsolutePath.fromString("/CS2103");
     }
     @Test
-    void execute_validAbsolutePath_blockSuccessfullyDeleted() throws CommandException {
+    void execute_validAbsolutePathWithNoChildren_blockSuccessfullyDeleted() throws CommandException {
+        final AbsolutePath toDeletePath = TypicalBlockModel.PATH_TO_CS2106_TUTORIAL_1;
         final DeleteCommand deleteCommand = new DeleteCommand(toDeletePath);
 
         deleteCommand.execute(model);
 
         assertFalse(model.hasPath(toDeletePath));
 
+    }
+
+    @Test
+    void execute_validRelativePathWithNoChildren_blockSuccessfullyDeleted() throws CommandException {
+        final RelativePath toDeletePath = RelativePath.fromString("CS2106/Lecture Notes");
+        final DeleteCommand deleteCommand = new DeleteCommand(toDeletePath);
+
+        deleteCommand.execute(model);
+
+        assertFalse(model.hasPath(toDeletePath.toAbsolutePath(TypicalBlockModel.INITIAL_OPEN_PATH)));
+
+    }
+
+    @Test
+    void execute_validAbsolutePathWithChildren_blockSuccessfullyDeleted() throws CommandException {
+        final AbsolutePath toDeletePath = TypicalBlockModel.PATH_TO_CS2106;
+        final DeleteCommand deleteCommand = new DeleteCommand(toDeletePath);
+
+        final AbsolutePath expectedChildrenDeletion1 = TypicalBlockModel.PATH_TO_CS2106_LECTURES;
+        final AbsolutePath expectedChildrenDeletion2 = TypicalBlockModel.PATH_TO_CS2106_TUTORIALS;
+        final AbsolutePath expectedChildrenDeletion3 = TypicalBlockModel.PATH_TO_CS2106_TUTORIAL_1;
+
+        deleteCommand.execute(model);
+
+
+        assertFalse(model.hasPath(toDeletePath));
+        assertFalse(model.hasPath(expectedChildrenDeletion1));
+        assertFalse(model.hasPath(expectedChildrenDeletion2));
+        assertFalse(model.hasPath(expectedChildrenDeletion3));
     }
 
     @Test
@@ -56,8 +80,16 @@ class DeleteCommandTest {
     }
 
     @Test
-    void execute_deleteNoneExistingBlock_throwsCommandException() {
+    void execute_deleteNoneExistingBlockAbsolutePath_throwsCommandException() {
         final AbsolutePath rootPath = AbsolutePath.fromString("/NonExisting");
+        final DeleteCommand deleteCommand = new DeleteCommand(rootPath);
+
+        assertThrows(CommandException.class, () -> deleteCommand.execute(model));
+    }
+
+    @Test
+    void execute_deleteNoneExistingBlockRelativePath_throwsCommandException() {
+        final RelativePath rootPath = RelativePath.fromString("./CS2103T/NonExisiting");
         final DeleteCommand deleteCommand = new DeleteCommand(rootPath);
 
         assertThrows(CommandException.class, () -> deleteCommand.execute(model));
