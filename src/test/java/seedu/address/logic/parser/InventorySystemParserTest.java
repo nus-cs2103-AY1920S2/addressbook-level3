24f5910acd6a76ff0490e691aa65cce2f806cbfc
junhaotan <40597739+junhaotan@users.noreply.github.com>
2020-04-09 20:58:30 +0800

    Added more test for InventorySystemParserTest

--- src/test/java/seedu/address/logic/parser/InventorySystemParserTest.java
+++ src/test/java/seedu/address/logic/parser/InventorySystemParserTest.java
@@ -6,7 +6,9 @@ import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT
 import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PRODUCT;
 import static seedu.address.testutil.customer.TypicalCustomers.ALICE_ID;
+import static seedu.address.testutil.product.TypicalProducts.ABACUS_ID;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,38 +25,48 @@ import seedu.address.logic.commands.customer.EditCustomerCommand;
 import seedu.address.logic.commands.customer.EditCustomerCommand.EditCustomerDescriptor;
 import seedu.address.logic.commands.customer.FindCustomerCommand;
 import seedu.address.logic.commands.customer.ListCustomerCommand;
+import seedu.address.logic.commands.product.AddProductCommand;
+import seedu.address.logic.commands.product.ClearProductCommand;
+import seedu.address.logic.commands.product.DeleteProductCommand;
+import seedu.address.logic.commands.product.EditProductCommand;
+import seedu.address.logic.commands.product.FindProductCommand;
+import seedu.address.logic.commands.product.ListProductCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.customer.Customer;
+import seedu.address.model.product.Product;
 import seedu.address.testutil.customer.CustomerBuilder;
 import seedu.address.testutil.customer.CustomerUtil;
 import seedu.address.testutil.customer.EditCustomerDescriptorBuilder;
+import seedu.address.testutil.product.EditProductDescriptorBuilder;
+import seedu.address.testutil.product.ProductBuilder;
+import seedu.address.testutil.product.ProductUtil;
 
 public class InventorySystemParserTest {
 
     private final InventorySystemParser parser = new InventorySystemParser();
 
     @Test
-    public void parseCommand_add() throws Exception {
+    public void parseCommand_addCustomer() throws Exception {
         Customer customer = new CustomerBuilder(ALICE_ID).build();
         AddCustomerCommand command = (AddCustomerCommand) parser.parseCommand(CustomerUtil.getAddCommand(customer));
         assertEquals(new AddCustomerCommand(customer), command);
     }
 
     @Test
-    public void parseCommand_clear() throws Exception {
+    public void parseCommand_clearCustomer() throws Exception {
         assertTrue(parser.parseCommand(ClearCustomerCommand.COMMAND_WORD) instanceof ClearCustomerCommand);
         assertTrue(parser.parseCommand(ClearCustomerCommand.COMMAND_WORD + " 3") instanceof ClearCustomerCommand);
     }
 
     @Test
-    public void parseCommand_delete() throws Exception {
+    public void parseCommand_deleteCustomer() throws Exception {
         DeleteCustomerCommand command = (DeleteCustomerCommand) parser.parseCommand(
                 DeleteCustomerCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
         assertEquals(new DeleteCustomerCommand(INDEX_FIRST_PERSON), command);
     }
 
     @Test
-    public void parseCommand_edit() throws Exception {
+    public void parseCommand_editCustomer() throws Exception {
         Customer customer = new CustomerBuilder(ALICE_ID).build();
         EditCustomerDescriptor descriptor = new EditCustomerDescriptorBuilder(customer).build();
         EditCustomerCommand command = (EditCustomerCommand) parser.parseCommand(EditCustomerCommand.COMMAND_WORD + " "
@@ -63,17 +75,60 @@ public class InventorySystemParserTest {
     }
 
     @Test
-    public void parseCommand_exit() throws Exception {
-        assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
-        assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
-    }
-
-    @Test
-    public void parseCommand_find() throws Exception {
+    public void parseCommand_findCustomer() throws Exception {
         List<String> keywords = Arrays.asList("foo", "bar", "baz");
         FindCustomerCommand command = (FindCustomerCommand) parser.parseCommand(
                 FindCustomerCommand.COMMAND_WORD + " n/" + keywords.stream().collect(Collectors.joining(" ")));
         //        assertEquals(new FindCustomerCommand(new NameContainsKeywordsPredicate(keywords)), command);
+    }
+
+    @Test
+    public void parseCommand_listCustomer() throws Exception {
+        assertTrue(parser.parseCommand(ListCustomerCommand.COMMAND_WORD) instanceof ListCustomerCommand);
+        assertTrue(parser.parseCommand(ListCustomerCommand.COMMAND_WORD + " 3") instanceof ListCustomerCommand);
+    }
+
+    @Test
+    public void parseCommand_addProduct() throws Exception {
+        Product product = new ProductBuilder(ABACUS_ID).build();
+        AddProductCommand command = (AddProductCommand) parser.parseCommand(ProductUtil.getAddCommand(product));
+        assertEquals(new AddProductCommand(product), command);
+    }
+
+    @Test
+    public void parseCommand_clearProduct() throws Exception {
+        assertTrue(parser.parseCommand(ClearProductCommand.COMMAND_WORD) instanceof ClearProductCommand);
+        assertTrue(parser.parseCommand(ClearProductCommand.COMMAND_WORD + " 3") instanceof ClearProductCommand);
+    }
+
+    @Test
+    public void parseCommand_deleteProduct() throws Exception {
+        DeleteProductCommand command = (DeleteProductCommand) parser.parseCommand(
+                DeleteProductCommand.COMMAND_WORD + " " + INDEX_FIRST_PRODUCT.getOneBased());
+        assertEquals(new DeleteProductCommand(INDEX_FIRST_PRODUCT), command);
+    }
+
+    @Test
+    public void parseCommand_editProduct() throws Exception {
+        Product product = new ProductBuilder(ABACUS_ID).build();
+        EditProductCommand.EditProductDescriptor descriptor = new EditProductDescriptorBuilder(product).build();
+        EditProductCommand command = (EditProductCommand) parser.parseCommand(EditProductCommand.COMMAND_WORD + " "
+                + INDEX_FIRST_PRODUCT.getOneBased() + " " + ProductUtil.getEditProductDescriptorDetails(descriptor));
+        //        assertEquals(new EditProductCommand(INDEX_FIRST_PRODUCT, descriptor), command);
+    }
+
+    @Test
+    public void parseCommand_findProduct() throws Exception {
+        List<String> keywords = Arrays.asList("foo", "bar", "baz");
+        FindProductCommand command = (FindProductCommand) parser.parseCommand(
+                FindProductCommand.COMMAND_WORD + " p/" + keywords.stream().collect(Collectors.joining(" ")));
+        //        assertEquals(new FindProductCommand(new DescriptionContainsKeywordsPredicate(keywords)), command);
+    }
+
+    @Test
+    public void parseCommand_listProduct() throws Exception {
+        assertTrue(parser.parseCommand(ListProductCommand.COMMAND_WORD) instanceof ListProductCommand);
+        assertTrue(parser.parseCommand(ListProductCommand.COMMAND_WORD + " 3") instanceof ListProductCommand);
     }
 
     @Test
@@ -83,9 +138,9 @@ public class InventorySystemParserTest {
     }
 
     @Test
-    public void parseCommand_list() throws Exception {
-        assertTrue(parser.parseCommand(ListCustomerCommand.COMMAND_WORD) instanceof ListCustomerCommand);
-        assertTrue(parser.parseCommand(ListCustomerCommand.COMMAND_WORD + " 3") instanceof ListCustomerCommand);
+    public void parseCommand_exit() throws Exception {
+        assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD) instanceof ExitCommand);
+        assertTrue(parser.parseCommand(ExitCommand.COMMAND_WORD + " 3") instanceof ExitCommand);
     }
 
     @Test
