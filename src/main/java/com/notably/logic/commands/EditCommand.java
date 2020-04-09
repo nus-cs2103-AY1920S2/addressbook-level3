11aef16b948b7d87a4da356b9b53b5e41ced49cf
Jia Zheng <ljiazh3ng@gmail.com>
2020-04-09 14:48:10 +0800

    Remove Edit Command parser

--- src/main/java/com/notably/logic/commands/EditCommand.java
+++ src/main/java/com/notably/logic/commands/EditCommand.java
@@ -4,8 +4,6 @@ import static java.util.Objects.requireNonNull;
 
 import com.notably.logic.commands.exceptions.CommandException;
 import com.notably.model.Model;
-import com.notably.model.block.Body;
-import com.notably.model.block.exceptions.CannotModifyRootException;
 
 /**
  * Represent a command that edits the block's body.
@@ -13,25 +11,14 @@ import com.notably.model.block.exceptions.CannotModifyRootException;
 public class EditCommand extends Command {
     public static final String COMMAND_WORD = "edit";
     public static final String COMMAND_SHORTHAND = "e";
-    private final Body body;
-
-    public EditCommand(Body body) {
-        requireNonNull(body);
-        this.body = body;
-    }
 
     /**
      * Edit the Block body of the current directory.
      * @param notablyModel used to access the tree structure.
      * @throws CommandException
      */
-    public void execute(Model notablyModel) throws CommandException {
+    public void execute(Model notablyModel) {
         requireNonNull(notablyModel);
-        try {
-            notablyModel.updateCurrentlyOpenBlockBody(body);
-        } catch (CannotModifyRootException ex) {
-            throw new CommandException(ex.getMessage());
-        }
     }
 }
 
