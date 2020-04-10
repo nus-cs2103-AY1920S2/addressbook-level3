fcb47413ad0fb30c0c7ce58cc35d12ceb5bab990
Firzan Armani <mrfirzanarmani@gmail.com>
2020-04-10 14:50:09 +0800

    Update NewSuggestionCommandParser title error message

--- src/main/java/com/notably/logic/parser/suggestion/NewSuggestionCommandParser.java
+++ src/main/java/com/notably/logic/parser/suggestion/NewSuggestionCommandParser.java
@@ -25,6 +25,7 @@ public class NewSuggestionCommandParser implements SuggestionCommandParser<Sugge
     private static final String ERROR_MESSAGE_INVALID_COMMAND = "\"%s\" is an invalid creation format. "
             + "The correct format is \"new -t TITLE [-o]\"";
     private static final String ERROR_MESSAGE_INVALID_TITLE = "Title \"%s\" is invalid. "
+            + "Titles cannot start with the period character. "
             + "Titles should only contain alphanumeric characters and symbols except - and /";
 
     private Model model;
