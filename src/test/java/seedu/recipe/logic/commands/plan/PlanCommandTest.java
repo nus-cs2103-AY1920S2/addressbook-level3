f040161dc264cd540d8c94e96bc8e2966e226d06
beatricetay <beatricetayyt@gmail.com>
2020-04-11 07:16:27 +0800

    Plan command test

--- src/test/java/seedu/recipe/logic/commands/plan/PlanCommandTest.java
+++ src/test/java/seedu/recipe/logic/commands/plan/PlanCommandTest.java
@@ -1,10 +1,14 @@
 package seedu.recipe.logic.commands.plan;
 
+import static seedu.recipe.testutil.Assert.assertThrows;
+import static seedu.recipe.testutil.TypicalDates.DATE_TODAY;
+import static seedu.recipe.testutil.TypicalIndexes.INDEX_FIRST_RECIPE;
 import static seedu.recipe.testutil.TypicalRecipes.getTypicalRecipeBook;
 import static seedu.recipe.testutil.TypicalRecords.getTypicalRecordBook;
 
 import org.junit.jupiter.api.Test;
 
+import seedu.recipe.commons.core.index.Index;
 import seedu.recipe.model.Model;
 import seedu.recipe.model.ModelManager;
 import seedu.recipe.model.UserPrefs;
@@ -18,9 +22,16 @@ class PlanCommandTest {
 
     @Test
     public void constructor_null_throwsNullPointerException() {
-        // null index, null date
-        //assertThrows(NullPointerException.class, () -> new PlanCommand(null, null));
+        // null indexes, null date
+        assertThrows(NullPointerException.class, () -> new PlanCommand(null, null));
 
+        // valid indexes, null date
+        Index[] validIndexArray = new Index[1];
+        validIndexArray[0] = INDEX_FIRST_RECIPE;
+        assertThrows(NullPointerException.class, () -> new PlanCommand(validIndexArray, null));
+
+        // null indexes, valid date
+        assertThrows(NullPointerException.class, () -> new PlanCommand(null, DATE_TODAY));
     }
 
 }
