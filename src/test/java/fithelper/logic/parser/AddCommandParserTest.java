13f67c2489fc7f318f013dc84f4def03837a6a36
WANG-Yuchen-Alice <wangalice2000331@icloud.com>
2020-04-10 23:03:41 +0800

    Checkstyle.

--- src/test/java/fithelper/logic/parser/AddCommandParserTest.java
+++ src/test/java/fithelper/logic/parser/AddCommandParserTest.java
@@ -21,17 +21,17 @@ import static fithelper.logic.commands.CommandTestUtil.VALID_NAME_BURGER;
 import static fithelper.logic.commands.CommandTestUtil.VALID_TIME_BURGER;
 import static fithelper.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static fithelper.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static fithelper.testutil.TypicalEntriesUtil.CAKE;
 import static fithelper.testutil.TypicalEntriesUtil.BURGER;
+import static fithelper.testutil.TypicalEntriesUtil.CAKE;
 
 import org.junit.jupiter.api.Test;
 
 import fithelper.logic.commands.AddCommand;
 import fithelper.model.entry.Calorie;
 import fithelper.model.entry.Location;
+import fithelper.model.entry.Entry;
 import fithelper.model.entry.Name;
 import fithelper.model.entry.Time;
-import fithelper.model.entry.Entry;
 import fithelper.testutil.EntryBuilder;
 
 public class AddCommandParserTest {
@@ -60,8 +60,7 @@ public class AddCommandParserTest {
 
         // multiple calories - last calories accepted
         assertParseSuccess(parser, TYPE_DESC_FOOD + NAME_DESC_BURGER + TIME_DESC_BURGER 
-                + LOCATION_DESC_BURGER
-                + CALORIE_DESC_CAKE + CALORIE_DESC_BURGER, new AddCommand(expectedEntry));
+                + LOCATION_DESC_BURGER + CALORIE_DESC_CAKE + CALORIE_DESC_BURGER, new AddCommand(expectedEntry));
 
     }
 
@@ -80,28 +79,23 @@ public class AddCommandParserTest {
 
         // missing name prefix
         assertParseFailure(parser, TYPE_DESC_FOOD + VALID_NAME_BURGER + TIME_DESC_BURGER
-                + LOCATION_DESC_BURGER
-                + CALORIE_DESC_BURGER, expectedMessage);
+                + LOCATION_DESC_BURGER + CALORIE_DESC_BURGER, expectedMessage);
 
         // missing time prefix
         assertParseFailure(parser, TYPE_DESC_FOOD + NAME_DESC_BURGER + VALID_TIME_BURGER
-                + LOCATION_DESC_BURGER
-                + CALORIE_DESC_BURGER, expectedMessage);
+                + LOCATION_DESC_BURGER + CALORIE_DESC_BURGER, expectedMessage);
 
         // missing location prefix
         assertParseFailure(parser, TYPE_DESC_FOOD + NAME_DESC_BURGER + TIME_DESC_BURGER
-                + VALID_LOCATION_BURGER
-                + CALORIE_DESC_BURGER, expectedMessage);
+                + VALID_LOCATION_BURGER + CALORIE_DESC_BURGER, expectedMessage);
 
         // missing calorie prefix
         assertParseFailure(parser, TYPE_DESC_FOOD + NAME_DESC_BURGER + TIME_DESC_BURGER
-                + LOCATION_DESC_BURGER
-                + VALID_CALORIE_BURGER, expectedMessage);
+                + LOCATION_DESC_BURGER + VALID_CALORIE_BURGER, expectedMessage);
 
         // all prefixes missing
         assertParseFailure(parser, TYPE_DESC_FOOD + VALID_NAME_BURGER + VALID_TIME_BURGER
-                + VALID_LOCATION_BURGER
-                + VALID_CALORIE_BURGER, expectedMessage);
+                + VALID_LOCATION_BURGER + VALID_CALORIE_BURGER, expectedMessage);
     }
 
     @Test
@@ -125,8 +119,7 @@ public class AddCommandParserTest {
 
         // two invalid values, only first invalid value reported
         assertParseFailure(parser, TYPE_DESC_FOOD + INVALID_NAME_DESC + TIME_DESC_BURGER
-                        + LOCATION_DESC_BURGER + INVALID_CALORIE_DESC,
-                Name.MESSAGE_CONSTRAINTS);
+                        + LOCATION_DESC_BURGER + INVALID_CALORIE_DESC, Name.MESSAGE_CONSTRAINTS);
 
     }
     
