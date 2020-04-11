1c93b04fc4245f82e602289649ca7fb5ca15eb58
tiendat161 <tiendat98vn@gmail.com>
2020-04-11 18:29:58 +0800

    Docs for ReadOnlyUserPrefs

--- src/main/java/seedu/address/model/ReadOnlyUserPrefs.java
+++ src/main/java/seedu/address/model/ReadOnlyUserPrefs.java
@@ -1,15 +1,22 @@
 package seedu.address.model;
 
-import java.nio.file.Path;
 import seedu.address.commons.core.GuiSettings;
+
+import java.nio.file.Path;
 
 /**
  * Unmodifiable view of user prefs.
  */
 public interface ReadOnlyUserPrefs {
 
+  /**
+   * Get GUI Settings.
+   */
   GuiSettings getGuiSettings();
 
+  /**
+   * Get address book File Path.
+   */
   Path getAddressBookFilePath();
 
 }
