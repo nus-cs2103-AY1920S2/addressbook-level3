3b4731a3cac9af1a7774226b31f9738ab9e30977
tharshita <>
2020-04-12 07:26:48 +0800

    updated list goals

--- src/main/java/seedu/recipe/logic/commands/ListGoalsCommand.java
+++ src/main/java/seedu/recipe/logic/commands/ListGoalsCommand.java
@@ -11,7 +11,8 @@ public class ListGoalsCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": lists all available goals to be tracked";
     public static final String LIST = "Bulk like the Hulk\n"
             + "Wholesome Wholegrains\n"
-            + "Herbivore";
+            + "Herbivore\n"
+            + "Fruity Fiesta";
 
     @Override
     public CommandResult execute(Model model) {
