9846ddd283141fcfb8c012e165bd02d1bc14e93c
beatricetay <beatricetayyt@gmail.com>
2020-04-12 07:34:16 +0800

    Refactored isValidName

--- src/main/java/seedu/recipe/storage/cooked/JsonAdaptedRecord.java
+++ src/main/java/seedu/recipe/storage/cooked/JsonAdaptedRecord.java
@@ -61,7 +61,7 @@ class JsonAdaptedRecord {
         if (name == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
         }
-        if (!Name.isValidQuantity(name)) {
+        if (!Name.isValidName(name)) {
             throw new IllegalValueException(Name.MESSAGE_CONSTRAINTS);
         }
         final Name modelName = new Name(name);
