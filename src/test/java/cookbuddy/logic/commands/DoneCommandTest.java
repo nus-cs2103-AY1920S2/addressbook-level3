9ba1c44c543e134841189dd168d35e62366a5266
AdarshChugani <adarsh.chugani@gmail.com>
2020-04-11 16:25:46 +0800

    Add test cases for done command

---
+++ src/test/java/cookbuddy/logic/commands/DoneCommandTest.java
@@ -0,0 +1,45 @@
+package cookbuddy.logic.commands;
+
+import static cookbuddy.logic.commands.CommandTestUtil.assertCommandFailure;
+import static cookbuddy.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static cookbuddy.testutil.TypicalIndexes.INDEX_FIRST_RECIPE;
+import static cookbuddy.testutil.TypicalRecipes.getTypicalRecipeBook;
+
+import org.junit.jupiter.api.Test;
+
+import cookbuddy.commons.core.Messages;
+import cookbuddy.commons.core.index.Index;
+import cookbuddy.model.Model;
+import cookbuddy.model.ModelManager;
+import cookbuddy.model.UserPrefs;
+import cookbuddy.model.recipe.Recipe;
+
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
+ */
+public class DoneCommandTest {
+
+    private Model model = new ModelManager(getTypicalRecipeBook(), new UserPrefs());
+
+    @Test
+    public void execute_validIndexUnfilteredList_success() {
+        Recipe recipeToDo = model.getFilteredRecipeList().get(INDEX_FIRST_RECIPE.getZeroBased());
+        DoneCommand doneCommand = new DoneCommand(INDEX_FIRST_RECIPE);
+
+        String expectedMessage = String.format(DoneCommand.MESSAGE_DONE_RECIPE_SUCCESS, recipeToDo.getName());
+
+        ModelManager expectedModel = new ModelManager(model.getRecipeBook(), new UserPrefs());
+        expectedModel.favRecipe(recipeToDo);
+
+        assertCommandSuccess(doneCommand, model, expectedMessage, expectedModel);
+    }
+
+    @Test
+    public void execute_invalidIndexUnfilteredList_throwsCommandException() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredRecipeList().size() + 1);
+        DoneCommand doneCommand = new DoneCommand(outOfBoundIndex);
+
+        assertCommandFailure(doneCommand, model, Messages.MESSAGE_INVALID_RECIPE_DISPLAYED_INDEX);
+    }
+}
