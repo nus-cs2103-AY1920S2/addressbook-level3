45ce7426184a250e40b709b1de2fd8555281b684
HemanshuGandhi <hemanshu.gandhi@gmail.com>
2020-04-11 14:43:35 +0800

    Add more test cases to ViewStateModel

--- src/test/java/com/notably/model/viewstate/ViewStateModelTest.java
+++ src/test/java/com/notably/model/viewstate/ViewStateModelTest.java
@@ -2,6 +2,7 @@ package com.notably.model.viewstate;
 
 import static com.notably.testutil.Assert.assertThrows;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -31,6 +32,13 @@ public class ViewStateModelTest {
         assertThrows(NullPointerException.class, () -> viewStateModel.setInput(null));
     }
 
+    @Test
+    public void setInputAndGetInput() {
+        final String input = "This is the new input";
+        viewStateModel.setInput(input);
+        assertEquals(input, viewStateModel.getInput());
+    }
+
     //============ HelpFlagModel ==================================================================
     @Test
     public void helpOpenProperty_void_returnsValidProperty() {
@@ -47,6 +55,14 @@ public class ViewStateModelTest {
         assertThrows(NullPointerException.class, () -> viewStateModel.setHelpOpen(null));
     }
 
+    @Test
+    public void setHelpOpenAndGetIsHelpOpen() {
+        viewStateModel.setHelpOpen(true);
+        assertEquals(true, viewStateModel.isHelpOpen());
+        viewStateModel.setHelpOpen(false);
+        assertEquals(false, viewStateModel.isHelpOpen());
+    }
+
     //============ BlockEditFlagModel =============================================================
     @Test
     public void blockEditableProperty_void_returnsValidProperty() {
@@ -61,5 +77,13 @@ public class ViewStateModelTest {
     @Test
     public void setBlockEditable_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> viewStateModel.setBlockEditable(null));
+    }
+
+    @Test
+    public void setBlockEditableAndGetBlockEditable() {
+        viewStateModel.setBlockEditable(true);
+        assertEquals(true, viewStateModel.isBlockEditable());
+        viewStateModel.setBlockEditable(false);
+        assertEquals(false, viewStateModel.isBlockEditable());
     }
 }
