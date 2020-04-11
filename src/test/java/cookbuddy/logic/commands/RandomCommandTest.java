bd1585a6dbcb546f08e88cd3d706eb773e2f7090
AdarshChugani <adarsh.chugani@gmail.com>
2020-04-11 17:30:28 +0800

    Fix checkstyle

--- src/test/java/cookbuddy/logic/commands/RandomCommandTest.java
+++ src/test/java/cookbuddy/logic/commands/RandomCommandTest.java
@@ -1,15 +1,12 @@
 package cookbuddy.logic.commands;
 
-import static cookbuddy.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static cookbuddy.testutil.TypicalRecipes.getTypicalRecipeBook;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import cookbuddy.commons.core.index.Index;
 import cookbuddy.model.Model;
 import cookbuddy.model.ModelManager;
 import cookbuddy.model.UserPrefs;
-import cookbuddy.model.recipe.Recipe;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -24,7 +21,7 @@ public class RandomCommandTest {
     @Test
     public void equalityCheck() {
        RandomCommand randomCom = new RandomCommand();
-       RandomCommand sameVal = rc;
+       RandomCommand sameVal = randomCom;
        RandomCommand newRandom = new RandomCommand();
        assertEquals(randomCom, sameVal);
        if (randomCom.getTargetIndex().equals(newRandom.getTargetIndex())) {
