ab97c917002c3b69457fd54822a6815fc6c29973
Don Tay Xi Cheng <dontay0209@gmail.com>
2020-04-13 00:41:24 +0800

    Rectified build failure

--- src/main/java/nasa/ui/CalendarView.java
+++ src/main/java/nasa/ui/CalendarView.java
@@ -19,7 +19,6 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
