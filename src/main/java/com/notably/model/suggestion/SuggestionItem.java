787f8e4b9170030e94d48e0d269f3f6f094e141f
johannagwan <johannagwan@gmail.com>
2020-04-12 19:07:26 +0800

    Update javadoc SugItem

--- src/main/java/com/notably/model/suggestion/SuggestionItem.java
+++ src/main/java/com/notably/model/suggestion/SuggestionItem.java
@@ -8,15 +8,17 @@ import java.util.Optional;
 public interface SuggestionItem {
     /**
      * Gets the property value of the SuggestionItem based on the key.
+     *
      * @param key The key to a key-value mapping.
      * @return The value of the mapping.
      */
     Optional<String> getProperty(String key);
 
     /**
-     * Executes the action taken after the generator item is chosen.
+     * Executes the action taken after the suggestion item is chosen.
      * The action will be different depending on the type of SuggestionGenerator.
-     * @return The action taken after the generator item is chosen.
+     *
+     * @return The action taken after the suggestion item is chosen.
      */
     Runnable getAction();
 }
