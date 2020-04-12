114d59029bd5b38d17d9c6a9f023562bf1375df3
Jia Zheng <ljiazh3ng@gmail.com>
2020-04-12 13:25:31 +0800

    Delete command fix

--- src/main/java/com/notably/logic/commands/DeleteCommand.java
+++ src/main/java/com/notably/logic/commands/DeleteCommand.java
@@ -2,9 +2,6 @@ package com.notably.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.logging.Logger;
-
-import com.notably.commons.LogsCenter;
 import com.notably.commons.path.AbsolutePath;
 import com.notably.commons.path.Path;
 import com.notably.commons.path.RelativePath;
@@ -18,9 +15,6 @@ import com.notably.model.block.exceptions.NoSuchBlockException;
  */
 public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
-
-    private static final Logger logger = LogsCenter.getLogger(DeleteCommand.class);
-
     private Path targetPath;
 
     public DeleteCommand(Path targetPath) {
@@ -30,7 +24,6 @@ public class DeleteCommand extends Command {
     @Override
     public void execute(Model notablyModel) throws CommandException {
         requireNonNull(notablyModel);
-        logger.info("Executing DeleteCommand");
         if (this.targetPath instanceof RelativePath) {
             RelativePath toConvert = (RelativePath) this.targetPath;
             this.targetPath = toConvert.toAbsolutePath(notablyModel.getCurrentlyOpenPath());
@@ -38,16 +31,10 @@ public class DeleteCommand extends Command {
 
         try {
             notablyModel.removeBlock((AbsolutePath) this.targetPath);
-        } catch (NoSuchBlockException ex) {
-            logger.warning(String.format("Path '%s' cannot be found", this.targetPath));
-            throw new CommandException(ex.getMessage());
-        } catch (CannotModifyRootException ex) {
-            logger.warning("Deleting the root is forbidden");
+        } catch (NoSuchBlockException | CannotModifyRootException ex) {
             throw new CommandException(ex.getMessage());
         }
-        logger.info(String.format("Block at path '%s' deleted", this.targetPath.toString()));
     }
-
 
 
 }
