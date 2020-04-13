41dff67ef327b47dc0eaa4abba87f781c7caefc7
agnesnatasya <e0313565@u.nus.edu>
2020-04-13 23:28:45 +0800

    Implement FinaliseCommand test

---
+++ src/test/java/hirelah/logic/commands/FinaliseCommandTest.java
@@ -0,0 +1,36 @@
+package hirelah.logic.commands;
+
+import hirelah.commons.exceptions.IllegalValueException;
+import hirelah.model.Model;
+import hirelah.model.ModelManager;
+import hirelah.model.hirelah.AttributeList;
+import org.junit.jupiter.api.Test;
+
+import static hirelah.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static hirelah.logic.commands.CommandTestUtility.VALID_ATTRIBUTE_INTEGRITY;
+import static hirelah.logic.commands.FinaliseCommand.MESSAGE_SUCCESS;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FinaliseCommandTest {
+
+    FinaliseCommand command = new FinaliseCommand();
+
+    @Test
+    void execute_validFinalise_success() throws IllegalValueException {
+        StorageStub storage = new StorageStub();
+        Model actual = new ModelManager();
+
+        Model expected = new ModelManager();
+        expected.finaliseInterviewProperties();
+        CommandResult expectedCommandResult =
+                new ToggleCommandResult(MESSAGE_SUCCESS, ToggleView.INTERVIEWEE);
+
+        assertCommandSuccess(command, actual, expectedCommandResult, expected, storage,
+                expected::isFinalisedInterviewProperties);
+    }
+
+    @Test
+    void testEquals() {
+        assertEquals(new FinaliseCommand(), command);
+    }
+}
