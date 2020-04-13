bfb7e670fa9686b56f4f9c7c2acd55fc20d611ec
WillySeahh <willy.seah@hotmail.com>
2020-04-13 11:40:12 +0800

    Fixing 1 bug issue related to PersonAmountBook storage test, requesting help from teammate

--- src/test/java/seedu/eylah/expensesplitter/storage/JsonSerializablePersonAmountBookTest.java
+++ src/test/java/seedu/eylah/expensesplitter/storage/JsonSerializablePersonAmountBookTest.java
@@ -1,5 +1,6 @@
 package seedu.eylah.expensesplitter.storage;
 
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.eylah.testutil.Assert.assertThrows;
 
 import java.nio.file.Path;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.Test;
 
 import seedu.eylah.commons.exceptions.IllegalValueException;
 import seedu.eylah.commons.util.JsonUtil;
+//import seedu.eylah.expensesplitter.model.PersonAmountBook;
+//import seedu.eylah.expensesplitter.testutil.TypicalPersons;
 
 public class JsonSerializablePersonAmountBookTest {
 
@@ -17,6 +20,19 @@ public class JsonSerializablePersonAmountBookTest {
     private static final Path TYPICAL_PERSONS_FILE = TEST_DATA_FOLDER.resolve("typicalPersonsAmountBook.json");
     private static final Path INVALID_PERSON_FILE = TEST_DATA_FOLDER.resolve("invalidPersonAmountBook.json");
     private static final Path DUPLICATE_PERSON_FILE = TEST_DATA_FOLDER.resolve("duplicatePersonAmountBook.json");
+
+    /*
+    @Test
+    public void toModelType_typicalPersonAmountFile_success() throws Exception {
+        JsonSerializablePersonAmountBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
+            JsonSerializablePersonAmountBook.class).get();
+        PersonAmountBook personAmountBookFromFile = dataFromFile.toModelType();
+        PersonAmountBook typicalPersonAmountBook = TypicalPersons.getTypicalPersonAmountBook();
+        assertEquals(personAmountBookFromFile, typicalPersonAmountBook);
+    }
+
+     */
+
 
     @Test
     public void toModelType_invalidPersonFile_throwsIllegalValueException() throws Exception {
