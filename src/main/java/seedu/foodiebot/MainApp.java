f4f44bc7308af50b8ac3cba57c10195d5e3d6e35
Xavier Lee <43834485+lsjxavier@users.noreply.github.com>
2020-04-13 13:43:10 +0800

    Fix bug in budget file not available

--- src/main/java/seedu/foodiebot/MainApp.java
+++ src/main/java/seedu/foodiebot/MainApp.java
@@ -129,6 +129,7 @@ public class MainApp extends Application {
             storage.saveFoodieBot(initialData, "Canteen");
             storage.saveFoodieBot(initialData, "Stall");
             storage.saveFoodieBot(initialData, "Food");
+            storage.saveFoodieBot(initialData, "Budget");
 
         } catch (IOException e) {
             logger.warning("Failed to save foodiebot");
