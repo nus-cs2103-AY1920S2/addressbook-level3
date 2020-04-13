b446e35ac0a605039b72ba169dffed211708e61a
beatricetay <beatricetayyt@gmail.com>
2020-04-13 23:23:51 +0800

    import

--- src/main/java/seedu/recipe/logic/commands/plan/PlanCommand.java
+++ src/main/java/seedu/recipe/logic/commands/plan/PlanCommand.java
@@ -2,7 +2,6 @@ package seedu.recipe.logic.commands.plan;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.recipe.logic.parser.CliSyntax.PREFIX_DATE;
-import static seedu.recipe.model.Model.PREDICATE_SHOW_ALL_PLANNED_RECIPES;
 
 import java.util.ArrayList;
 import java.util.Arrays;
