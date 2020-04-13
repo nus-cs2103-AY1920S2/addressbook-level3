7cea2c7f1084daed460496a7458e5555a778f278
Jarrod-Bob <e0309003@u.nus.edu>
2020-04-13 21:50:21 +0800

    fixed checkstyle

--- src/main/java/seedu/eylah/diettracker/logic/parser/WeightCommandParser.java
+++ src/main/java/seedu/eylah/diettracker/logic/parser/WeightCommandParser.java
@@ -2,7 +2,6 @@ package seedu.eylah.diettracker.logic.parser;
 
 import static seedu.eylah.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import seedu.eylah.commons.logic.command.exception.CommandException;
 import seedu.eylah.commons.logic.parser.Parser;
 import seedu.eylah.commons.logic.parser.exception.ParseException;
 import seedu.eylah.diettracker.logic.commands.WeightCommand;
